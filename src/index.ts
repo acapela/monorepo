5c807e5b99381e29240c0252c99c010b0f41caea
Uku Markus Tammet <ukutammet@gmail.com>
2020-09-20 13:50:14 +0300

    Add more server boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,49 @@
 import express, { Application } from 'express';
+import { promisify } from 'util';
+import { Server, createServer } from 'http';
+import securityMiddleware from 'helmet';
+import { json } from 'body-parser';
+import { createTerminus } from '@godaddy/terminus';
 
 import { PORT, IS_PRODUCTION } from './config';
 import * as logger from './logger';
 
-function configureApp(): Application {
+function setupServer(): Server {
   const app = express();
-  app.use(logger.middleware());
-  return app;
+  setupMiddleware(app);
+  setupRoutes(app);
+
+  const server = createServer(app);
+  setupGracefulShutdown(server);
+
+  return server;
 }
 
-function start() {
-  const app = configureApp();
-  app.listen(PORT, () => logger.info('App started', { port: PORT, production: IS_PRODUCTION }));
+function setupMiddleware(app: Application): void {
+  app.use(securityMiddleware());
+  app.use(logger.middleware());
+  app.use(json());
+}
+
+function setupRoutes(app: Application): void {
+  app.get('/', (_, res) => res.end('Hello, world'));
+}
+
+function setupGracefulShutdown(server: Server) {
+  const closeServer = promisify(server.close.bind(server));
+
+  createTerminus(server, {
+    onSignal: async () => {
+      await closeServer();
+    },
+  });
+}
+
+function start(): void {
+  const server = setupServer();
+  server.listen(PORT, () =>
+    logger.info('Server started', { port: PORT, production: IS_PRODUCTION }),
+  );
 }
 
 start();
