ca0b70ee8d1dfcd87bb8af3ff5c3d493bac8ae72
Uku Markus Tammet <ukutammet@gmail.com>
2020-09-20 15:05:04 +0300

    Add initial authentication and user support

---
+++ src/authentication.ts
@@ -0,0 +1,64 @@
+import { Request, Response, Router } from 'express';
+import admin from 'firebase-admin';
+import firebase from 'firebase-admin';
+
+import { createOrFindUser, User } from './users';
+import { AuthenticationError } from './errors';
+
+export const router = Router();
+
+router.post('/users', verifyAuthentication, async (_, res) => {
+  const firebaseUser: FirebaseUser = res.locals.firebaseUser;
+  const user = await createOrFindUser({
+    firebaseId: firebaseUser.id,
+    email: firebaseUser.email!, // TODO: do we need email here? no guarantee it's there atm
+  });
+  await addHasuraClaimsForUser(user);
+  res.status(200).json({
+    id: user.id,
+    firebaseId: user.firebaseId,
+    email: user.email,
+  });
+});
+
+async function verifyAuthentication(req: Request, res: Response, next: (error?: Error) => any) {
+  const authenticationHeader = req.get('Authorization')!;
+  if (!authenticationHeader) {
+    throw new AuthenticationError('No authorization header present');
+  }
+  const type = authenticationHeader.slice(0, authenticationHeader.indexOf(' '));
+  if (type !== 'Bearer') {
+    throw new AuthenticationError('Unsupported authorization type');
+  }
+  const token = authenticationHeader.slice('Bearer '.length);
+  if (!token) {
+    throw new AuthenticationError('No bearer token present');
+  }
+  try {
+    const claims = await firebase.auth().verifyIdToken(token);
+    res.locals.firebaseUser = extractFirebaseUserFromClaims(claims);
+    next();
+  } catch (e) {
+    throw new AuthenticationError('Invalid bearer token');
+  }
+}
+
+function addHasuraClaimsForUser(user: User) {
+  return firebase.auth().setCustomUserClaims(user.firebaseId, {
+    'X-Hasura-User-Id': user.id,
+    'X-Hasura-Allowed-Roles': ['user'], // TODO: dynamically load this from the database
+    'X-Hasura-Default-Role': 'user',
+  });
+}
+
+function extractFirebaseUserFromClaims(claims: admin.auth.DecodedIdToken): FirebaseUser {
+  return {
+    id: claims.sub,
+    email: claims.email,
+  };
+}
+
+interface FirebaseUser {
+  id: string;
+  email: string | undefined;
+}
---
+++ src/users.ts
@@ -0,0 +1,67 @@
+import { v4 as uuid } from 'uuid';
+import database from './database';
+
+export async function createOrFindUser({
+  email,
+  firebaseId,
+}: {
+  email: string;
+  firebaseId: string;
+}) {
+  const user = await findUserByFirebaseId(firebaseId);
+  if (user) {
+    return user;
+  }
+  return createUser({ email, firebaseId });
+}
+
+export async function findUserByFirebaseId(firebaseId: string): Promise<User | null> {
+  const [databaseUser] = await database
+    .select('id, email, firebase_id')
+    .from('user')
+    .where({ firebase_id: firebaseId })
+    .limit(1);
+
+  if (databaseUser) {
+    return convertDatabaseUser(databaseUser);
+  }
+
+  return null;
+}
+
+export async function createUser({
+  email,
+  firebaseId,
+}: {
+  email: string;
+  firebaseId: string;
+}): Promise<User> {
+  const [databaseUser] = await database('user')
+    .insert({
+      id: uuid(),
+      email,
+      firebaseId,
+    })
+    .returning('id, email, firebase_id');
+  return convertDatabaseUser(databaseUser);
+}
+
+export interface User {
+  id: string;
+  email: string;
+  firebaseId: string;
+}
+
+interface DatabaseUser {
+  id: string;
+  email: string;
+  firebase_id: string;
+}
+
+function convertDatabaseUser(databaseUser: DatabaseUser): User {
+  return {
+    id: databaseUser.id,
+    email: databaseUser.email,
+    firebaseId: databaseUser.firebase_id,
+  };
+}
