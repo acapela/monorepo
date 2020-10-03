32a2bc382e8b99198b30951904471f8f85e244f6
Uku Markus Tammet <ukutammet@gmail.com>
2020-10-03 13:20:07 +0300

    Move to google cloud sql connection style and test connection

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,30 @@
 import knex from "knex";
 import config from "config";
+import * as logger from "./logger";
+
+const socketPath: string = config.get("database.host.socketPath") || "/cloudsql";
+const connection: string = config.get("database.host.connection");
+const host = connection ? `${socketPath}/${connection}` : socketPath;
 
 const database = knex({
   client: "pg",
-  connection: config.get("database.connectionUrl"),
+  connection: {
+    user: config.get("database.user"),
+    password: config.get("database.password"),
+    database: config.get("database.name"),
+    host,
+  },
 });
+
+async function testConnection() {
+  try {
+    await database.raw("select 1+1 as result");
+    logger.info("Successfully connected to database");
+  } catch (e) {
+    logger.error("Failed to connect to database");
+  }
+}
+
+testConnection();
 
 export default database;
