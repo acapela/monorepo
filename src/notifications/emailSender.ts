f7ac6e45daae14e069289405d6a5d24c256fc55b
Heiki Riesenkampf <17280588+heikir@users.noreply.github.com>
2021-01-27 16:38:46 +0100

    Fix type in secret config file (#31)
    * Fix type in secret config file
    
    * Add try catch to sending emails with sendgrid to capture errors

--- src/notifications/emailSender.ts
+++ src/notifications/emailSender.ts
@@ -1,8 +1,13 @@
 import sendgrid, { MailDataRequired } from "@sendgrid/mail";
 import config from "../config";
+import { InternalServerError } from "../errors";
 
 sendgrid.setApiKey(config.get("sendgrid.apiKey"));
 
 export async function sendEmail(email: MailDataRequired, isMultiple?: boolean): Promise<void> {
-  await sendgrid.send(email, isMultiple);
+  try {
+    await sendgrid.send(email, isMultiple);
+  } catch (e) {
+    throw new InternalServerError("Sending email failed");
+  }
 }
