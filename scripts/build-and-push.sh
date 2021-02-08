f506521e1d26072bdc85fb8988025f1034220a30
Christoph Witzko <github@christophwitzko.com>
2021-02-08 15:01:17 +0000

    chore: add docker build and push script

---
+++ scripts/build-and-push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ -z "${1:-}" ]; then
+  echo "version not set"
+  exit 1
+fi
+
+VERSION=$1
+IMAGE_NAME="eu.gcr.io/meetnomoreapp/backend"
+IMAGE_NAME_VERSION="$IMAGE_NAME:$VERSION"
+
+echo "building image..."
+docker build -t $IMAGE_NAME_VERSION .
+docker tag $IMAGE_NAME_VERSION $IMAGE_NAME
+
+echo "pushing to gcr..."
+docker push $IMAGE_NAME_VERSION
+docker push $IMAGE_NAME
