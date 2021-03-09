ce03a82b03e22cfad5b71eb4a45382f74c2e03bf
Christoph Witzko <github@christophwitzko.com>
2021-03-09 16:58:39 +0000

    feat: move hasura folder to backend

--- backend/hasura/migrations/1614693648235_create_table_public_verification_requests/up.sql
+++ backend/hasura/migrations/1614693648235_create_table_public_verification_requests/up.sql
@@ -14,5 +14,5 @@ CREATE TRIGGER "set_public_verification_requests_updated_at"
 BEFORE UPDATE ON "public"."verification_requests"
 FOR EACH ROW
 EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
-COMMENT ON TRIGGER "set_public_verification_requests_updated_at" ON "public"."verification_requests" 
+COMMENT ON TRIGGER "set_public_verification_requests_updated_at" ON "public"."verification_requests"
 IS 'trigger to set value of column "updated_at" to current timestamp on row update';
