91259b2a58ae913926619433230108d65c068bbd
Christoph Witzko <github@christophwitzko.com>
2021-03-26 13:40:53 +0100

    feat: load proxy server config from env

--- frontend/next-server.js
+++ frontend/next-server.js
@@ -2,22 +2,44 @@
 const express = require("express");
 const next = require("next");
 const proxy = require("express-http-proxy");
+const dotenv = require("dotenv");
+const path = require("path");
 
 const production = process.env.NODE_ENV === "production";
+dotenv.config({ path: production ? process.cwd() : path.resolve(__dirname, "..", ".env") });
+
 const app = next({ dir: process.cwd(), customServer: false });
 const handle = app.getRequestHandler();
 
+function proxyReqPathResolverWithPrefix(prefix) {
+  prefix = prefix || "/";
+  return (req) => {
+    const parts = req.url.split("?");
+    const newRoute = prefix + req.params[0] + (parts.length == 2 ? "?" + parts[1] : "");
+    return newRoute;
+  };
+}
+
+const config = {
+  apiEndpoint: process.env.BACKEND_HOST || "https://api-staging.acape.la",
+  hasuraEndpoint: process.env.HASURA_HOST || "https://backend-staging.acape.la",
+};
+
 async function start() {
-  console.log("Starting server");
+  console.log("Starting server...");
+  console.log(config);
   const app = express();
 
-  app.get("/healthz", (req, res) => {
-    res.send({ ok: true });
-  });
+  app.get("/healthz", (req, res) => res.send({ ok: true }));
 
-  // TODO: use correct endpoint from env
-  app.use("/graphql", proxy("https://backend-staging.acape.la/v1/graphql"));
-  app.use("/api/backend/*", proxy("https://api-staging.acape.la/api/"));
+  app.use(
+    "/graphql/*",
+    proxy(config.hasuraEndpoint, { proxyReqPathResolver: proxyReqPathResolverWithPrefix("/v1/graphql/") })
+  );
+  app.use(
+    "/api/backend/*",
+    proxy(config.apiEndpoint, { proxyReqPathResolver: proxyReqPathResolverWithPrefix("/api/") })
+  );
 
   app.all("*", (req, res) => handle(req, res));
 
