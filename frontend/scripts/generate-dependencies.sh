b098eb056f68d783606d01f4acda09c23e3c0540
Christoph Witzko <github@christophwitzko.com>
2021-04-08 16:53:44 +0200

    fix: include sub package dependencies

--- frontend/scripts/generate-dependencies.sh
+++ frontend/scripts/generate-dependencies.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-jq -rs '(.[0] * .[1]).dependencies | to_entries | map([.key, .value] | join("@")) | join("\n")' ../package.json package.json |
+jq -rs '(.[0] * .[1] * .[2] * .[3] * .[4] * .[5]).dependencies | to_entries | map([.key, .value] | join("@")) | join("\n")' package.json ../package.json ../ui/package.json ../shared/package.json ../db/package.json ../config/package.json |
 grep -v '@acapela'
