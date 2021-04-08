cbe89dd1fe7dff71a3dd1bbcb3575078847f1bbf
Christoph Witzko <github@christophwitzko.com>
2021-04-08 17:05:30 +0200

    docs: add comment to jq script

--- frontend/scripts/generate-dependencies.sh
+++ frontend/scripts/generate-dependencies.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 
+# this merges all dependencies from all packages in the workspace
 jq -rs '(.[0] * .[1] * .[2] * .[3] * .[4] * .[5]).dependencies | to_entries | map([.key, .value] | join("@")) | join("\n")' package.json ../package.json ../ui/package.json ../shared/package.json ../db/package.json ../config/package.json |
 grep -v '@acapela'
