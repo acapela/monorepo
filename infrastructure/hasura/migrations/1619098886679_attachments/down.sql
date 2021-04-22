8c43141b5029ceaa058d8f69fb4f06d5817df32a
Oleksii Koliada <alex.fiator@gmail.com>
2021-04-22 16:51:39 +0300

    refactor: generate attachment ID on the DB level

--- infrastructure/hasura/migrations/1619098886679_attachments/down.sql
+++ infrastructure/hasura/migrations/1619098886679_attachments/down.sql
@@ -5,5 +5,7 @@ ALTER TABLE "public"."message" DROP COLUMN "is_draft";
 
 DROP TABLE "public"."message_attachments";
 
+alter table "public"."attachment" drop constraint "attachment_id_key";
+
 DROP TABLE "public"."attachment";
 
