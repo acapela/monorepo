71544efd30b2850099359d1c0197fcfb326cf026
Oleksii Koliada <alex.fiator@gmail.com>
2021-04-27 13:51:57 +0300

    refactor: householding

--- backend/src/localtunnel.ts
+++ backend/src/localtunnel.ts
@@ -1,10 +1,15 @@
+import { hostname } from "os";
 import localtunnel, { Tunnel } from "localtunnel";
 
 let tunnel: Tunnel | null = null;
 
 export async function getTunnelPublicUrl() {
   if (!tunnel) {
-    tunnel = await localtunnel({ subdomain: "acapela-dev", port: process.env.BACKEND_PORT, allow_invalid_cert: true });
+    tunnel = await localtunnel({
+      subdomain: `acapela-dev-${hostname}`,
+      port: process.env.BACKEND_PORT,
+      allow_invalid_cert: true,
+    });
 
     tunnel.on("close", () => {
       tunnel = null;
