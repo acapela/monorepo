a3031cf60de86633ea9a131bc39f27bacc661d74
Oleksii Koliada <alex.fiator@gmail.com>
2021-04-28 17:14:37 +0300

    refactor: extract transcription logic into transcription service

---
+++ backend/src/transcriptions/transcriptionService.ts
@@ -0,0 +1,54 @@
+import { db } from "~db";
+import { assert } from "~shared/assert";
+import { getSignedDownloadUrl } from "../attachments/googleStorage";
+import { getSonixClient, MediaResponse } from "./sonixClient";
+
+export async function sendForTranscription(messageId: string) {
+  const messageAttachment = await db.message_attachments.findFirst({
+    where: { message_id: messageId },
+    include: { attachment: true },
+  });
+  const attachment = messageAttachment?.attachment;
+
+  assert(attachment, "Message to be transcribed has no attachment");
+
+  const sonix = getSonixClient();
+  const attachmentUrl = await getSignedDownloadUrl(attachment.id);
+  const language = "en";
+
+  const media = await sonix.submitNewMedia({
+    messageId,
+    fileUrl: attachmentUrl,
+    fileName: attachment.original_name,
+    language,
+  });
+
+  await db.message.update({
+    where: { id: messageId },
+    data: {
+      transcription: {
+        create: {
+          sonix_media_id: media.id,
+          status: media.status,
+        },
+      },
+    },
+  });
+}
+
+export async function updateMessageTranscription(media: MediaResponse) {
+  const sonix = getSonixClient();
+  const transcript = await sonix.getJsonTranscript({ mediaId: media.id });
+
+  await db.message.update({
+    where: { id: media.custom_data.messageId },
+    data: {
+      transcription: {
+        update: {
+          status: media.status,
+          transcript: transcript.transcript,
+        },
+      },
+    },
+  });
+}
--- backend/src/transcriptions/transcriptions.ts
+++ backend/src/transcriptions/transcriptions.ts
@@ -1,12 +1,15 @@
 import { Request, Response, Router } from "express";
-import { db } from "~db";
 import { Transcription_Status_Enum } from "~frontend/src/gql";
 import { assert } from "~shared/assert";
 import logger from "~shared/logger";
-import { getSonixClient, MediaResponse } from "./sonixClient";
+import { MediaResponse } from "./sonixClient";
+import { updateMessageTranscription } from "./transcriptionService";
 
 export const router = Router();
 
+/**
+ * This endpoint handles webhooks from Sonix once transcription completes or fails
+ */
 router.post("/v1/transcriptions", async (req: Request, res: Response) => {
   const { secret } = req.query;
 
@@ -21,20 +24,7 @@ router.post("/v1/transcriptions", async (req: Request, res: Response) => {
   assert(media, "Sonix call has no body");
 
   if (media.status === Transcription_Status_Enum.Completed) {
-    const sonix = getSonixClient();
-    const transcript = await sonix.getJsonTranscript({ mediaId: media.id });
-
-    await db.message.update({
-      where: { id: media.custom_data.messageId },
-      data: {
-        transcription: {
-          update: {
-            status: media.status,
-            transcript: transcript.transcript,
-          },
-        },
-      },
-    });
+    await updateMessageTranscription(media);
   }
 
   res.status(204).end();
