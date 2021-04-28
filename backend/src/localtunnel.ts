891c2560c3d5b9b153bd0257ccfb698fe6f317b1
Oleksii Koliada <alex.fiator@gmail.com>
2021-04-28 16:13:22 +0300

    refactor: address PR comments

--- backend/src/localtunnel.ts
+++ backend/src/localtunnel.ts
@@ -2,7 +2,11 @@ import localtunnel, { Tunnel } from "localtunnel";
 
 let tunnel: Tunnel | null = null;
 
-export async function getTunnelPublicUrl() {
+/**
+ * localtunnel creates a tunnel from localhost to a publicly available URL
+ * This way it's possible to receive webhooks to the dev environment
+ */
+export async function getTunnelPublicUrl(): Promise<string> {
   if (!tunnel) {
     tunnel = await localtunnel({ subdomain: "acapela-dev", port: process.env.BACKEND_PORT, allow_invalid_cert: true });
 
