feae4cd653a33f77287a13302d197787788c0f13
Adam Pietrasiak <adam@pietrasiak.com>
2021-05-05 15:34:02 +0200

    chore: fixed wrongly resolved conflicts

--- backend/src/localtunnel.ts
+++ backend/src/localtunnel.ts
@@ -1,4 +1,7 @@
+import os from "os";
 import localtunnel, { Tunnel } from "localtunnel";
+
+const hostname = os.hostname().toLocaleLowerCase().replace(/\./g, "-");
 
 let tunnel: Tunnel | null = null;
 
@@ -8,7 +11,11 @@ let tunnel: Tunnel | null = null;
  */
 export async function getTunnelPublicUrl(): Promise<string> {
   if (!tunnel) {
-    tunnel = await localtunnel({ subdomain: "acapela-dev", port: process.env.BACKEND_PORT, allow_invalid_cert: true });
+    tunnel = await localtunnel({
+      subdomain: `acapela-dev-${hostname}`,
+      port: process.env.BACKEND_PORT,
+      allow_invalid_cert: true,
+    });
 
     tunnel.on("close", () => {
       tunnel = null;
