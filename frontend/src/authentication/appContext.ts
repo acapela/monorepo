bbf587958603f200a7e4c344bd196528bdbb51a8
Adam Pietrasiak <adam@pietrasiak.com>
2021-05-06 15:02:55 +0200

    feat: automatically redirect to home view if user is logged in

---
+++ frontend/src/authentication/appContext.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest } from "next";
+import { parseJWTWithoutValidation } from "./jwt";
+import { Session } from "next-auth";
+import { IncomingMessage } from "http";
+
+type Empty = Record<string, never>;
+
+export function getUserFromAppContext<SessionData = Empty>(reqInput?: IncomingMessage) {
+  if (!reqInput) return null;
+
+  // Raw node request is enchaned in next.js with things like parsed cookies etc.
+  const req = reqInput as NextApiRequest;
+  const sessionToken = (req?.cookies?.["next-auth.session-token"] as string) ?? null;
+
+  if (!sessionToken) {
+    return null;
+  }
+
+  const userTokenContent = parseJWTWithoutValidation<Session & SessionData>(sessionToken);
+
+  return userTokenContent;
+}
