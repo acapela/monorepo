4409e1649debd59198443a023e4b937b41c43f20
Adam Pietrasiak <adam@pietrasiak.com>
2021-05-14 14:56:16 +0200

    fix: fixed invitations gql definitions

--- frontend/src/gql/invitations.ts
+++ frontend/src/gql/invitations.ts
@@ -10,30 +10,36 @@ import {
 
 import { createMutation, createQuery } from "./utils";
 
-export const [useCreateInviteMutation] = createMutation<CreateInviteMutation, CreateInviteMutationVariables>(gql`
-  mutation CreateInvite($email: String!, $roomId: uuid) {
-    invite: insert_room_invites_one(object: { email: $email, room_id: $roomId }) {
-      id
-      email
-      usedAt: used_at
+export const [useCreateInviteMutation] = createMutation<CreateInviteMutation, CreateInviteMutationVariables>(
+  () => gql`
+    mutation CreateInvite($email: String!, $roomId: uuid) {
+      invite: insert_room_invites_one(object: { email: $email, room_id: $roomId }) {
+        id
+        email
+        usedAt: used_at
+      }
     }
-  }
-`);
+  `
+);
 
-export const [useGetRoomInvitesQuery] = createQuery<GetRoomInvitesQuery, GetRoomInvitesQueryVariables>(gql`
-  query GetRoomInvites($roomId: uuid!) {
-    invites: room_invites(where: { room_id: { _eq: $roomId } }) {
-      id
-      email
-      usedAt: used_at
+export const [useGetRoomInvitesQuery] = createQuery<GetRoomInvitesQuery, GetRoomInvitesQueryVariables>(
+  () => gql`
+    query GetRoomInvites($roomId: uuid!) {
+      invites: room_invites(where: { room_id: { _eq: $roomId } }) {
+        id
+        email
+        usedAt: used_at
+      }
     }
-  }
-`);
+  `
+);
 
-export const [useAcceptInviteMutation] = createMutation<AcceptInviteMutation, AcceptInviteMutationVariables>(gql`
-  mutation AcceptInvite($code: String!) {
-    invite: accept_invite(code: $code) {
-      roomId: room_id
+export const [useAcceptInviteMutation] = createMutation<AcceptInviteMutation, AcceptInviteMutationVariables>(
+  () => gql`
+    mutation AcceptInvite($code: String!) {
+      invite: accept_invite(code: $code) {
+        roomId: room_id
+      }
     }
-  }
-`);
+  `
+);
