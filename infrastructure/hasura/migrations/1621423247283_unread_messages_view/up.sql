ab885bba2e71dd3c6f006b3642aee406298868da
Adam Pietrasiak <adam@pietrasiak.com>
2021-05-19 15:09:47 +0200

    fix: fixed broken migration

--- infrastructure/hasura/migrations/1621423247283_unread_messages_view/up.sql
+++ infrastructure/hasura/migrations/1621423247283_unread_messages_view/up.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE VIEW "public"."unread_messages" AS 
- SELECT rp.user_id,
+ SELECT rm.user_id,
     room.id AS room_id,
     topic.id AS topic_id,
     count(message.id) AS unread_messages
-   FROM ((((room_participants rp
-     LEFT JOIN room ON ((room.id = rp.room_id)))
+   FROM ((((room_member rm
+     LEFT JOIN room ON ((room.id = rm.room_id)))
      LEFT JOIN topic ON ((topic.room_id = room.id)))
-     LEFT JOIN last_seen_message ON (((last_seen_message.user_id = rp.user_id) AND (last_seen_message.topic_id = topic.id))))
+     LEFT JOIN last_seen_message ON (((last_seen_message.user_id = rm.user_id) AND (last_seen_message.topic_id = topic.id))))
      LEFT JOIN message ON ((message.topic_id = topic.id)))
   WHERE ((message.created_at > last_seen_message.seen_at) OR (last_seen_message.seen_at IS NULL))
-  GROUP BY rp.user_id, room.id, topic.id
-  ORDER BY rp.user_id, room.id, topic.id;
+  GROUP BY rm.user_id, room.id, topic.id
+  ORDER BY rm.user_id, room.id, topic.id;
