c1bc044365935375ebf7c464e8850545916d6ed2
Oleksii Koliada <alex.fiator@gmail.com>
2021-05-20 14:48:19 +0300

    fix: unread messages with new membership setup

--- infrastructure/hasura/migrations/1621423247283_unread_messages_view/up.sql
+++ infrastructure/hasura/migrations/1621423247283_unread_messages_view/up.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE VIEW "public"."unread_messages" AS 
- SELECT rm.user_id,
+ SELECT sm.user_id,
     room.id AS room_id,
     topic.id AS topic_id,
     count(message.id) AS unread_messages
-   FROM ((((room_member rm
-     LEFT JOIN room ON ((room.id = rm.room_id)))
+   FROM ((((space_member sm
+     LEFT JOIN room ON ((room.space_id = sm.space_id)))
      LEFT JOIN topic ON ((topic.room_id = room.id)))
-     LEFT JOIN last_seen_message ON (((last_seen_message.user_id = rm.user_id) AND (last_seen_message.topic_id = topic.id))))
+     LEFT JOIN last_seen_message ON (((last_seen_message.user_id = sm.user_id) AND (last_seen_message.topic_id = topic.id))))
      LEFT JOIN message ON ((message.topic_id = topic.id)))
   WHERE ((message.created_at > last_seen_message.seen_at) OR (last_seen_message.seen_at IS NULL))
-  GROUP BY rm.user_id, room.id, topic.id
-  ORDER BY rm.user_id, room.id, topic.id;
+  GROUP BY sm.user_id, room.id, topic.id
+  ORDER BY sm.user_id, room.id, topic.id;
