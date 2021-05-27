38f6c0aa857d8d5f606a0607aede2ddb07d9e4dd
Oleksii Koliada <alex.fiator@gmail.com>
2021-05-27 15:34:00 +0300

    fix: add topic modal

--- frontend/src/views/RoomView/TopicsList/index.tsx
+++ frontend/src/views/RoomView/TopicsList/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { TopicDetailedInfoFragment } from "~frontend/gql";
 import { useSingleRoomQuery } from "~frontend/gql/rooms";
 import { useUnreadMessages } from "~frontend/gql/topics";
-import { TopicCreationButton } from "~frontend/rooms/TopicCreationButton";
+import { useBoolean } from "~frontend/hooks/useBoolean";
+import { routes } from "~frontend/routes";
 import { UnreadTopicIndicator } from "~frontend/ui/UnreadTopicsIndicator";
+import { Button } from "~ui/button";
+import { AddTopicModal } from "./AddTopicModal";
 import { TopicMenuItem } from "./TopicMenuItem";
 
 interface Props {
@@ -14,6 +18,7 @@ interface Props {
 export function TopicsList({ roomId, activeTopicId }: Props) {
   const [roomData] = useSingleRoomQuery.subscription({ id: roomId });
   const [unreadMessagesData] = useUnreadMessages.subscription();
+  const [isAddingTopic, { set: openAddTopicModal, unset: closeAddTopicModal }] = useBoolean(false);
 
   const room = roomData?.room;
 
@@ -21,6 +26,16 @@ export function TopicsList({ roomId, activeTopicId }: Props) {
 
   return (
     <>
+      {isAddingTopic && (
+        <AddTopicModal
+          roomId={roomId}
+          onCloseRequest={closeAddTopicModal}
+          onCreated={(topic: TopicDetailedInfoFragment) =>
+            routes.spaceRoomTopic.push({ topicId: topic.id, spaceId: topic.room.space_id, roomId: topic.room.id })
+          }
+        />
+      )}
+
       {topics.length === 0 && <UINoAgendaMessage>This room has no topics yet.</UINoAgendaMessage>}
 
       {topics.map((topic) => {
@@ -35,7 +50,7 @@ export function TopicsList({ roomId, activeTopicId }: Props) {
           </UITopic>
         );
       })}
-      <TopicCreationButton roomId={room?.id} />
+      <Button onClick={openAddTopicModal}>Add topic</Button>
     </>
   );
 }
