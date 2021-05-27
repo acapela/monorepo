40adc6387ee533a52a59a417df89e5fe793f1aa8
Oleksii Koliada <alex.fiator@gmail.com>
2021-05-27 15:34:00 +0300

    fix: add topic modal

---
+++ frontend/src/views/RoomView/TopicsList/AddTopicModal.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { TopicDetailedInfoFragment } from "~frontend/gql";
+import { useCreateTopicMutation } from "~frontend/gql/topics";
+import { createNextIndex } from "~frontend/rooms/order";
+import { Modal } from "~frontend/ui/Modal";
+import { slugify } from "~shared/slugify";
+import { Button } from "~ui/button";
+import { TextInput } from "~ui/forms/TextInput";
+
+interface Props {
+  roomId: string;
+  onCloseRequest: () => void;
+  onCreated: (topic: TopicDetailedInfoFragment) => void;
+  lastTopicIndex?: string;
+}
+
+export const AddTopicModal = ({ roomId, lastTopicIndex, onCloseRequest, onCreated }: Props) => {
+  const [createTopic] = useCreateTopicMutation();
+  const [name, setName] = useState("");
+
+  async function onCreate() {
+    if (!name.trim()) {
+      onCloseRequest();
+      return;
+    }
+
+    const index = createNextIndex(lastTopicIndex);
+    const slug = slugify(name);
+
+    const { data: createTopicResult } = await createTopic({
+      name,
+      slug,
+      index,
+      roomId,
+    });
+
+    const topic = createTopicResult?.topic;
+
+    if (!topic) {
+      onCloseRequest();
+      return;
+    }
+
+    onCloseRequest();
+    onCreated(topic);
+  }
+
+  return (
+    <Modal
+      head={{
+        title: "Add topic",
+        description: "",
+      }}
+      onCloseRequest={onCloseRequest}
+    >
+      <UIContentWrapper>
+        <TextInput placeholder="Enter topic name" value={name} onChangeText={(value) => setName(value)} />
+        <Button onClick={onCreate}>Add</Button>
+      </UIContentWrapper>
+    </Modal>
+  );
+};
+
+const UIContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  ${Button} {
+    margin-top: 1rem;
+  }
+`;
