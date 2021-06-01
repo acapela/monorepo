ebba3549f5e0adf5f8b6c105f9193acff070d57e
Adam Pietrasiak <adam@pietrasiak.com>
2021-06-01 09:36:16 +0200

    verify if all env variables are set (#107)
    * feat: verify if all env variables are set
    
    * chore: updated logic flow of env var assert
    
    * fix: added STAGE to default env config
    
    * fix: parse string env vars to int

--- config/dotenv.ts
+++ config/dotenv.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 function getDotEnvPath() {
   if (!__dirname) {
-    // It is possible that __dirname is not avaliable (in next.js it is by design to disable access to file system)
+    // It is possible that __dirname is not set (in next.js it is by design to disable access to file system)
     return null;
   }
 
@@ -14,40 +14,99 @@ function getDotEnvPath() {
   return path.resolve(__dirname, "..", ".env");
 }
 
+/**
+ * We're setting list of all required env variables as an const array.
+ *
+ * We need it in runtime to exit early in case of missing env variables.
+ *
+ * We make it const so it can be used to provide autocompletion for type-safe process.env.[name]
+ */
+const requiredEnvVarNames = [
+  "STAGE",
+  "GOOGLE_APPLICATION_CREDENTIALS",
+  "GOOGLE_STORAGE_BUCKET",
+  "HASURA_GRAPHQL_URL",
+  "DB_HOST",
+  "DB_PORT",
+  "DB_USER",
+  "DB_PASSWORD",
+  "DB_NAME",
+  "FRONTEND_URL",
+  "AUTH_SECRET",
+  "AUTH_JWT_TOKEN_SECRET",
+  "GOOGLE_CLIENT_ID",
+  "GOOGLE_CLIENT_SECRET",
+  "HASURA_EVENT_SECRET",
+  "HASURA_API_URL",
+  "HASURA_API_SECRET",
+  "HASURA_API_ADMIN_ROLE",
+  "HASURA_ACTION_SECRET",
+  "HASURA_NOTIFICATION_SECRET",
+  "HASURA_WEBSOCKET_ENDPOINT",
+  "LOGGING_LEVEL",
+  "BACKEND_PORT",
+  "BACKEND_ROOT_URL",
+  "BACKEND_AUTH_TOKEN",
+  "SENDGRID_API_KEY",
+  "SONIX_API_KEY",
+  "SONIX_CALLBACK_SECRET",
+] as const;
+
+// Out of array of variable names, prepare types for process.env
+
+type EnvVarName = typeof requiredEnvVarNames[number];
+type EnvVariablesMap = Record<EnvVarName, string>;
+
+// It is possible to mark some env vars as optional by providing their default value.
+const optionalEnvVars: Partial<NodeJS.ProcessEnv> = {
+  STAGE: "production",
+};
+
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
     // Let's extend ProcessEnv to provide process.env autocompletion
-    export interface ProcessEnv {
+    export interface ProcessEnv extends EnvVariablesMap {
+      // It is possible to narrow down types of some env variables here.
       STAGE: "development" | "staging" | "production";
-      GOOGLE_APPLICATION_CREDENTIALS: string;
-      GOOGLE_STORAGE_BUCKET: string;
-      HASURA_GRAPHQL_URL: string;
-      DB_HOST: string;
-      DB_PORT: string;
-      DB_USER: string;
-      DB_PASSWORD: string;
-      DB_NAME: string;
-      FRONTEND_URL: string;
-      AUTH_SECRET: string;
-      AUTH_JWT_TOKEN_SECRET: string;
-      GOOGLE_CLIENT_ID: string;
-      GOOGLE_CLIENT_SECRET: string;
-      HASURA_EVENT_SECRET: string;
-      HASURA_API_URL: string;
-      HASURA_API_SECRET: string;
-      HASURA_API_ADMIN_ROLE: string;
-      HASURA_ACTION_SECRET: string;
-      HASURA_NOTIFICATION_SECRET: string;
-      HASURA_WEBSOCKET_ENDPOINT: string;
-      LOGGING_LEVEL: string;
-      BACKEND_PORT: number;
-      BACKEND_ROOT_URL: string;
-      BACKEND_AUTH_TOKEN: string;
-      SENDGRID_API_KEY: string;
-      SONIX_API_KEY: string;
-      SONIX_CALLBACK_SECRET: string;
     }
+  }
+}
+
+function assertRequiredEnvVariablesLoaded() {
+  const missingEnvVars: string[] = [];
+
+  for (const envVarName of requiredEnvVarNames) {
+    const envVarValue = process.env[envVarName];
+
+    if (typeof envVarValue !== "undefined") {
+      // Variable is set, there is nothing to do about it.
+      continue;
+    }
+
+    // We have no value, let's try to load it from defaults.
+    const valueFromOptionalEnvVars = optionalEnvVars[envVarName];
+
+    // We have default value provided - assign it to env var and continue without marking it as missing.
+    if (valueFromOptionalEnvVars) {
+      // Let's warn about it so developers are aware of it even if defaults are saving them from crash.
+      console.warn(`Using default value for env variable ${envVarName}.`);
+      // Assign it to process.env
+      Reflect.set(process.env, envVarName, valueFromOptionalEnvVars);
+      continue;
+    }
+
+    // There is no value and not able to load it from defaults.
+
+    // Add it to list of missing env variables instead of throwing now so we can throw with names of ALL missing variables
+    // at once.
+    missingEnvVars.push(envVarName);
+  }
+
+  if (missingEnvVars.length > 0) {
+    const missingEnvVarsNames = missingEnvVars.join(", ");
+
+    throw new Error(`Missing env variables - ${missingEnvVarsNames}`);
   }
 }
 
@@ -55,16 +114,12 @@ function loadRootDotEnv(): void {
   const dotEnvPath = getDotEnvPath();
 
   if (!dotEnvPath) {
+    console.warn(`Not able to get .env file path. Skip loading env variables in runtime.`);
     return;
-  }
-
-  if (!process.env.DB_NAME) {
-    console.warn(
-      `It was not possible to load dotenv file and env variables were not loaded in other way. It might lead to bugs or crashes.`
-    );
   }
 
   dotenv.config({ path: dotEnvPath });
 }
 
 loadRootDotEnv();
+assertRequiredEnvVariablesLoaded();
