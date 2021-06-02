243dc7af5bab600420961395f76dec214e0b8d8f
Heiki Riesenkampf <me@heikir.com>
2021-06-02 11:12:34 +0200

    feat(ui): add userback feedback button

---
+++ frontend/public/scripts/userback.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+window.Userback = window.Userback || {};
+Userback.access_token = "29970|44040|bV1489q26af7ZoRzAgnJrfPsV";
+(function (d) {
+  var s = d.createElement("script");
+  s.async = true;
+  s.src = "https://static.userback.io/widget/v1.js";
+  (d.head || d.body).appendChild(s);
+})(document);
