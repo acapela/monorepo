d01dee42880bd64ff8c34370b0f04949d4486507
Rodion Chachura <geekrodion@gmail.com>
2021-06-03 12:55:15 +0400

    feat: ACA 339 Rename Topics (#114)

---
+++ frontend/src/views/RoomView/TopicsList/ManageTopic/index.tsx
@@ -0,0 +1,65 @@
+import React, { useCallback } from "react";
+import styled from "styled-components";
+import { PopoverMenu, PopoverPosition } from "~ui/PopoverMenu";
+import { IconVerticalThreeDots } from "~ui/icons";
+import { ACTION_ACTIVE_COLOR, hoverTransition } from "~ui/transitions";
+import { TopicDetailedInfoFragment } from "~frontend/gql";
+import { openUIPrompt } from "~frontend/utils/prompt";
+import { useEditTopicMutation } from "~frontend/gql/topics";
+
+interface Props {
+  topic: TopicDetailedInfoFragment;
+}
+
+export const ManageTopic = ({ topic }: Props) => {
+  const [editTopic] = useEditTopicMutation();
+  const handleRenameSelect = useCallback(async () => {
+    const name = await openUIPrompt({
+      initialValue: topic.name || "",
+      title: "Rename topic",
+      submitLabel: "Rename",
+      placeholder: "Enter topic name",
+    });
+    if (!name?.trim()) {
+      return;
+    }
+    await editTopic({ topicId: topic.id, name });
+  }, [topic.name]);
+
+  return (
+    <>
+      <PopoverMenu
+        position={PopoverPosition.RIGHT_BOTTOM}
+        options={[
+          {
+            label: "Rename",
+            onSelect: handleRenameSelect,
+          },
+        ]}
+      >
+        <Toggle>
+          <IconVerticalThreeDots />
+        </Toggle>
+      </PopoverMenu>
+    </>
+  );
+};
+
+const TOGGLE_SIZE_PX = 18;
+
+const Toggle = styled.button`
+  width: ${TOGGLE_SIZE_PX}px;
+  height: ${TOGGLE_SIZE_PX}px;
+  background: #ffffff;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${hoverTransition()};
+  cursor: pointer;
+  @media (hover) {
+    :hover {
+      background-color: ${ACTION_ACTIVE_COLOR};
+    }
+  }
+`;
