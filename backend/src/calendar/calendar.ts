3041caf62f96969e14b6ab581b440d0d5c123014
Heiki Riesenkampf <me@heikir.com>
2021-06-08 12:21:41 +0200

    chore(backend): add initial API request handler with param validation logic

---
+++ backend/src/calendar/calendar.ts
@@ -0,0 +1,44 @@
+import { Request, Response, Router } from "express";
+import { assert } from "~shared/assert";
+import logger from "~shared/logger";
+import { extractAndAssertBearerToken } from "../authentication";
+import { AuthenticationError, BadRequestError } from "../errors";
+import { CalendarAPIRequestBody } from "./googleCalendarClient";
+
+export const router = Router();
+
+/**
+ * This endpoint handles handles requests from the client and fetches events from Google Calendar
+ */
+router.post("/v1/calendar", async (req: Request, res: Response) => {
+  const token = extractAndAssertBearerToken(req.get("Authorization") || "");
+
+  if (!token) {
+    logger.info("No token provided for the calendar API request");
+    throw new AuthenticationError("Calendar events API request made with invalid token");
+  }
+
+  if (token !== process.env.BACKEND_AUTH_TOKEN) {
+    logger.info("Incorrect token provided for the calendar API request");
+    throw new AuthenticationError("Calendar events API request made with invalid token");
+  }
+
+  const requestBody = req.body as CalendarAPIRequestBody;
+
+  assert(requestBody, "Calendar API request body is missing");
+
+  if (!requestBody.oAuthToken || !requestBody.eventsEndDate || !requestBody.eventsStartDate) {
+    throw new BadRequestError("Calendar events API request made with invalid body");
+  }
+
+  const eventsStartDate = requestBody.eventsStartDate ? new Date(requestBody.eventsStartDate) : new Date();
+  let eventsEndDate = new Date();
+  if (!requestBody.eventsEndDate) {
+    const TWO_WEEKS_IN_DAYS = 14;
+    eventsEndDate.setDate(eventsStartDate.getDate() + TWO_WEEKS_IN_DAYS);
+  } else {
+    eventsEndDate = new Date(requestBody.eventsEndDate);
+  }
+
+  res.status(204).end();
+});
