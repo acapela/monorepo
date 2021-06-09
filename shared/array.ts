a1f661eaf80178c610df0785f4e6af48ff891ddb
Adam Pietrasiak <adam@pietrasiak.com>
2021-06-09 10:43:31 +0200

    feat: semi random space gradient colors

--- shared/array.ts
+++ shared/array.ts
@@ -16,3 +16,44 @@ export function isLastItem<T>(array: T[], item: T) {
 
   return index === array.length - 1;
 }
+
+/**
+ * This function will pick 'random' item from array, but in a repetitive way, basing on provided 'seed' string.
+ * As long as the seed is the same, result of 'random' pick will be the same as well.
+ *
+ * It is useful for 'randomized' effects, but in a such way that give the same result every time.
+ */
+export function pickArrayItemWithSeed<T>(array: T[], seed: string) {
+  if (!array.length) return null;
+
+  const seedAsNumber = seed
+    .split("")
+    .map((seedChar) => seedChar.charCodeAt(0))
+    .reduce((nextCharNumber, buffer) => nextCharNumber * buffer, 1);
+
+  const index = seedAsNumber % array.length;
+
+  return array[index];
+}
+
+/**
+ * This function will pick n 'random' items from array, but in a repetitive way, basing on provided 'seed' string.
+ * As long as the seed is the same, result of 'random' pick will be the same as well.
+ *
+ * It is useful for 'randomized' effects, but in a such way that give the same result every time.
+ */
+export function pickNArrayItemsWithSeed<T>(array: T[], countToPick: number, seed: string) {
+  const arrayClone = [...array];
+  const pickedItems: T[] = [];
+
+  Array.from({ length: countToPick }).forEach(() => {
+    const nextItem = pickArrayItemWithSeed(arrayClone, seed);
+
+    if (nextItem === null) return;
+
+    pickedItems.push(nextItem);
+    removeElementFromArray(arrayClone, nextItem);
+  });
+
+  return pickedItems;
+}
--- ui/colors.ts
+++ ui/colors.ts
@@ -4,3 +4,21 @@ export const ACTIVE_COLOR = `hsl(250, 100%, 68%)`;
 export const SUCCESS_COLOR = `hsla(149, 99%, 33%, 1)`;
 export const HIGHLIGHT_COLOR = `hsl(47, 85%, 64%)`;
 export const NOTIFICATION_COLOR = `hsl(352, 92%, 71%)`;
+
+/**
+ * Those colors are in harmony with the rest of our colors, but are not neccessarily part of our design language.
+ *
+ * They might be used eg. for random gradients, avatar backgrounds etc.
+ */
+export const COLORS_PALETTE = {
+  ORANGE_RED_CRAYOLA: "#ff5c5c",
+  MEDIUM_SLATE_BLUE: "#775cff",
+  ORANGE_YELLOW_CRAYOLA: "#ffd275",
+  KEPPEL: "#49beaa",
+  RUSSIAN_VIOLET: "#311847",
+  POWDER_BLUE: "#b8d8d8",
+  MAYA_BLUE: "#5cc8ff",
+  PINK_LAVENDER: "#e6c0e9",
+  RAISIN_BLACK: "#1e1e24",
+  MINT_GREEN: "#82ff9e",
+};
