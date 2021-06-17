ec5748e179c92c064737cdc9b398a6c7e490abc6
Adam Pietrasiak <adam@pietrasiak.com>
2021-06-17 12:45:10 +0200

    chore: adjusted prettier formatting

--- backend/src/events/eventHandlers.ts
+++ backend/src/events/eventHandlers.ts
@@ -1,13 +1,12 @@
 import { Message, Room, Space, Team, TeamInvitation, Topic, User } from "~db";
 import { createHasuraEventsHandler } from "../hasura/events";
 
-export const hasuraEvents =
-  createHasuraEventsHandler<{
-    message_updates: Message;
-    room_updates: Room;
-    space_updates: Space;
-    team_invitation_updates: TeamInvitation;
-    team_updates: Team;
-    topic_updates: Topic;
-    user_updates: User;
-  }>();
+export const hasuraEvents = createHasuraEventsHandler<{
+  message_updates: Message;
+  room_updates: Room;
+  space_updates: Space;
+  team_invitation_updates: TeamInvitation;
+  team_updates: Team;
+  topic_updates: Topic;
+  user_updates: User;
+}>();
