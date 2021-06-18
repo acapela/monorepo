099c0eef7bc5555928f2dc4120a9023bb4b437be
Christoph Witzko <github@christophwitzko.com>
2021-06-18 16:54:27 +0200

    feat: add lookup_team_name action handler

--- backend/src/actions/actionHandlers.ts
+++ backend/src/actions/actionHandlers.ts
@@ -1,10 +1,11 @@
 import { acceptInvite } from "../teamInvitation/acceptInvite";
 import { getUploadUrl, getDownloadUrl } from "../attachments/attachments";
+import { lookupTeamName } from "../teamInvitation/lookupTeamName";
 
 export interface ActionHandler<DataT, ResponseT> {
   actionName: string;
-  handle: (userId: string, data: DataT) => Promise<ResponseT>;
+  handle: (userId: string | undefined, data: DataT) => Promise<ResponseT>;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const handlers: ActionHandler<any, any>[] = [acceptInvite, getUploadUrl, getDownloadUrl];
+export const handlers: ActionHandler<any, any>[] = [acceptInvite, getUploadUrl, getDownloadUrl, lookupTeamName];
---
+++ backend/src/teamInvitation/lookupTeamName.ts
@@ -0,0 +1,45 @@
+import { validate as validateUuid } from "uuid";
+import { db } from "~db";
+import { ActionHandler } from "../actions/actionHandlers";
+import { NotFoundError, UnprocessableEntityError } from "../errors";
+import { getInviterName } from "./events";
+
+export interface LookupTeamNameActionInputs {
+  token: string;
+}
+
+export const lookupTeamName: ActionHandler<
+  LookupTeamNameActionInputs,
+  {
+    team_name: string;
+    inviter_name: string;
+  }
+> = {
+  actionName: "lookup_team_name",
+
+  async handle(userId, { token }) {
+    if (!validateUuid(token)) {
+      throw new UnprocessableEntityError("malformed invite code");
+    }
+
+    const teamInvitation = await db.team_invitation.findFirst({
+      where: {
+        token,
+        used_by_user_id: null,
+      },
+      include: {
+        team: true,
+        user: true,
+      },
+    });
+
+    if (!teamInvitation) {
+      throw new NotFoundError("invalid invite token");
+    }
+
+    return {
+      team_name: teamInvitation.team.name,
+      inviter_name: getInviterName(teamInvitation.user),
+    };
+  },
+};
