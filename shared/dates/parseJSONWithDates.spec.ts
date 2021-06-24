05ab39affd3b4f5dfc4d392a5928f0e0e72c981d
Adam Pietrasiak <adam@pietrasiak.com>
2021-06-24 16:36:02 +0200

    fix: only parse JSON or ISO dates in JSON parser

---
+++ shared/dates/parseJSONWithDates.spec.ts
@@ -0,0 +1,37 @@
+import { parseJsonWithDates } from "./parseJSONWithDates";
+
+describe("parseJsonWithDates", () => {
+  it("properly parses dates in object", () => {
+    const input = {
+      foo: new Date(),
+      bar: [new Date()],
+    };
+
+    const json = JSON.stringify(input);
+
+    expect(parseJsonWithDates(json)).toEqual(input);
+  });
+
+  const shouldNotParse = [1624544210021, "1624544210021", "2020-01-01", "1-1"];
+
+  shouldNotParse.forEach((shouldNotParseInput) => {
+    it(`will not parse date like, bot non ISO dates (${JSON.stringify(shouldNotParseInput)})`, () => {
+      const testJSON = {
+        value: shouldNotParseInput,
+      };
+
+      const json = JSON.stringify(testJSON);
+
+      const parsed = parseJsonWithDates<{ value: unknown }>(json);
+
+      expect(typeof shouldNotParseInput).toEqual(typeof parsed.value);
+    });
+  });
+
+  it("should parse ISO string", () => {
+    const testDate = new Date(2020, 0, 0, 0, 0, 0, 0);
+    const isoString = "2019-12-30T23:00:00.000Z";
+
+    expect(parseJsonWithDates<Date>(JSON.stringify(isoString))).toEqual(testDate);
+  });
+});
--- shared/dates/parseJSONWithDates.ts
+++ shared/dates/parseJSONWithDates.ts
@@ -1,3 +1,29 @@
+import { parseISO } from "date-fns";
+
+/**
+ * 2013-10-21T13:28:06.419Z >>> 24 chars
+ * 2012-04-21T18:25:43-05:00 (ISO 8601) >>> 25 chars
+ */
+const JSON_DATE_STRING_MIN_LENGTH = 24;
+
+function tryParseISOStringDate<T>(input: T): Date | T {
+  if (typeof input !== "string") return input;
+
+  if (input.length < JSON_DATE_STRING_MIN_LENGTH) return input;
+
+  try {
+    const date = parseISO(input);
+
+    if (isNaN(date.getTime())) {
+      return input;
+    }
+
+    return date;
+  } catch (error) {
+    return input;
+  }
+}
+
 export function tryParseStringDate(originalValue: unknown) {
   if (typeof originalValue !== "string") {
     return originalValue;
@@ -13,7 +39,7 @@ export function tryParseStringDate(originalValue: unknown) {
 }
 
 function jsonRetriever(key: string, originalValue: unknown) {
-  return tryParseStringDate(originalValue);
+  return tryParseISOStringDate(originalValue);
 }
 
 export function parseJsonWithDates<T>(input: string): T {
