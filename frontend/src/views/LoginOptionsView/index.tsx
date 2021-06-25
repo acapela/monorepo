7bc4b9a84b3c95b7a2eac0d74b277268b20b0174
Heiki Riesenkampf <me@heikir.com>
2021-06-25 13:24:00 +0300

    chore: remove login withg email option

--- frontend/src/views/LoginOptionsView/index.tsx
+++ frontend/src/views/LoginOptionsView/index.tsx
@@ -1,12 +1,9 @@
-import { EmailLoginButton } from "~frontend/authentication/EmailLoginButton";
 import { GoogleLoginButton } from "~frontend/authentication/GoogleLoginButton";
 
 export function LoginOptionsView() {
   return (
     <>
       <GoogleLoginButton />
-      &nbsp;
-      <EmailLoginButton />
     </>
   );
 }
