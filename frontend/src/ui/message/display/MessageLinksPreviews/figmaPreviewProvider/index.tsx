48feaf6d647eb84c357ab352d70242c3d3e7e1fb
Rodion Chachura <geekrodion@gmail.com>
2021-06-29 15:40:43 +0400

    simplify Figma's PreviewComponent

--- frontend/src/ui/message/display/MessageLinksPreviews/figmaPreviewProvider/index.tsx
+++ frontend/src/ui/message/display/MessageLinksPreviews/figmaPreviewProvider/index.tsx
@@ -7,9 +7,5 @@ const PREVIEW_RATIO = 800 / 450;
 
 export const figmaPreviewProvider: MessageEmbedPreviewConfig = {
   isUrlSupported: (url) => url.includes("figma.com/file"),
-  PreviewComponent: ({ url }) => {
-    const embedUrl = getFigmaEmbedUrl(url);
-
-    return <MessageLinkPreviewIFrame ratio={PREVIEW_RATIO} url={embedUrl} />;
-  },
+  PreviewComponent: ({ url }) => <MessageLinkPreviewIFrame ratio={PREVIEW_RATIO} url={getFigmaEmbedUrl(url)} />,
 };
