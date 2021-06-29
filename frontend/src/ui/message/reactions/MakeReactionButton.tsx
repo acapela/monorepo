2301438c376822599ee10480c8d04e6ccc0bc1bf
Rodion Chachura <geekrodion@gmail.com>
2021-06-29 12:21:16 +0400

    Don't remove reactions from emoji picker

--- frontend/src/ui/message/reactions/MakeReactionButton.tsx
+++ frontend/src/ui/message/reactions/MakeReactionButton.tsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { AnimatePresence } from "framer-motion";
+import { EmojiData } from "emoji-mart";
 import { IconEmotionSmile } from "~ui/icons";
 import { IconButton } from "~ui/buttons/IconButton";
 import { Popover } from "~ui/popovers/Popover";
@@ -8,7 +9,7 @@ import { EmojiPickerWindow } from "~ui/EmojiPicker/EmojiPickerWindow";
 import { isBaseEmoji } from "~richEditor/EmojiButton";
 import { MessageDetailedInfoFragment } from "~gql";
 import { useAssertCurrentUser } from "~frontend/authentication/useCurrentUser";
-import { addMessageReaction, removeMessageReaction } from "~frontend/gql/reactions";
+import { addMessageReaction } from "~frontend/gql/reactions";
 
 interface Props {
   message: MessageDetailedInfoFragment;
@@ -21,42 +22,36 @@ export const MakeReactionButton = ({ message }: Props) => {
 
   const [isPicking, { set: open, unset: close }] = useBoolean(false);
 
+  const handleEmojiSelect = (emoji: EmojiData) => {
+    if (!isBaseEmoji(emoji)) {
+      console.warn("Custom emojis are not supported");
+      return;
+    }
+
+    close();
+
+    const userAlreadyReacted = message.message_reactions.some(
+      (reaction) => reaction.emoji === emoji.native && reaction.user.id === user.id
+    );
+
+    if (!userAlreadyReacted) {
+      addMessageReaction({
+        input: {
+          emoji: emoji.native,
+          message_id: message.id,
+          user_id: user.id,
+        },
+      });
+    }
+  };
+
   return (
     <>
       <IconButton ref={buttonRef} tooltip="Add reaction" onClick={open} icon={<IconEmotionSmile />} />
       <AnimatePresence>
         {isPicking && (
           <Popover anchorRef={buttonRef} placement="bottom-end">
-            <EmojiPickerWindow
-              onCloseRequest={close}
-              onSelect={(emoji) => {
-                if (!isBaseEmoji(emoji)) {
-                  console.warn("Custom emojis are not supported");
-                  return;
-                }
-
-                close();
-
-                const userAlreadyReacted = message.message_reactions.some(
-                  (reaction) => reaction.emoji === emoji.native && reaction.user.id === user.id
-                );
-                if (userAlreadyReacted) {
-                  removeMessageReaction({
-                    emoji: emoji.native,
-                    messageId: message.id,
-                    userId: user.id,
-                  });
-                } else {
-                  addMessageReaction({
-                    input: {
-                      emoji: emoji.native,
-                      message_id: message.id,
-                      user_id: user.id,
-                    },
-                  });
-                }
-              }}
-            />
+            <EmojiPickerWindow onCloseRequest={close} onSelect={handleEmojiSelect} />
           </Popover>
         )}
       </AnimatePresence>
