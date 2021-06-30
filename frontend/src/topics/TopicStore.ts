20d6b775ac404d99ef1c539914b159fb11557bef
Adam Pietrasiak <adam@pietrasiak.com>
2021-06-30 13:05:50 +0200

    feat: updates shared state to allow using selectors

--- frontend/src/topics/TopicStore.ts
+++ frontend/src/topics/TopicStore.ts
@@ -3,8 +3,11 @@ import { createSharedStateContext } from "~shared/sharedState";
 
 interface State {
   currentlyReplyingToMessage: MessageBasicInfoFragment | null;
+  editedMessageId: string | null;
 }
 
-export const [TopicStoreContext, useTopicStore] = createSharedStateContext<State>(() => ({
-  currentlyReplyingToMessage: null,
-}));
+export const [TopicStoreContext, { useSharedState: useTopicStore, useSharedStateSelector: useTopicStoreSelector }] =
+  createSharedStateContext<State>(() => ({
+    currentlyReplyingToMessage: null,
+    editedMessageId: null,
+  }));
