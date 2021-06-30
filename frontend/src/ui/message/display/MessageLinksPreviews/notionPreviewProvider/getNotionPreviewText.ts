088fbc2190f91f7045a3b9c94d1bad188fe9892a
Rodion Chachura <geekrodion@gmail.com>
2021-06-30 16:34:50 +0400

    remove try-catch from getNotionPreviewText

--- frontend/src/ui/message/display/MessageLinksPreviews/notionPreviewProvider/getNotionPreviewText.ts
+++ frontend/src/ui/message/display/MessageLinksPreviews/notionPreviewProvider/getNotionPreviewText.ts
@@ -1,12 +1,7 @@
 export const getNotionPreviewText = (url: string) => {
-  try {
-    const [pageNameWithId] = url.split("/").reverse();
+  const [pageNameWithId] = url.split("/").reverse();
 
-    const previewText = pageNameWithId.split("-").slice(0, -1).join(" ");
+  const previewText = pageNameWithId.split("-").slice(0, -1).join(" ");
 
-    return previewText;
-  } catch (err) {
-    console.error(`Failed to parse Notion URL: ${url}`);
-    return null;
-  }
+  return previewText;
 };
