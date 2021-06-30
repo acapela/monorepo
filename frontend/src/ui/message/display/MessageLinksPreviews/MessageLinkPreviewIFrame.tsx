88176fca56addbe0be9c576da366d58f4ad3fee1
Rodion Chachura <geekrodion@gmail.com>
2021-06-30 16:25:52 +0400

    use numeric value for ratio

--- frontend/src/ui/message/display/MessageLinksPreviews/MessageLinkPreviewIFrame.tsx
+++ frontend/src/ui/message/display/MessageLinksPreviews/MessageLinkPreviewIFrame.tsx
@@ -3,16 +3,16 @@ import styled from "styled-components";
 
 interface Props {
   url: string;
-  ratio: string;
+  ratio: number;
 }
 
 export const MessageLinkPreviewIFrame = ({ url, ratio }: Props) => {
   return <UIPreview ratio={ratio} src={url} allow="fullscreen" />;
 };
 
-const UIPreview = styled.iframe<{ ratio: string }>`
+const UIPreview = styled.iframe<{ ratio: number }>`
   width: 100%;
-  aspect-ratio: ${(props) => props.ratio};
+  aspect-ratio: ${(props) => props.ratio} / 1;
 
   @supports not (aspect-ratio: auto) {
     min-height: 320px;
