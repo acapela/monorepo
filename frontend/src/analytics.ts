0b885c55419ebef6ce031f75248314e7139cbb46
Heiki Riesenkampf <me@heikir.com>
2021-07-07 16:46:48 +0200

    chore: added more user activity events and comments

--- frontend/src/analytics.ts
+++ frontend/src/analytics.ts
@@ -5,16 +5,32 @@ export type UserProfile = {
 };
 
 type trackingEvent =
-  | "Account Created"
+  | "Account Created" // team created
   | "Account Deleted"
   | "Signed Up"
   | "Signed In"
   | "Signed Out"
-  | "Invite Sent"
-  | "Account Added User"
+  | "Invite Sent" // invited a new team mate
+  | "Account Added User" // invitation accepted
   | "Account Removed User"
-  | "Trial Started"
-  | "Trial Ended";
+  | "Trial Started" // same as team created, will be changed later
+  | "Trial Ended"
+  | "Created Space"
+  | "Joined Space"
+  | "Left Space"
+  | "Deleted Space"
+  | "Created Room"
+  | "Joined Room"
+  | "Left Room"
+  | "Closed Room"
+  | "Deleted Room"
+  | "Made Room Public"
+  | "Made Room Private"
+  | "Created Topic"
+  | "Closed Topic"
+  | "Added New Message" // should track type of message (video, text, audio, files)
+  | "Edited Message"
+  | "Deleted Message";
 
 export function identifyUser(userId: string, userProfile: UserProfile) {
   if (window.analytics) {
