473cf3d903f8dbbb19db62c820cb466182729c3c
Heiki Riesenkampf <me@heikir.com>
2021-07-07 10:47:00 +0200

    chore: add strict typing to analytics package

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -1,11 +1,13 @@
-// Ensure this is treated as a module.
-export {};
+import { SegmentAnalytics } from "@segment/analytics.js-core";
 
 declare global {
   interface Window {
     Userback: {
       access_token: string;
     };
-    analytics: any;
+    analytics: SegmentAnalytics.AnalyticsJS;
   }
 }
+
+// Ensure this is treated as a module.
+export {};
