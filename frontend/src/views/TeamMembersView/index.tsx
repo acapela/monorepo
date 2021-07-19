2f8c0dcd31bb3d26b592bf7aa1747b4415dd5ea6
Rodion Chachura <geekrodion@gmail.com>
2021-07-19 14:41:40 +0400

    empty TeamMembersView

---
+++ frontend/src/views/TeamMembersView/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const TeamMembersView = () => {
+  return <UIHolder>TeamMembersView will be here</UIHolder>;
+};
+
+const UIHolder = styled.div`
+  display: flex;
+  justify-content: center;
+
+  min-height: 100%;
+  padding-top: 84px;
+`;
