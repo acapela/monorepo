28bcc0005f91bd3c32afe38be4b8499d670228da
Rodion Chachura <geekrodion@gmail.com>
2021-07-20 11:30:44 +0400

    AddMemberInlineForm component

---
+++ frontend/src/ui/MembersManager/AddMemberInlineForm.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+import { Button } from "~ui/buttons/Button";
+import { IconPlusSquare } from "~ui/icons";
+import { useShortcut } from "~ui/keyboard/useShortcut";
+
+interface Props {
+  input: ReactNode;
+  isValid: boolean;
+  onSubmit: () => void;
+}
+
+export const AddMemberInlineForm = ({ input, isValid, onSubmit }: Props) => {
+  useShortcut("Enter", () => {
+    if (!isValid) return;
+
+    onSubmit();
+  });
+
+  return (
+    <UIHolder>
+      {input}
+      <Button iconPosition="start" icon={<IconPlusSquare />} onClick={onSubmit} isDisabled={!isValid}>
+        Add Member
+      </Button>
+    </UIHolder>
+  );
+};
+
+const UIHolder = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 16px;
+`;
--- frontend/src/views/TeamMembersView/index.tsx
+++ frontend/src/views/TeamMembersView/index.tsx
@@ -3,19 +3,25 @@ import { MembersManagerContainer } from "~frontend/ui/MembersManager/MembersMana
 import { useCurrentTeamDetails } from "~frontend/gql/teams";
 import { MembersContainer } from "~frontend/ui/MembersManager/MembersContainer";
 import { MemberItem } from "~frontend/ui/MembersManager/MemberItem";
+import { AddMemberInlineForm } from "~frontend/ui/MembersManager/AddMemberInlineForm";
 
 export const TeamMembersView = () => {
   const [team] = useCurrentTeamDetails();
 
   const teamMembers = team?.memberships.map((membership) => membership.user) ?? [];
 
+  const handleRemoveTeamMember = (userId: string) => {
+    console.log("remove team member: ", userId);
+  };
+
   return (
     <UIHolder>
       <MembersManagerContainer title="Team members">
+        <AddMemberInlineForm input={null} isValid={false} onSubmit={console.log} />
         {teamMembers.length > 0 && (
           <MembersContainer>
             {teamMembers.map((user) => (
-              <MemberItem key={user.id} user={user} onRemove={console.log} />
+              <MemberItem key={user.id} user={user} onRemove={() => handleRemoveTeamMember(user.id)} />
             ))}
           </MembersContainer>
         )}
