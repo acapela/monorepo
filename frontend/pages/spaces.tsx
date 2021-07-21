71743de44a10ce8f729e3ac0794ed2582c3038fe
Adam Pietrasiak <adam@pietrasiak.com>
2021-07-21 20:57:21 +0200

    feat: add page titles for various pages (#258)

--- frontend/pages/spaces.tsx
+++ frontend/pages/spaces.tsx
@@ -1,10 +1,16 @@
 import { withServerSideAuthRedirect } from "~frontend/authentication/withServerSideAuthRedirect";
 import { AppLayout } from "~frontend/layouts/AppLayout";
 import { assignPageLayout } from "~frontend/utils/pageLayout";
+import { PageMeta } from "~frontend/utils/PageMeta";
 import { SpacesView } from "~frontend/views/SpacesView";
 
 export default function SpacesPage(): JSX.Element {
-  return <SpacesView />;
+  return (
+    <>
+      <PageMeta title="Spaces" />
+      <SpacesView />
+    </>
+  );
 }
 
 export const getServerSideProps = withServerSideAuthRedirect();
