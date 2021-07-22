3078926e3712340ea8f75499cfb0f20d411f4ded
Rodion Chachura <geekrodion@gmail.com>
2021-07-22 11:26:48 +0400

    refactoring: use SpacedAppLayoutContainer on the team page

--- frontend/src/layouts/AppLayout/SpacedAppLayoutContainer.tsx
+++ frontend/src/layouts/AppLayout/SpacedAppLayoutContainer.tsx
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 import { Container } from "~ui/layout/Container";
 
-export const SpacedAppLayoutContainer = styled(Container)`
-  padding-top: 32px;
+type TopSpazeSize = "medium" | "large";
+
+interface Props {
+  topSpaceSize?: TopSpazeSize;
+}
+
+export const SpacedAppLayoutContainer = styled(Container)<Props>`
+  padding-top: ${({ topSpaceSize = "medium" }) =>
+    ({
+      medium: "32px",
+      large: "80px",
+    }[topSpaceSize])};
   padding-bottom: 32px;
 `;
--- frontend/src/views/TeamMembersView/index.tsx
+++ frontend/src/views/TeamMembersView/index.tsx
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 import { CurrentTeamMembersManager } from "./CurrentTeamMembersManager";
 import { useCurrentUser } from "~frontend/authentication/useCurrentUser";
+import { SpacedAppLayoutContainer } from "~frontend/layouts/AppLayout/SpacedAppLayoutContainer";
 
 export const TeamMembersView = () => {
   const user = useCurrentUser();
 
-  return <UIHolder>{user?.currentTeamId && <CurrentTeamMembersManager />}</UIHolder>;
+  if (!user?.currentTeamId) return null;
+
+  return (
+    <SpacedAppLayoutContainer topSpaceSize="large">
+      <UIHolder>
+        <CurrentTeamMembersManager />
+      </UIHolder>
+    </SpacedAppLayoutContainer>
+  );
 };
 
 const UIHolder = styled.div`
   display: flex;
   justify-content: center;
   align-items: start;
-
-  min-height: 100%;
-  padding-top: 84px;
 `;
