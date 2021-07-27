5165d1c1aca698cac8bee6b8671519437570e23b
Rodion Chachura <geekrodion@gmail.com>
2021-07-27 12:06:41 +0400

    refactoring: delete unused room_invites table (#267)

--- frontend/src/gql/invitations.ts
+++ frontend/src/gql/invitations.ts
@@ -1,38 +1,7 @@
 import { gql } from "@apollo/client";
-import {
-  AcceptInviteMutation,
-  AcceptInviteMutationVariables,
-  CreateInviteMutation,
-  CreateInviteMutationVariables,
-  LookupTeamNameQuery,
-  LookupTeamNameQueryVariables,
-} from "~gql";
+import { LookupTeamNameQuery, LookupTeamNameQueryVariables } from "~gql";
 
-import { createMutation, createQuery } from "./utils";
-
-export const [useCreateInviteMutation] = createMutation<CreateInviteMutation, CreateInviteMutationVariables>(
-  () => gql`
-    mutation CreateInvite($email: String!, $roomId: uuid) {
-      invite: insert_room_invites_one(object: { email: $email, room_id: $roomId }) {
-        id
-        email
-        usedAt: used_at
-      }
-    }
-  `
-);
-
-export const [useAcceptInviteMutation] = createMutation<AcceptInviteMutation, AcceptInviteMutationVariables>(
-  () => gql`
-    mutation AcceptInvite($token: String!) {
-      invite: accept_invite(token: $token) {
-        team {
-          id
-        }
-      }
-    }
-  `
-);
+import { createQuery } from "./utils";
 
 export const [lookupTeamName] = createQuery<LookupTeamNameQuery, LookupTeamNameQueryVariables>(
   () => gql`
