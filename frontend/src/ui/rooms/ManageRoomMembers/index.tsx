f71524f9454612e4ff9c80358b8019e24a14929c
Rodion Chachura <geekrodion@gmail.com>
2021-07-28 12:24:07 +0400

    chore: remove MembersManager component

--- frontend/src/ui/rooms/ManageRoomMembers/index.tsx
+++ frontend/src/ui/rooms/ManageRoomMembers/index.tsx
@@ -1,9 +1,17 @@
-import { MembersManager } from "~frontend/ui/MembersManager";
+import styled from "styled-components";
+import { AnimatePresence } from "framer-motion";
 import { useAddRoomMemberMutation, isCurrentUserRoomMember, useRemoveRoomMemberMutation } from "~frontend/gql/rooms";
 import { useCurrentUser } from "~frontend/authentication/useCurrentUser";
 import { assertDefined } from "~shared/assert";
 import { RoomDetailedInfoFragment } from "~gql";
 import { openLastPrivateRoomMemberDeletionPrompt } from "./openLastPrivateRoomMemberDeletionPrompt";
+import { useBoolean } from "~shared/hooks/useBoolean";
+import { MembersManagerModal } from "~frontend/ui/MembersManager/MembersManagerModal";
+import { handleWithStopPropagation } from "~shared/events";
+import { AvatarList } from "~frontend/ui/users/AvatarList";
+import { CircleIconButton } from "~ui/buttons/CircleIconButton";
+import { IconPlus } from "~ui/icons";
+import { JoinToggleButton } from "~frontend/ui/buttons/JoinToggleButton";
 
 interface Props {
   room: RoomDetailedInfoFragment;
@@ -40,13 +48,66 @@ export const ManageRoomMembers = ({ room, onCurrentUserLeave }: Props) => {
     }
   }
 
+  const [isPickingUser, { set: openUserPicker, unset: closeUserPicker }] = useBoolean(false);
+
   return (
-    <MembersManager
-      title="Invite your team to this room"
-      isReadonly={!amIMember}
-      users={members}
-      onAddMemberRequest={handleJoin}
-      onRemoveMemberRequest={handleLeave}
-    />
+    <>
+      <AnimatePresence>
+        {isPickingUser && (
+          <MembersManagerModal
+            title={"Invite your team to this room"}
+            currentUsers={members}
+            onCloseRequest={closeUserPicker}
+            onAddUser={handleJoin}
+            onRemoveUser={handleLeave}
+          />
+        )}
+      </AnimatePresence>
+      <UIHolder>
+        <UIMembers onClick={handleWithStopPropagation(openUserPicker)}>
+          {members.length > 0 && <AvatarList users={members} size="inherit" />}
+          {amIMember && (
+            <CircleIconButton
+              kind="primary"
+              onClick={handleWithStopPropagation(openUserPicker)}
+              icon={<IconPlus />}
+              size="inherit"
+              tooltip="Manage members"
+            />
+          )}
+        </UIMembers>
+        <UIActions>
+          {currentUser && (
+            <JoinToggleButton
+              isMember={amIMember}
+              onJoin={() => handleJoin(currentUser.id)}
+              onLeave={() => handleLeave(currentUser.id)}
+            />
+          )}
+        </UIActions>
+      </UIHolder>
+    </>
   );
 };
+
+const UIHolder = styled.div`
+  margin-top: 4px;
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const UIMembers = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 4px;
+  align-items: center;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+const UIActions = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
