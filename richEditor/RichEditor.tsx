65b0708feb8bd7518afca491fa6a0c537c912d44
Adam Pietrasiak <adam@pietrasiak.com>
2021-07-28 15:30:17 +0200

    fix: fixed bug causing autofocus to put cursor outside of editor selectable content

--- richEditor/RichEditor.tsx
+++ richEditor/RichEditor.tsx
@@ -43,6 +43,56 @@ export interface RichEditorProps {
   onEditorReady?: (editor: Editor) => void;
 }
 
+/**
+ * This is a bit tricky.
+ *
+ * It seems like this function should not be needed and editor.chain().focus('end') should be enough.
+ *
+ * Turns out 'end' focus puts cursor at content.size position, which is 'after' selectable area which I don't fully understand.
+ *
+ * Example:
+ * Creating some complex content (eg bullet lists) you might do 2 things:
+ * - call content.size and also manually (by mouse or keyboard) put cursor at the end of visible content and then measure
+ * cursor position.
+ *
+ * Turns out those are different numbers. content.size is always bigger. I think it is a bug in tiptap as looking at
+ * focus('end') implementation it is exactly what happens (pseudo code: focusAt(content.size)).
+ *
+ * This leads to bunch of weird behaviors eg. if you have 1 line content, focus('end') and then press backspace twice,
+ * it'll first select entire content and then remove your entire content.
+ *
+ * So long story short - this function will get last cursor position that is able to resolve to actual node.
+ */
+function getLastSelectableCursorPosition(editor: Editor) {
+  // Let's get content size and start from there looking for first 'selectable' cursor position
+  const contentSize = editor.state.doc.content.size;
+
+  let cursorPosition = contentSize;
+
+  // Start looking until will reach 'start' position.
+  while (cursorPosition > 1) {
+    // Let's check if there is an actual node at this cursor position
+    const node = editor.state.doc.nodeAt(cursorPosition);
+
+    // If there is - set position of cursor 'just after' this node.
+    if (node) {
+      return cursorPosition + 1;
+    }
+
+    // Keep looking
+    cursorPosition--;
+  }
+
+  // We've reached 'start' position so let's return it.
+  return 1;
+}
+
+function getFocusEditorAtEndCommand(editor: Editor) {
+  const lastSelectablePosition = getLastSelectableCursorPosition(editor);
+
+  return editor.chain().focus(lastSelectablePosition);
+}
+
 export const RichEditor = forwardRef<Editor, RichEditorProps>(function RichEditor(
   {
     value = getEmptyRichContent(),
@@ -66,10 +116,13 @@ export const RichEditor = forwardRef<Editor, RichEditorProps>(function RichEdito
       new Editor({
         extensions: finalExtensions,
         content: value,
-        enableInputRules: true,
       })
   );
   const forceUpdate = useUpdate();
+
+  function getFocusAtEndCommand() {
+    return getFocusEditorAtEndCommand(editor);
+  }
 
   useEffect(() => {
     editor.on("transaction", forceUpdate);
@@ -118,10 +171,8 @@ export const RichEditor = forwardRef<Editor, RichEditorProps>(function RichEdito
   useEffect(() => {
     if (!editor || !autofocusKey) return;
 
-    console.log("elomelofocus");
-
     return createTimeout(() => {
-      editor.chain?.().focus("end").run();
+      getFocusAtEndCommand().run();
     }, 0);
   }, [autofocusKey, editor]);
 
@@ -153,9 +204,7 @@ export const RichEditor = forwardRef<Editor, RichEditorProps>(function RichEdito
 
     if (!didChange) return;
 
-    editor.setOptions({ content: value });
-
-    // editor?.chain().focus().setContent(value).run();
+    getFocusAtEndCommand().setContent(value).run();
   }, [value]);
 
   /**
@@ -203,7 +252,7 @@ export const RichEditor = forwardRef<Editor, RichEditorProps>(function RichEdito
        * TLDR: only focusing the editor was enough - tiptap did capture keyboard event in such case and did properly
        * insert pressed key into the content.
        */
-      editor?.chain().focus("end").run();
+      getFocusAtEndCommand().run();
       return true;
     },
     { isEnabled: !isFocused && !isDisabled }
