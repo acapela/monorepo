832bb2ef593b5b96f4b3651d2724109ba9a07bfb
Gregor <gregorwbr@gmail.com>
2021-07-29 11:09:27 +0200

    Fix AutoScroll (#277)
    * fix: always scroll to last message if scrolled to bottom
    
    * fix: prevent auto-scroll while editing
    
    * refactor: remove useEventListener in favor of useElementEvent

--- frontend/src/views/topic/ScrollableMessages.tsx
+++ frontend/src/views/topic/ScrollableMessages.tsx
@@ -1,7 +1,10 @@
-import React, { ReactNode, useRef } from "react";
+import React, { ReactNode, useCallback, useRef } from "react";
 import styled from "styled-components";
 import { ScrollToBottomMonitor } from "./ScrollToBottomMonitor";
-import { getDidUserJustInteract } from "~shared/interaction/isUserInteracting";
+import { assertDefined } from "~shared/assert";
+import { useTopicStoreContext } from "~frontend/topics/TopicStore";
+import { select } from "~shared/sharedState";
+import { useElementEvent } from "~shared/domEvents";
 
 interface Props {
   children: ReactNode;
@@ -10,21 +13,22 @@ interface Props {
 
 export const ScrollableMessages = styled(({ children, className }: Props) => {
   const holderRef = useRef<HTMLDivElement>(null);
+  const isScrolledToBottom = useRef(true);
 
+  const handleScroll = useCallback(() => {
+    const parent = assertDefined(holderRef.current, "Event can't have been called on deleted element");
+    isScrolledToBottom.current = parent.scrollTop == parent.scrollHeight - parent.clientHeight;
+  }, []);
+  useElementEvent(holderRef, "scroll", handleScroll);
+
+  const topicContext = useTopicStoreContext();
+  const isInEditMode = select(() => !!topicContext.editedMessageId);
+
+  const getShouldScroll = useCallback(() => isScrolledToBottom.current && !isInEditMode, [isInEditMode]);
   return (
     <UIHolder className={className} ref={holderRef}>
       <UIInner>
-        <ScrollToBottomMonitor
-          parentRef={holderRef}
-          getShouldScroll={() => {
-            if (getDidUserJustInteract()) {
-              return false;
-            }
-
-            return true;
-          }}
-        />
-
+        <ScrollToBottomMonitor parentRef={holderRef} getShouldScroll={getShouldScroll} />
         {children}
       </UIInner>
     </UIHolder>
