d4f138e837c295f2f674d9b087342270975e3bd7
Adam Pietrasiak <adam@pietrasiak.com>
2021-07-29 12:42:13 +0200

    feat: added ui for new types of notifications

--- frontend/src/ui/notifications/NotificationLabel/NotificationLabel.tsx
+++ frontend/src/ui/notifications/NotificationLabel/NotificationLabel.tsx
@@ -9,6 +9,7 @@ import {
   NotificationTypesMap,
 } from "~shared/notifications/types";
 import { NotificationPlainLabel } from "./NotificationPlainLabel";
+import { useSingleRoomQuery } from "~frontend/gql/rooms";
 
 interface Props {
   notification: NotificationInfoFragment;
@@ -17,6 +18,31 @@ interface Props {
 interface NotificationTypeComponentProps<T extends NotificationType> {
   notification: NotificationInfoFragment;
   payload: NotificationTypesMap[T];
+}
+
+export function NotificationLabel({ notification }: Props) {
+  const notificationData = notification.data as AnyNotificationData;
+  if (isNotificationDataOfType(notificationData, "topicMention")) {
+    return <MentionNotificationLabel notification={notification} payload={notificationData.payload} />;
+  }
+
+  if (isNotificationDataOfType(notificationData, "topicClosed")) {
+    return <TopicClosedNotificationLabel notification={notification} payload={notificationData.payload} />;
+  }
+
+  if (isNotificationDataOfType(notificationData, "addedToTopic")) {
+    return <AddedToTopicClosedNotificationLabel notification={notification} payload={notificationData.payload} />;
+  }
+
+  if (isNotificationDataOfType(notificationData, "roomClosed")) {
+    return <RoomClosedNotificationLabel notification={notification} payload={notificationData.payload} />;
+  }
+
+  if (isNotificationDataOfType(notificationData, "addedToRoom")) {
+    return <AddedToRoomClosedNotificationLabel notification={notification} payload={notificationData.payload} />;
+  }
+
+  return null;
 }
 
 function MentionNotificationLabel({
@@ -46,11 +72,104 @@ function MentionNotificationLabel({
   );
 }
 
-export function NotificationLabel({ notification }: Props) {
-  const notificationData = notification.data as AnyNotificationData;
-  if (isNotificationDataOfType(notificationData, "topicMention")) {
-    return <MentionNotificationLabel notification={notification} payload={notificationData.payload} />;
-  }
+function TopicClosedNotificationLabel({
+  payload: { closedByUserId, topicId },
+  notification,
+}: NotificationTypeComponentProps<"topicClosed">) {
+  const [topic] = useSingleTopicQuery({ id: topicId });
+  const closingUser = useCurrentTeamMember(closedByUserId);
 
-  return null;
+  if (!topic || !closingUser) return null;
+
+  return (
+    <NotificationPlainLabel
+      notification={notification}
+      id={notification.id}
+      date={new Date(notification.created_at)}
+      titleNode={
+        <>
+          <strong>{closingUser.name}</strong> closed the topic <strong>{topic.name}</strong>
+        </>
+      }
+      userId={closedByUserId}
+    />
+  );
+}
+
+function AddedToTopicClosedNotificationLabel({
+  payload: { addedByUserId, topicId },
+  notification,
+}: NotificationTypeComponentProps<"addedToTopic">) {
+  const [topic] = useSingleTopicQuery({ id: topicId });
+  const addedByUser = useCurrentTeamMember(addedByUserId);
+
+  if (!topic || !addedByUser) return null;
+
+  return (
+    <NotificationPlainLabel
+      notification={notification}
+      id={notification.id}
+      date={new Date(notification.created_at)}
+      titleNode={
+        <>
+          <strong>{addedByUser.name}</strong> added you to the topic <strong>{topic.name}</strong>
+        </>
+      }
+      userId={addedByUser.id}
+      onClick={() => {
+        routes.spaceRoomTopic.push({ topicId, spaceId: topic.room.space_id, roomId: topic.room.id });
+      }}
+    />
+  );
+}
+
+function RoomClosedNotificationLabel({
+  payload: { closedByUserId, roomId },
+  notification,
+}: NotificationTypeComponentProps<"roomClosed">) {
+  const [room] = useSingleRoomQuery({ id: roomId });
+  const closingUser = useCurrentTeamMember(closedByUserId);
+
+  if (!room || !closingUser) return null;
+
+  return (
+    <NotificationPlainLabel
+      notification={notification}
+      id={notification.id}
+      date={new Date(notification.created_at)}
+      titleNode={
+        <>
+          <strong>{closingUser.name}</strong> closed the room <strong>{room.name}</strong>
+        </>
+      }
+      userId={closedByUserId}
+    />
+  );
+}
+
+function AddedToRoomClosedNotificationLabel({
+  payload: { roomId, addedByUserId },
+  notification,
+}: NotificationTypeComponentProps<"addedToRoom">) {
+  const [room] = useSingleRoomQuery({ id: roomId });
+  const addingUser = useCurrentTeamMember(addedByUserId);
+
+  if (!room || !addingUser) return null;
+
+  return (
+    <NotificationPlainLabel
+      notification={notification}
+      id={notification.id}
+      date={new Date(notification.created_at)}
+      titleNode={
+        <>
+          <strong>{addingUser.name}</strong> added you to the room <strong>{room.name}</strong>
+        </>
+      }
+      userId={addedByUserId}
+      onClick={() => {
+        routes.spaceRoom.push({ spaceId: room.space_id, roomId: room.id });
+      }}
+    />
+  );
 }
