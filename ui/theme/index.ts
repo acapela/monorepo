b83d77e2b146d7c92b6bedb7f59270b7fd669e53
Omar Duarte <omarduarte@gmail.com>
2021-07-29 11:44:45 +0300

    refactor: move theme proxy to own file

--- ui/theme/index.ts
+++ ui/theme/index.ts
@@ -1,5 +1,3 @@
-import { get, isFunction, isPlainObject } from "lodash";
-import DeepProxy from "proxy-deep";
 import { FlattenSimpleInterpolation } from "styled-components";
 import { borderRadius, shadow } from "~ui/baseStyles";
 import { spacer } from "~ui/spacer";
@@ -8,7 +6,7 @@ import { zIndex } from "~ui/zIndex";
 import { ActionStateInterpolations, variantToStyles } from "./actions/styleBuilder";
 import { getColorTheme, ThemeColorScheme, ThemeColorSchemeName } from "./colors";
 import { Font, font } from "./font";
-import { getIsTerminal } from "./proxy/nonTerminal";
+import { buildThemeProxy } from "./proxy";
 
 export type Variant = "primary" | "secondary" | "tertiary";
 
@@ -58,64 +56,5 @@ export function getTheme(colorScheme: ThemeColorSchemeName): Theme {
   } as const;
 }
 
-/*
- * Creates an access layer that allows us to easily use the theme provided to the styled-components context.
- * It looks for all of the leafs in a provided object of type Theme and proxies them to a function that returns the value
- * as found in the styled-components context.
- *
- * Before:
- * const UIComponent = styled.div`
- *   background: ${props => props.theme.colors.status.error};
- * `;
- *
- * After:
- * const UIComponent = styled.div`
- *   // `theme.colors.status.error` returns `props => props.theme.colors.status.error`
- *   background: ${theme.colors.status.error};
- * `;
- *
- */
-export const theme = new DeepProxy(defaultTheme, {
-  get(target, propertyName, receiver) {
-    const value = Reflect.get(target, propertyName, receiver);
-
-    if (isPlainObject(value)) {
-      // Creates a nested DeepProxy
-      // Calling nest with the value allows DeepProxy to build the `path` (👀 this.path) to nested props
-
-      return this.nest(value);
-    }
-
-    if (isFunction(value)) {
-      /* 
-      There's functions inside the theme tree that are used inside chainable/builder patterns.
-      These functions should return a chainable/builder object instead of being proxied.
-
-      Since these are exceptional, they should be marked as non terminal.
-      Now: `style.font.withExceptionalSize("123px").spezia.build` still works
-      */
-      if (!getIsTerminal(value)) {
-        return (...args: unknown[]) => {
-          return value.call(target, ...args);
-        };
-      }
-
-      /* 
-      First function returned is the outer layer of the utility function that's being called. 
-      We use this to grab the arguments from that util and pass them as a closure to the equivalent theme function.
-
-      This way:
-      `style.utilityFunction(1,2,3)` becomes `props => props.theme.utilityFunction(1,2,3)`
-      */
-      return (...args: unknown[]) =>
-        (props: { theme: Theme }) => {
-          const fn = get(props.theme, [...this.path, propertyName]);
-          return fn.call(target, ...args);
-        };
-    }
-
-    return (props: { theme: Theme }) => {
-      return get(props.theme, [...this.path, propertyName]);
-    };
-  },
-});
+// 👀 buildThemeProxy for docs on how to use
+export const theme = buildThemeProxy(defaultTheme);
