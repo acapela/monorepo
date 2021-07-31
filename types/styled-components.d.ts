3e0aedaa95cec171727f1c3b4388c76291549071
Adam Pietrasiak <adam@pietrasiak.com>
2021-07-31 16:29:46 +0200

    chore: comments

--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -4,7 +4,7 @@
  * For some reason our VS IDE is not type-checking d.ts files here. I tried 'includes' and 'excludes' tsconfig part, but
  * without success.
  *
- * To avoid digging to deep, I defined this module in normal `.ts` file next to this file.
+ * To avoid digging too deep, Actual module typings are in normal `styled-components.ts` file next to this file.
  *
  * This file is still needed as only `d.ts` files can define 'new typings' (.ts files can only extend existing and typed modules)
  *
