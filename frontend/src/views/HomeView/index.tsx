98d0c8fe43230121237e875986f8951f81f20bfe
Omar Duarte <omarduarte@gmail.com>
2021-08-01 23:54:22 +0300

    feat: more padding and sort by latest activities

--- frontend/src/views/HomeView/index.tsx
+++ frontend/src/views/HomeView/index.tsx
@@ -1,21 +1,25 @@
+import { useMemo } from "react";
 import styled from "styled-components";
+import { useAssertCurrentUser } from "~frontend/authentication/useCurrentUser";
 import { SpacedAppLayoutContainer } from "~frontend/layouts/AppLayout/SpacedAppLayoutContainer";
-import { createOpenRoomFilter, createUserFilter } from "~frontend/ui/rooms/filters/factories";
+import {
+  createOpenRoomFilter,
+  createUserFilter,
+  createSortByLatestActivityFilter,
+} from "~frontend/ui/rooms/filters/factories";
 import { useRoomFilterVariables } from "~frontend/ui/rooms/filters/filter";
-import { RoomFilters } from "~frontend/ui/rooms/filters/RoomFilters";
 import { RoomsGroupedByActivities } from "~frontend/ui/rooms/RoomsList";
 import { CreateRoomButton } from "./CreateRoomButton";
-import { useAssertCurrentUser } from "~frontend/authentication/useCurrentUser";
-import { useMemo } from "react";
 
 const openRoomFilter = createOpenRoomFilter(true);
+const sortByLatestActivity = createSortByLatestActivityFilter();
 
 export function HomeView() {
   const user = useAssertCurrentUser();
 
   const currentUserFilter = useMemo(() => createUserFilter(user), [user]);
 
-  const [roomQuery] = useRoomFilterVariables([openRoomFilter, currentUserFilter]);
+  const [roomQuery] = useRoomFilterVariables([openRoomFilter, currentUserFilter, sortByLatestActivity]);
 
   return (
     <UIHolder isNarrow>
@@ -34,13 +38,8 @@ const UIContent = styled.div`
   flex-direction: column;
   align-items: stretch;
 
-  ${RoomFilters} {
-    margin-bottom: 32px;
-    align-self: center;
-  }
-
   ${RoomsGroupedByActivities} {
-    margin-bottom: 32px;
+    margin-top: 40px;
     width: 100%;
   }
 `;
