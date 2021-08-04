b8491f453f39b35bfd3a5b6a0dd2108aed0b6328
Gregor Weber <gregorwbr@gmail.com>
2021-08-04 10:26:29 +0200

    fix: make all trigger drops optional

--- infrastructure/hasura/migrations/1627982813188_drop_refresh_search_triggers/up.sql
+++ infrastructure/hasura/migrations/1627982813188_drop_refresh_search_triggers/up.sql
@@ -1,8 +1,8 @@
-DROP TRIGGER refresh_search ON room;
-DROP TRIGGER refresh_search ON room_member;
+DROP TRIGGER IF EXISTS refresh_search ON room;
+DROP TRIGGER IF EXISTS refresh_search ON room_member;
 
-DROP TRIGGER refresh_search ON attachment;
-DROP TRIGGER refresh_search ON topic;
+DROP TRIGGER IF EXISTS refresh_search ON attachment;
+DROP TRIGGER IF EXISTS refresh_search ON topic;
 
-DROP TRIGGER refresh_search ON transcription;
+DROP TRIGGER IF EXISTS refresh_search ON transcription;
 DROP TRIGGER IF EXISTS refresh_search ON message;
