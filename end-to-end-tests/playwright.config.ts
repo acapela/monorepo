4059f8dd4f2632ef354e3b875c712c834bc604c5
Gregor Weber <gregorwbr@gmail.com>
2021-08-05 00:45:48 +0200

    feat: upload test fail screenshots

--- end-to-end-tests/playwright.config.ts
+++ end-to-end-tests/playwright.config.ts
@@ -2,5 +2,8 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   globalSetup: require.resolve("./helper/global-setup"),
+  use: {
+    screenshot: "only-on-failure",
+  },
 };
 export default config;
