9c17ad4803e1d80aaaf62e45805c58fcf351eab2
Gregor <gregorwbr@gmail.com>
2021-08-04 15:01:57 +0200

    feat: reuse CreateRoom-Modal/-Button and make it save members (#306)
    * fix: save room members when creating a new room
    
    * chore: reuse CreateRoomButton in SpaceView

--- frontend/src/views/SpaceView/index.tsx
+++ frontend/src/views/SpaceView/index.tsx
@@ -1,22 +1,17 @@
 import styled from "styled-components";
-import { useCreateRoomMutation } from "~frontend/gql/rooms";
 import { useIsCurrentUserSpaceMember, useSingleSpaceQuery } from "~frontend/gql/spaces";
-import { routes } from "~frontend/router";
-import { openUIPrompt } from "~frontend/utils/prompt";
-import { Button } from "~ui/buttons/Button";
 import { RoomFilters } from "~frontend/ui/rooms/filters/RoomFilters";
 import { useMemo } from "react";
-import { createLengthValidator } from "~shared/validation/inputValidation";
 import { useRoomFilterVariables } from "~frontend/ui/rooms/filters/filter";
 import { RoomsGroupedByMembership } from "~frontend/ui/rooms/RoomsList";
 import { createSpaceFilter } from "~frontend/ui/rooms/filters/factories";
-import { IconPlusSquare } from "~ui/icons";
 import { SpacedAppLayoutContainer } from "~frontend/layouts/AppLayout/SpacedAppLayoutContainer";
 import { SpaceHeader } from "./SpaceHeader";
 import { CenteredContentWithSides } from "~ui/layout/CenteredContentWithSides";
 import { SpaceTools } from "./SpaceTools";
 import { AvatarList } from "~frontend/ui/users/AvatarList";
 import { PageMeta } from "~frontend/utils/PageMeta";
+import { CreateRoomButton } from "~frontend/ui/rooms/CreateRoomButton";
 
 interface Props {
   spaceId: string;
@@ -28,31 +23,6 @@ export function SpaceView({ spaceId }: Props) {
   const amIMember = useIsCurrentUserSpaceMember(space ?? undefined);
 
   const [roomQuery, setFilters] = useRoomFilterVariables([roomsInCurrentSpaceFilter]);
-
-  const [createRoom] = useCreateRoomMutation();
-
-  async function onCreate() {
-    const roomName = await openUIPrompt({
-      title: "Room name",
-      placeholder: "Daily standup...",
-      submitLabel: "Create room",
-      validateInput: createLengthValidator("Room name", 3),
-    });
-
-    if (!roomName?.trim()) {
-      return;
-    }
-
-    const [room] = await createRoom({ input: { name: roomName, space_id: spaceId } });
-
-    const roomId = room?.id;
-
-    if (!roomId) {
-      return;
-    }
-
-    routes.spaceRoom.push({ spaceId, roomId });
-  }
 
   const spaceMembers = space?.members.map((member) => member.user) ?? [];
 
@@ -67,14 +37,9 @@ export function SpaceView({ spaceId }: Props) {
 
         <CenteredContentWithSides
           rightNode={
-            <UIFlyingCreateRoomButton
-              onClick={onCreate}
-              iconPosition="start"
-              icon={<IconPlusSquare />}
-              isDisabled={!amIMember && { reason: `You have to be space member to add new room` }}
-            >
+            <CreateRoomButton isDisabled={!amIMember && { reason: `You have to be space member to add new room` }}>
               New Room
-            </UIFlyingCreateRoomButton>
+            </CreateRoomButton>
           }
         >
           <UIFilters onFiltersChange={setFilters} />
@@ -109,5 +74,3 @@ const UIRooms = styled.div<{}>`
   flex-direction: column;
   gap: 8px;
 `;
-
-const UIFlyingCreateRoomButton = styled(Button)<{}>``;
