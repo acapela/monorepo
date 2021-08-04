14a25a84c3738207f083fff11a6ad84fa990f202
Gregor Weber <gregorwbr@gmail.com>
2021-08-05 00:33:18 +0200

    feat: add room create test

--- end-to-end-tests/index.test.ts
+++ end-to-end-tests/index.test.ts
@@ -11,3 +11,19 @@ test("view spaces", async ({ page, db, auth }) => {
   await page.goto(basePath + "/spaces");
   await page.waitForSelector(`text=${db.space.name}`);
 });
+
+test("create a room", async ({ page, db, auth }) => {
+  await auth.login(db.user1);
+  await page.goto(basePath);
+  await page.click("text=Select this team");
+
+  await page.click("text=New Room");
+  await page.click("text=Room name", { force: true });
+  await page.keyboard.type("Roomba");
+  await page.click("text='Space'", { force: true });
+  await page.click("text=" + db.space.name);
+  await page.click("text='Create'");
+
+  await page.waitForNavigation({ url: (url) => url.pathname.startsWith("/space") });
+  await page.waitForSelector("text=Roomba");
+});
