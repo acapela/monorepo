ddcdef89785273c359c4ca36b657637c24373fbe
Heiki Riesenkampf <me@heikir.com>
2021-08-05 10:27:10 +0200

    chore: add missing tracking events

--- shared/types/analytics.ts
+++ shared/types/analytics.ts
@@ -19,27 +19,36 @@ export type AnalyticsEventsMap = {
   "Invite Sent": void;
   // invitation accepted
   "Account Added User": void;
+  // delete a team member from team management space
   "Account Removed User": void;
-  // same as team created, will be changed later
+  // should be called simultanously with Account Created for now
   "Trial Started": void;
+  // can be ignored for now
   "Trial Ended": void;
   "Created Space": void;
+  // current user or when current user adds someone else
   "Joined Space": void;
   "Left Space": void;
   "Deleted Space": void;
   "Created Room": void;
   "Joined Room": void;
   "Left Room": void;
+  "Reopened Room": void;
   "Closed Room": void;
   "Deleted Room": void;
+  "Renamed Room": void;
   "Made Room Public": void;
   "Made Room Private": void;
   "Created Topic": void;
+  "Reopened Topic": void;
   "Closed Topic": void;
+  "Deleted Topic": void;
+  "Renamed Topic": void;
   // should track type of message (video, text, audio, files)
-  "Added New Message": void;
+  "Sent Message": void;
   "Edited Message": void;
   "Deleted Message": void;
+  "Reacted To Message": void;
 };
 
 export type AnalyticsEventName = keyof AnalyticsEventsMap;
