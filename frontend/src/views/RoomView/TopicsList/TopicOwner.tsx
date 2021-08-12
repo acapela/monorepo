565aea398feb77588c732b14fbae7f94eb9eaf37
Rodion Chachura <geekrodion@gmail.com>
2021-08-12 11:21:46 +0400

    feature: change topic owner

--- frontend/src/views/RoomView/TopicsList/TopicOwner.tsx
+++ frontend/src/views/RoomView/TopicsList/TopicOwner.tsx
@@ -2,8 +2,15 @@ import styled, { css } from "styled-components";
 import { IconChevronDown } from "~ui/icons";
 import { theme } from "~ui/theme";
 import { UserAvatar } from "~frontend/ui/users/UserAvatar";
-import { TopicDetailedInfoFragment } from "~gql";
+import { TopicDetailedInfoFragment, UserBasicInfoFragment } from "~gql";
 import { useIsCurrentUserTopicManager } from "~frontend/topics/useIsCurrentUserTopicManager";
+import { useBoolean } from "~shared/hooks/useBoolean";
+import { useRef } from "react";
+import { AnimatePresence } from "framer-motion";
+import { Popover } from "~ui/popovers/Popover";
+import { ItemsDropdown } from "~ui/forms/OptionsDropdown/ItemsDropdown";
+import { updateTopic } from "~frontend/gql/topics";
+import { getUserDisplayName } from "~frontend/utils/getUserDisplayName";
 
 interface Props {
   topic: TopicDetailedInfoFragment;
@@ -12,14 +19,39 @@ interface Props {
 export const TopicOwner = ({ topic }: Props) => {
   const isTopicManager = useIsCurrentUserTopicManager(topic);
 
+  const openerRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpened, { unset: closeMenu, toggle: toggleMenu }] = useBoolean(false);
+
   if (!topic.owner) return null;
 
+  const onOwnerSelect = (user: UserBasicInfoFragment) => {
+    updateTopic({ topicId: topic.id, input: { owner_id: user.id } });
+    closeMenu();
+  };
+
   return (
-    <UIHolder isInteractive={isTopicManager}>
-      <UserAvatar size="extra-small" user={topic.owner} disableNameTooltip />
-      {topic.owner.name}
-      {isTopicManager && <IconChevronDown />}
-    </UIHolder>
+    <>
+      <UIHolder ref={openerRef} onClick={toggleMenu} isInteractive={isTopicManager}>
+        <UserAvatar size="extra-small" user={topic.owner} disableNameTooltip />
+        {topic.owner.name}
+        {isTopicManager && <IconChevronDown />}
+      </UIHolder>
+      <AnimatePresence>
+        {isMenuOpened && (
+          <Popover anchorRef={openerRef} placement="bottom-start">
+            <ItemsDropdown
+              items={topic.room.members.map(({ user }) => user)}
+              keyGetter={(user) => user.id}
+              labelGetter={getUserDisplayName}
+              onItemSelected={onOwnerSelect}
+              onCloseRequest={closeMenu}
+              selectedItems={[topic.owner]}
+              iconGetter={(user) => <UserAvatar size="small" user={user} />}
+            />
+          </Popover>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
