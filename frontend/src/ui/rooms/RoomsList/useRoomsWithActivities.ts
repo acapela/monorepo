e5270cd6605f759e63daf32931beb9d52254b1b3
Omar Duarte <omarduarte@gmail.com>
2021-08-13 15:07:28 +0300

    fix: rooms list now shown in correct order

--- frontend/src/ui/rooms/RoomsList/useRoomsWithActivities.ts
+++ frontend/src/ui/rooms/RoomsList/useRoomsWithActivities.ts
@@ -1,3 +1,4 @@
+import { compareAsc, compareDesc } from "date-fns";
 import { useSSRRoomsMessagesCount } from "~frontend/utils/unreadMessages";
 import { RoomDetailedInfoFragment } from "~gql";
 
@@ -6,11 +7,31 @@ export interface RoomWithActivities {
   unreadMessages: number;
 }
 
-export function useRoomsWithActivities(rooms: RoomDetailedInfoFragment[]): RoomWithActivities[] {
+interface Props {
+  rooms: RoomDetailedInfoFragment[];
+  options?: {
+    sort?: "asc" | "desc";
+  };
+}
+
+const EARLIEST_POSSIBLE_DATE = new Date(0);
+
+export function useRoomsWithActivities({ rooms, options }: Props): RoomWithActivities[] {
   const roomUnreadMessages = useSSRRoomsMessagesCount();
 
-  return rooms.map((room: RoomDetailedInfoFragment) => ({
+  const roomsWithActivities = rooms.map((room: RoomDetailedInfoFragment) => ({
     room,
     unreadMessages: roomUnreadMessages[room.id] ?? 0,
   }));
+
+  if (options?.sort) {
+    const compareFn = options.sort === "asc" ? compareAsc : compareDesc;
+    roomsWithActivities.sort(({ room: roomA }, { room: roomB }) => {
+      const dateA = roomA.last_activity_at ? new Date(roomA.last_activity_at) : EARLIEST_POSSIBLE_DATE;
+      const dateB = roomB.last_activity_at ? new Date(roomB.last_activity_at) : EARLIEST_POSSIBLE_DATE;
+      return compareFn(dateA, dateB);
+    });
+  }
+
+  return roomsWithActivities;
 }
