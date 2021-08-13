c46727097fa529a3150e13eeb41c8e016df597ca
Omar Duarte <omarduarte@gmail.com>
2021-08-13 16:17:27 +0300

    refactor: extract room sort function

--- frontend/src/ui/rooms/RoomsList/useRoomsWithActivities.ts
+++ frontend/src/ui/rooms/RoomsList/useRoomsWithActivities.ts
@@ -7,14 +7,27 @@ export interface RoomWithActivities {
   unreadMessages: number;
 }
 
+type Sort = "asc" | "desc";
+
 interface Props {
   rooms: RoomDetailedInfoFragment[];
   options?: {
-    sort?: "asc" | "desc";
+    sort?: Sort;
   };
 }
 
 const EARLIEST_POSSIBLE_DATE = new Date(0);
+
+function sortRoomsBy(roomsWithActivities: RoomWithActivities[], sort: Sort): void {
+  const compareFn = sort === "asc" ? compareAsc : compareDesc;
+  const lastActivityFallback = sort === "asc" ? new Date() : EARLIEST_POSSIBLE_DATE;
+
+  roomsWithActivities.sort(({ room: roomA }, { room: roomB }) => {
+    const dateA = roomA.last_activity_at ? new Date(roomA.last_activity_at) : lastActivityFallback;
+    const dateB = roomB.last_activity_at ? new Date(roomB.last_activity_at) : lastActivityFallback;
+    return compareFn(dateA, dateB);
+  });
+}
 
 export function useRoomsWithActivities({ rooms, options }: Props): RoomWithActivities[] {
   const roomUnreadMessages = useSSRRoomsMessagesCount();
@@ -25,13 +38,7 @@ export function useRoomsWithActivities({ rooms, options }: Props): RoomWithActiv
   }));
 
   if (options?.sort) {
-    const compareFn = options.sort === "asc" ? compareAsc : compareDesc;
-    const lastActivityFallback = options.sort === "asc" ? new Date() : EARLIEST_POSSIBLE_DATE;
-    roomsWithActivities.sort(({ room: roomA }, { room: roomB }) => {
-      const dateA = roomA.last_activity_at ? new Date(roomA.last_activity_at) : lastActivityFallback;
-      const dateB = roomB.last_activity_at ? new Date(roomB.last_activity_at) : lastActivityFallback;
-      return compareFn(dateA, dateB);
-    });
+    sortRoomsBy(roomsWithActivities, options.sort);
   }
 
   return roomsWithActivities;
