6ee6770ffdbf9ce15ec6e41f9776f75aa2cd6693
Heiki Riesenkampf <me@heikir.com>
2021-08-17 18:22:33 +0200

    chore: fix linter errors

--- frontend/src/authentication/GoogleLoginButton.tsx
+++ frontend/src/authentication/GoogleLoginButton.tsx
@@ -1,6 +1,6 @@
+import { signIn } from "next-auth/client";
 import React, { ReactNode } from "react";
 
-import { signIn } from "next-auth/client";
 import { Button } from "~ui/buttons/Button";
 
 export const GoogleLoginButton = ({
