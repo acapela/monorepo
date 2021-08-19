0c8d8354d651a4fb5147f81345396d55d12a7355
Adam Pietrasiak <adam@pietrasiak.com>
2021-08-19 14:39:16 +0200

    feat: split task fragment into basic and detailed

--- frontend/src/gql/tasks.ts
+++ frontend/src/gql/tasks.ts
@@ -1,6 +1,11 @@
 import { gql } from "@apollo/client";
 
-import { TaskBasicInfoFragment as TaskBasicInfoFragmentType, TasksQuery, TasksQueryVariables } from "~gql";
+import {
+  TaskBasicInfoFragment as TaskBasicInfoFragmentType,
+  TaskDetailedInfoFragment as TaskDetailedInfoFragmentType,
+  TasksQuery,
+  TasksQueryVariables,
+} from "~gql";
 
 import { MessageBasicInfoFragment } from "./messages";
 import { UserBasicInfoFragment } from "./user";
@@ -8,30 +13,41 @@ import { createFragment, createQuery } from "./utils";
 
 export const TaskBasicInfoFragment = createFragment<TaskBasicInfoFragmentType>(
   () => gql`
-    ${MessageBasicInfoFragment()}
-    ${UserBasicInfoFragment()}
-
     fragment TaskBasicInfo on task {
       id
-      user {
-        ...UserBasicInfo
-      }
-      message {
-        ...MessageBasicInfo
-      }
+      user_id
+      message_id
       created_at
       done_at
     }
   `
 );
 
+export const TaskDetailedInfoFragment = createFragment<TaskDetailedInfoFragmentType>(
+  () => gql`
+    ${MessageBasicInfoFragment()}
+    ${UserBasicInfoFragment()}
+    ${TaskBasicInfoFragment()}
+
+    fragment TaskDetailedInfo on task {
+      ...TaskBasicInfo
+      user {
+        ...UserBasicInfo
+      }
+      message {
+        ...MessageBasicInfo
+      }
+    }
+  `
+);
+
 export const [useTasksQuery] = createQuery<TasksQuery, TasksQueryVariables>(
   () => gql`
-    ${TaskBasicInfoFragment()}
+    ${TaskDetailedInfoFragment()}
 
     query Tasks($limit: Int, $orderBy: [task_order_by!], $where: task_bool_exp) {
       tasks: task(where: $where, limit: $limit, order_by: $orderBy) {
-        ...TaskBasicInfo
+        ...TaskDetailedInfo
       }
     }
   `
