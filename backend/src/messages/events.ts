c4ab9dab477ca789c954e0e8fcc4c4549ae04687
Omar Duarte <omarduarte@gmail.com>
2021-08-26 12:48:53 +0300

    refactor: extract mention handlers from message events

--- backend/src/messages/events.ts
+++ backend/src/messages/events.ts
@@ -1,17 +1,13 @@
-import { uniq, uniqBy } from "lodash";
-
-import { Message, Notification, PrismaPromise, Task, db } from "~db";
+import { Message, db } from "~db";
 import { Message_Type_Enum } from "~gql";
-import { getNodesFromContentByType } from "~richEditor/content/helper";
 import { convertMessageContentToPlainText } from "~richEditor/content/plainText";
 import { RichEditorNode } from "~richEditor/content/types";
 import { assert } from "~shared/assert";
 import log from "~shared/logger";
-import { EditorMentionData } from "~shared/types/editor";
 
 import { HasuraEvent } from "../hasura";
-import { createNotification } from "../notifications/entity";
 import { updateRoomLastActivityDate } from "../rooms/rooms";
+import { createMessageMentionNotifications, createTasksFromNewMentions } from "./mentionHandlers";
 
 export async function prepareMessagePlainTextData(message: Message) {
   if ((message.type as Message_Type_Enum) !== "TEXT") {
@@ -28,83 +24,6 @@ export async function prepareMessagePlainTextData(message: Message) {
     log.warn("Failed to prepare message plain text content", message);
   }
 }
-
-function getMentionNodesFromMessage(message: Message) {
-  const content = message.content as RichEditorNode;
-  const mentionNodes = getNodesFromContentByType<{ data: EditorMentionData }>(content, "mention");
-
-  return uniqBy(mentionNodes, (mention) => mention.attrs.data.userId);
-}
-
-function getNewMentionNodesFromMessage(message: Message, messageBefore: Message | null) {
-  const mentionNodesNow = getMentionNodesFromMessage(message);
-
-  if (!messageBefore) {
-    return mentionNodesNow;
-  }
-
-  const mentionNodesBefore = getMentionNodesFromMessage(messageBefore);
-
-  const mentionedUserIdsBefore = new Set(mentionNodesBefore.map((n) => n.attrs.data.userId));
-  const newMentionNodes = mentionNodesNow.filter(
-    (mentionNodeNow) => !mentionedUserIdsBefore.has(mentionNodeNow.attrs.data.userId)
-  );
-
-  return newMentionNodes;
-}
-
-function getMentionedUserIdsFromMessage(message: Message, messageBefore: Message | null): string[] {
-  const mentionNodes = getNewMentionNodesFromMessage(message, messageBefore);
-
-  if (mentionNodes.length === 0) {
-    // no mentions in message
-    return [];
-  }
-
-  /**
-   * Avoid situation where multiple mentions of same user in the same message
-   */
-  const allUserIds = mentionNodes.map((mention) => mention.attrs.data.userId);
-  const uniqueUserIds = uniq(allUserIds);
-
-  const userIdsExcludingMessageAuthor = uniqueUserIds.filter((userId) => userId != message.user_id);
-
-  return userIdsExcludingMessageAuthor;
-}
-
-async function createMessageMentionNotifications(message: Message, messageBefore: Message | null) {
-  const mentionedUserIds = getMentionedUserIdsFromMessage(message, messageBefore);
-
-  const createNotificationPromises: Array<PrismaPromise<Notification>> = [];
-
-  for (const mentionedUserId of mentionedUserIds) {
-    const createNotificationPromise = createNotification({
-      type: "topicMention",
-      payload: { topicId: message.topic_id, mentionedByUserId: message.user_id },
-      userId: mentionedUserId,
-    });
-
-    createNotificationPromises.push(createNotificationPromise);
-  }
-
-  return await db.$transaction(createNotificationPromises);
-}
-
-async function createTasksFromNewMentions(message: Message, messageBefore: Message | null) {
-  const mentionedUserIds = getMentionedUserIdsFromMessage(message, messageBefore);
-  const createTasksPromises: Array<PrismaPromise<Task>> = [];
-
-  for (const mentionedUserId of mentionedUserIds) {
-    createTasksPromises.push(
-      db.task.create({
-        data: { message_id: message.id, user_id: mentionedUserId },
-      })
-    );
-  }
-
-  return await db.$transaction(createTasksPromises);
-}
-
 async function markPendingTasksAsDone(message: Message) {
   const { topic_id, user_id } = message;
 
---
+++ backend/src/messages/mentionHandlers.ts
@@ -0,0 +1,84 @@
+import { uniq, uniqBy } from "lodash";
+
+import { Message, Notification, PrismaPromise, Task, db } from "~db";
+import { getNodesFromContentByType } from "~richEditor/content/helper";
+import { RichEditorNode } from "~richEditor/content/types";
+import { EditorMentionData } from "~shared/types/editor";
+
+import { createNotification } from "../notifications/entity";
+
+function getMentionNodesFromMessage(message: Message) {
+  const content = message.content as RichEditorNode;
+  const mentionNodes = getNodesFromContentByType<{ data: EditorMentionData }>(content, "mention");
+
+  return uniqBy(mentionNodes, (mention) => mention.attrs.data.userId);
+}
+
+function getNewMentionNodesFromMessage(message: Message, messageBefore: Message | null) {
+  const mentionNodesNow = getMentionNodesFromMessage(message);
+
+  if (!messageBefore) {
+    return mentionNodesNow;
+  }
+
+  const mentionNodesBefore = getMentionNodesFromMessage(messageBefore);
+
+  const mentionedUserIdsBefore = new Set(mentionNodesBefore.map((n) => n.attrs.data.userId));
+  const newMentionNodes = mentionNodesNow.filter(
+    (mentionNodeNow) => !mentionedUserIdsBefore.has(mentionNodeNow.attrs.data.userId)
+  );
+
+  return newMentionNodes;
+}
+
+function getMentionedUserIdsFromMessage(message: Message, messageBefore: Message | null): string[] {
+  const mentionNodes = getNewMentionNodesFromMessage(message, messageBefore);
+
+  if (mentionNodes.length === 0) {
+    // no mentions in message
+    return [];
+  }
+
+  /**
+   * Avoid situation where multiple mentions of same user in the same message
+   */
+  const allUserIds = mentionNodes.map((mention) => mention.attrs.data.userId);
+  const uniqueUserIds = uniq(allUserIds);
+
+  const userIdsExcludingMessageAuthor = uniqueUserIds.filter((userId) => userId != message.user_id);
+
+  return userIdsExcludingMessageAuthor;
+}
+
+export async function createMessageMentionNotifications(message: Message, messageBefore: Message | null) {
+  const mentionedUserIds = getMentionedUserIdsFromMessage(message, messageBefore);
+
+  const createNotificationPromises: Array<PrismaPromise<Notification>> = [];
+
+  for (const mentionedUserId of mentionedUserIds) {
+    const createNotificationPromise = createNotification({
+      type: "topicMention",
+      payload: { topicId: message.topic_id, mentionedByUserId: message.user_id },
+      userId: mentionedUserId,
+    });
+
+    createNotificationPromises.push(createNotificationPromise);
+  }
+
+  return await db.$transaction(createNotificationPromises);
+}
+
+export async function createTasksFromNewMentions(message: Message, messageBefore: Message | null) {
+  const mentionedUserIds = getMentionedUserIdsFromMessage(message, messageBefore);
+  const createTasksPromises: Array<PrismaPromise<Task>> = [];
+
+  for (const mentionedUserId of mentionedUserIds) {
+    createTasksPromises.push(
+      db.task.create({
+        data: { message_id: message.id, user_id: mentionedUserId },
+      })
+    );
+  }
+
+  return await db.$transaction(createTasksPromises);
+}
