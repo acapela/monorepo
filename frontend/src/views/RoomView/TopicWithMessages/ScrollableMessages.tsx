1c3e3dc7e1365d6aaa6a6ac8abe6a9a37345d0c0
Gregor Weber <gregorwbr@gmail.com>
2021-08-30 10:38:33 +0200

    fix: safari message feed auto-scrolling

--- frontend/src/views/RoomView/TopicWithMessages/ScrollableMessages.tsx
+++ frontend/src/views/RoomView/TopicWithMessages/ScrollableMessages.tsx
@@ -1,9 +1,7 @@
-import React, { ReactNode, useCallback, useRef } from "react";
+import React, { ReactNode, useRef } from "react";
 import styled from "styled-components";
 
 import { useTopicStoreContext } from "~frontend/topics/TopicStore";
-import { assertDefined } from "~shared/assert";
-import { useElementEvent } from "~shared/domEvents";
 import { select } from "~shared/sharedState";
 
 import { ScrollToBottomMonitor } from "./ScrollToBottomMonitor";
@@ -15,22 +13,14 @@ interface Props {
 
 export const ScrollableMessages = styled<Props>(({ children, className }) => {
   const holderRef = useRef<HTMLDivElement>(null);
-  const isScrolledToBottom = useRef(true);
-
-  const handleScroll = useCallback(() => {
-    const parent = assertDefined(holderRef.current, "Event can't have been called on deleted element");
-    isScrolledToBottom.current = parent.scrollTop == parent.scrollHeight - parent.clientHeight;
-  }, []);
-  useElementEvent(holderRef, "scroll", handleScroll);
 
   const topicContext = useTopicStoreContext();
   const isInEditMode = select(() => !!topicContext.editedMessageId);
 
-  const getShouldScroll = useCallback(() => isScrolledToBottom.current && !isInEditMode, [isInEditMode]);
   return (
     <UIHolder className={className} ref={holderRef}>
       <UIInner>
-        <ScrollToBottomMonitor parentRef={holderRef} getShouldScroll={getShouldScroll} />
+        <ScrollToBottomMonitor parentRef={holderRef} preventAutoScroll={isInEditMode} />
         {children}
       </UIInner>
     </UIHolder>
