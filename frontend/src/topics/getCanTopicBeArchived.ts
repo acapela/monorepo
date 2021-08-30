3fc0abbd073c702363f2d9df4885ab02a19bfdf2
Rodion Chachura <geekrodion@gmail.com>
2021-08-30 16:14:54 +0400

    fix: getCanTopicBeArchived

--- frontend/src/topics/getCanTopicBeArchived.ts
+++ frontend/src/topics/getCanTopicBeArchived.ts
@@ -1,4 +1,4 @@
 import { ManageTopic_TopicFragment } from "~gql";
 
 export const getCanTopicBeArchived = ({ closed_at, archived_at }: ManageTopic_TopicFragment) =>
-  closed_at && archived_at;
+  closed_at && !archived_at;
