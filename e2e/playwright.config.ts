1c68edce8169bf90db681d8256768314e42d6028
Christoph Witzko <github@christophwitzko.com>
2021-09-01 10:39:06 +0200

    ci: enable playwright traces

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,6 +4,7 @@ const config: PlaywrightTestConfig = {
   globalSetup: require.resolve("./helper/global-setup"),
   use: {
     video: "retain-on-failure",
+    trace: "retain-on-failure",
   },
 };
 export default config;
