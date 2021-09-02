e784882917ec39e19a61dc2687d5fd3baaf79dcd
Gregor Weber <gregorwbr@gmail.com>
2021-09-02 16:26:36 +0200

    fix: add test

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -81,6 +81,26 @@ test("send a message", async ({ page, db, auth }) => {
   await page.waitForSelector("text=" + message);
 });
 
+test("replying to a request-response marks it as done", async ({ page, db, auth }) => {
+  await auth.login(db.user2);
+  await createTopic(page, { spaceName: db.space.name, roomName: "ResponsiveRoom", topicName: "cold takes" });
+
+  await page.click(COMPOSER_SELECTOR);
+  await page.keyboard.type("What is happening @u");
+  await page.click(`[role="option"]:has-text("${db.user2.name}")`);
+  await page.click("text=Request response");
+  await page.click(COMPOSER_SELECTOR);
+  await page.keyboard.press("Enter");
+
+  await page.waitForSelector(`text=Response from`);
+
+  await page.click(COMPOSER_SELECTOR);
+  await page.keyboard.type("Not much");
+  await page.keyboard.press("Enter");
+
+  await page.waitForSelector("[data-test-task-is-done]");
+});
+
 test("sending a message with tasks for read and response, asks for the latter", async ({ page, db, auth }) => {
   await auth.login(db.user2);
   await createTopic(page, { spaceName: db.space.name, roomName: "Mailbox", topicName: "hot takes" });
