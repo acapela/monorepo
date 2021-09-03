399dbef8d40104502d46a24c5ef41675bf1b81f1
Gregor Weber <gregorwbr@gmail.com>
2021-09-03 17:07:37 +0200

    fix: subscribe to message entity changes

--- infrastructure/hasura/migrations/default/1630678195474_alter_table_public_message_reaction_add_column_created_at/down.sql
+++ infrastructure/hasura/migrations/default/1630678195474_alter_table_public_message_reaction_add_column_created_at/down.sql
@@ -1,4 +1,11 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."message_reaction" add column "created_at" timestamptz
---  null default now();
+alter table "public"."message_reaction" drop column "created_at";
+
+drop index message_reaction.message_reaction_created_at_idx;
+
+drop index task.task_created_at_idx;
+drop index task.task_done_at_idx;
+
+drop index attachment.attachment_created_at_idx;
+
+drop index transcription.transcription_created_at_idx;
+
