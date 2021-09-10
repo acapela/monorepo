def0b54aeb5a3afed3cad59e9d63e18be3338dd7
Gregor Weber <gregorwbr@gmail.com>
2021-09-10 14:14:30 +0200

    fix: down migration warning

--- infrastructure/hasura/migrations/default/1630940609536_create_notification_tables/down.sql
+++ infrastructure/hasura/migrations/default/1630940609536_create_notification_tables/down.sql
@@ -1,18 +1,13 @@
-DROP TABLE notification_room_added_to CASCADE;
-DROP TABLE notification_room_closed CASCADE;
-DROP TABLE notification_topic_added_to CASCADE;
-DROP TABLE notification_topic_assigned CASCADE;
-DROP TABLE notification_topic_closed CASCADE;
-DROP TABLE notification_topic_mention CASCADE;
-
-TRUNCATE TABLE notification;
-
-ALTER TABLE notification
-  ADD COLUMN "data" JSONB NOT NULL,
-  DROP COLUMN notification_room_added_to_id,
-  DROP COLUMN notification_room_closed_id,
-  DROP COLUMN notification_topic_added_to_id,
-  DROP COLUMN notification_topic_assigned_id,
-  DROP COLUMN notification_topic_closed_id,
-  DROP COLUMN notification_topic_mention_id;
-
+-- This migration is destructive, only run it if you're sure this is what you want
+-- DROP TABLE notification_room_added_to CASCADE;
+-- DROP TABLE notification_room_closed CASCADE;
+-- DROP TABLE notification_topic_added_to CASCADE;
+-- DROP TABLE notification_topic_assigned CASCADE;
+-- DROP TABLE notification_topic_closed CASCADE;
+-- DROP TABLE notification_topic_mention CASCADE;
+--
+-- TRUNCATE TABLE notification;
+--
+-- ALTER TABLE notification
+--   ADD COLUMN "data" JSONB NOT NULL;
+--
