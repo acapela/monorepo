eaa1b656863288a9e5ebba98d6de06ec27b7e2d4
Gregor <gregorwbr@gmail.com>
2021-09-10 09:10:48 +0200

    refactor: add isServer util (#427)

--- shared/promises.ts
+++
@@ -1,5 +0,0 @@
-export function makePromiseVoidable(input: Promise<unknown>): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    input.then(() => resolve()).catch(reject);
-  });
-}
