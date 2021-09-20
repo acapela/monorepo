6aec7d54523026f603d21c59e5a8f0ab7a5edd29
Christoph Witzko <github@christophwitzko.com>
2021-09-20 17:58:45 +0200

    ci: add separate start scripts for frontend and backend

--- backend/start-ci.sh
+++ backend/start-ci.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 nohup yarn ts-node --transpile-only ./src/index.ts & echo $! > backend.pid
--- frontend/start-ci.sh
+++ frontend/start-ci.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 nohup yarn node ./next-server.js & echo $! > frontend.pid
