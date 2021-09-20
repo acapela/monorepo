90f333fd1f52e8ebf9ca04b2790939a984c169a5
Gregor Weber <gregorwbr@gmail.com>
2021-09-20 17:14:56 +0200

    feat: notification through slack connect

--- backend/src/actions/actionHandlers.ts
+++ backend/src/actions/actionHandlers.ts
@@ -1,19 +1,20 @@
 import { getUploadUrl } from "~backend/src/attachments/attachments";
 import { roomInvitationView } from "~backend/src/roomInvitation/roomInvitationView";
-import { getTeamSlackInstallationURL } from "~backend/src/slack/install";
+import { findSlackUser, getTeamSlackInstallationURL } from "~backend/src/slack/install";
 import { lookupTeamName } from "~backend/src/teamInvitation/lookupTeamName";
 import { resendInvitation } from "~backend/src/teamInvitation/resendInvitation";
 
 export interface ActionHandler<DataT, ResponseT> {
   actionName: string;
-  handle: (userId: string | undefined, data: DataT) => Promise<ResponseT>;
+  handle: (userId: string | undefined, data: DataT) => Promise<Omit<ResponseT, "__typename">>;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handlers: ActionHandler<any, any>[] = [
+  findSlackUser,
+  getTeamSlackInstallationURL,
   getUploadUrl,
   lookupTeamName,
   resendInvitation,
   roomInvitationView,
-  getTeamSlackInstallationURL,
 ];
