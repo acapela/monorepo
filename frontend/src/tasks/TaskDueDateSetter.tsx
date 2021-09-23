6dc1c57ea6e8cde82b7e39bd2de2ac432774c7ea
Omar Duarte <omarduarte@gmail.com>
2021-09-23 17:44:44 +0300

    chore: make task due date setter more accessible

--- frontend/src/tasks/TaskDueDateSetter.tsx
+++ frontend/src/tasks/TaskDueDateSetter.tsx
@@ -4,13 +4,13 @@ import React, { ReactNode, useRef } from "react";
 import styled from "styled-components";
 
 import { updateTask } from "~frontend/gql/tasks";
-import { MessageTask_TaskFragment } from "~gql";
 import { useBoolean } from "~shared/hooks/useBoolean";
 import { Popover } from "~ui/popovers/Popover";
 import { DateTimePicker } from "~ui/time/DateTimePicker";
 
 interface Props {
-  task: MessageTask_TaskFragment;
+  taskId: string;
+  previousDueDate?: string | null;
   children: ReactNode;
 }
 
@@ -23,17 +23,17 @@ function getDefaultInitialDueDate() {
   return setHours(nextWorkDay, DEFAULT_MIDDLE_OF_WORK_DAY);
 }
 
-export const TaskDueDateSetter = ({ task, children }: Props) => {
+export const TaskDueDateSetter = ({ taskId, previousDueDate, children }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
 
   const [isPickerOpen, { set: openPicker, unset: closePicker }] = useBoolean(false);
 
   const handleSubmit = async (date: Date) => {
     closePicker();
-    updateTask({ taskId: task.id, input: { due_at: date.toISOString() } });
+    updateTask({ taskId: taskId, input: { due_at: date.toISOString() } });
   };
 
-  const calendarInitialValue = task.due_at ? new Date(task.due_at) : getDefaultInitialDueDate();
+  const calendarInitialValue = previousDueDate ? new Date(previousDueDate) : getDefaultInitialDueDate();
 
   return (
     <>
