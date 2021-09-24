c5d61c8815ff20f61adda805a9c80459d4f41190
Rodion Chachura <geekrodion@gmail.com>
2021-09-24 16:27:35 +0400

    chore: update padding

--- frontend/src/views/DashboardView/Navigation/DashboardNavigationCard.tsx
+++ frontend/src/views/DashboardView/Navigation/DashboardNavigationCard.tsx
@@ -5,7 +5,7 @@ import { theme } from "~ui/theme";
 export const DashboardNavigationCard = styled.div`
   background-color: ${theme.colors.layout.foreground()};
   ${theme.borderRadius.item};
-  padding: 12px 16px;
+  padding: 8px 16px;
   gap: 8px;
   display: flex;
   flex-direction: column;
