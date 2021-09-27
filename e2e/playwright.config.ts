4f6e584ec59bcf6ef8e24805b8642457fb126eeb
Gregor Weber <gregorwbr@gmail.com>
2021-09-27 13:44:30 +0200

    fix: give failing tests 5 retries

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,6 +1,7 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  retries: 5, // We have some flakiness wrt topic creation
   globalSetup: require.resolve("./helper/global-setup"),
   use: {
     video: "retain-on-failure",
