7e3e743fce7b24e362278b7af7282f1005a9e557
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-04 15:34:05 +0200

    fix: fixed duplicate migrations

--- infrastructure/hasura/migrations/default/1633173878922_clientdb_updates/down.sql
+++ infrastructure/hasura/migrations/default/1633173878922_clientdb_updates/down.sql
@@ -1,24 +1,3 @@
-DROP TRIGGER IF EXISTS "set_public_attachment_updated_at" ON "public"."attachment";
-ALTER TABLE "public"."attachment" DROP COLUMN "updated_at";
-
-DROP TRIGGER IF EXISTS "set_public_message_updated_at" ON "public"."message";
-ALTER TABLE "public"."message" DROP COLUMN "updated_at";
-
-DROP TRIGGER IF EXISTS "set_public_room_updated_at" ON "public"."room";
-ALTER TABLE "public"."room" DROP COLUMN "updated_at";
-
-DROP TRIGGER IF EXISTS "set_public_space_updated_at" ON "public"."space";
-ALTER TABLE "public"."space" DROP COLUMN "updated_at";
-
-DROP TRIGGER IF EXISTS "set_public_team_updated_at" ON "public"."team";
-ALTER TABLE "public"."team" DROP COLUMN "updated_at";
-
-DROP TRIGGER IF EXISTS "set_public_topic_updated_at" ON "public"."topic";
-ALTER TABLE "public"."topic" DROP COLUMN "updated_at";
-
-DROP TRIGGER IF EXISTS "set_public_user_updated_at" ON "public"."user";
-ALTER TABLE "public"."user" DROP COLUMN "updated_at";
-
 alter table "public"."message_reaction" drop column "id" cascade
 alter table "public"."message_reaction" drop column "id";
 -- Could not auto-generate a down migration.
