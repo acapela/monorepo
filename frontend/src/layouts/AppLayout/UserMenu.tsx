849a8de0befe32f2e4d2004f9aeda2b3e4874bf4
Omar Duarte <omarduarte@gmail.com>
2021-10-06 15:19:40 +0300

    feat: update user menu

--- frontend/src/layouts/AppLayout/UserMenu.tsx
+++ frontend/src/layouts/AppLayout/UserMenu.tsx
@@ -1,15 +1,21 @@
+import router from "next/router";
+import React from "react";
 import styled from "styled-components";
 
+import { addToast } from "~frontend/../../ui/toasts/data";
 import { trackEvent } from "~frontend/analytics/tracking";
 import { useAssertCurrentUser } from "~frontend/authentication/useCurrentUser";
+import { useChangeCurrentTeamIdMutation } from "~frontend/gql/user";
 import { routes } from "~frontend/router";
 import { UserAvatar } from "~frontend/ui/users/UserAvatar";
 import { CircleIconButton } from "~ui/buttons/CircleIconButton";
-import { IconChevronDown } from "~ui/icons";
+import { IconLoader, IconMoreHoriz } from "~ui/icons";
 import { PopoverMenuTrigger } from "~ui/popovers/PopoverMenuTrigger";
 
 export function UserMenu() {
   const user = useAssertCurrentUser();
+
+  const [changeCurrentTeam] = useChangeCurrentTeamIdMutation();
 
   return (
     <UIHolder>
@@ -20,21 +26,35 @@ export function UserMenu() {
         }}
         options={[
           {
-            label: "Manage team",
+            label: "Settings",
             onSelect: () => {
-              routes.team.push({});
+              routes.settings.push({});
             },
           },
           {
-            label: "Logout",
+            label: "Switch teams",
+            onSelect: async () => {
+              addToast({ icon: <IconLoader />, type: "success", title: "Exiting current team..." });
+              await changeCurrentTeam({ userId: user.id, teamId: null });
+              router.reload();
+            },
+          },
+          {
+            label: "Visit website",
             onSelect: () => {
-              routes.logout.push({});
+              window?.open("https://acapela.com", "_blank")?.focus();
+            },
+          },
+          {
+            label: "Sign out",
+            onSelect: () => {
+              routes.logout.replace({});
               trackEvent("Signed Out");
             },
           },
         ]}
       >
-        <CircleIconButton kind="transparent" icon={<IconChevronDown />} />
+        <CircleIconButton kind="transparent" icon={<IconMoreHoriz />} />
       </PopoverMenuTrigger>
     </UIHolder>
   );
