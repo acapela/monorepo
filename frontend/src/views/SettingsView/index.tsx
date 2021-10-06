87ebb41bcb73f72059f4dd88405d4ec01ff12d8d
Omar Duarte <omarduarte@gmail.com>
2021-10-06 15:54:40 +0300

    feat: load settings only once team is loaded

--- frontend/src/views/SettingsView/index.tsx
+++ frontend/src/views/SettingsView/index.tsx
@@ -1,7 +1,9 @@
+import { observer } from "mobx-react";
 import React from "react";
 import styled from "styled-components";
 
 import { SidebarLayout } from "~frontend/layouts/SidebarLayout";
+import { useCurrentTeam } from "~frontend/team/useCurrentTeamId";
 
 import { NotificationSettings } from "./NotificationSettings";
 import { CurrentTeamMembersManager } from "./TeamMembersManager";
@@ -9,23 +11,28 @@ import { CurrentTeamMembersManager } from "./TeamMembersManager";
 const appVersion = process.env.NEXT_PUBLIC_SENTRY_RELEASE;
 const appBuildDate = process.env.NEXT_PUBLIC_BUILD_DATE;
 
-export function SettingsView() {
+export const SettingsView = observer(function SettingsView() {
+  const team = useCurrentTeam();
+
   return (
     <SidebarLayout>
-      <UIHolder>
-        <UIHeader>Settings</UIHeader>
-        <NotificationSettings />
-        <CurrentTeamMembersManager />
+      {!team && <>Loading...</>}
+      {team && (
+        <UIHolder>
+          <UIHeader>Settings</UIHeader>
+          <NotificationSettings />
+          <CurrentTeamMembersManager />
 
-        {appVersion && (
-          <div>
-            Version: {appVersion} ({appBuildDate})
-          </div>
-        )}
-      </UIHolder>
+          {appVersion && (
+            <div>
+              Version: {appVersion} ({appBuildDate})
+            </div>
+          )}
+        </UIHolder>
+      )}
     </SidebarLayout>
   );
-}
+});
 
 const UIHolder = styled.div<{}>`
   padding: 26px 20px;
