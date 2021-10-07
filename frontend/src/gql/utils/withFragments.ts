90012966023f04b65636cfd39300bf1de16d8016
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-07 16:03:02 +0200

    chore: remove withFragments

--- frontend/src/gql/utils/withFragments.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentNode } from "graphql";
-
-export function withFragments<Fragments extends Record<string, DocumentNode>, Fn extends Function>(
-  fragments: Fragments,
-  fn: Fn
-): Fn & { fragments: Fragments } {
-  return Object.assign(fn, { fragments });
-}
