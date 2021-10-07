ed1098cf30a5f3ce0a058acba9d52c27446b7785
Christoph Witzko <github@christophwitzko.com>
2021-10-07 10:49:38 +0200

    fix: wrongfully squashed migrations

--- infrastructure/hasura/migrations/default/1633527643622_drop_legacy_backend/up.sql
+++ infrastructure/hasura/migrations/default/1633527643622_drop_legacy_backend/up.sql
@@ -34,30 +34,6 @@ DROP table "public"."membership_status";
 
 DROP table "public"."topic_member";
 
-
-alter table "public"."team_member" add column "created_at" timestamptz
- not null default now();
-
-alter table "public"."team_member" add column "updated_at" timestamptz
- not null default now();
-
-CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
-RETURNS TRIGGER AS $$
-DECLARE
-  _new record;
-BEGIN
-  _new := NEW;
-  _new."updated_at" = NOW();
-  RETURN _new;
-END;
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER "set_public_team_member_updated_at"
-BEFORE UPDATE ON "public"."team_member"
-FOR EACH ROW
-EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
-COMMENT ON TRIGGER "set_public_team_member_updated_at" ON "public"."team_member"
-IS 'trigger to set value of column "updated_at" to current timestamp on row update';
-
 CREATE VIEW "public"."unread_messages" AS
 SELECT tm.user_id,
        topic.team_id,
