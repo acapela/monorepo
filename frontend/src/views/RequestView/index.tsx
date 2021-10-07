be947bcb321b661b5df69e6175ab05938999a0b8
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-07 21:49:17 +0200

    feat: automatically update url if slug changes while topic is opened

--- frontend/src/views/RequestView/index.tsx
+++ frontend/src/views/RequestView/index.tsx
@@ -1,8 +1,11 @@
+import { autorun } from "mobx";
 import { observer } from "mobx-react";
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
+import { routes } from "~frontend/../router";
 import { useDb } from "~frontend/clientdb";
+import { TopicEntity } from "~frontend/clientdb/topic";
 
 import { TopicWithMessages } from "./TopicWithMessages";
 
@@ -10,9 +13,33 @@ interface Props {
   topicSlug: string;
 }
 
+/**
+ * In case topic slug changes while we have it opened, replace the router so we still have it opened.
+ */
+function useUpdateRouterIfSlugChanges(topic: TopicEntity | null) {
+  useEffect(() => {
+    if (!topic) return;
+
+    let isFirstRun = true;
+    return autorun(() => {
+      const slugNow = topic.slug;
+
+      if (isFirstRun) {
+        isFirstRun = false;
+        return;
+      }
+
+      routes.topic.replace({ topicSlug: slugNow });
+    });
+  }, [topic]);
+}
+
 export const RequestView = observer(({ topicSlug }: Props) => {
   const db = useDb();
   const topic = db.topic.findByUniqueIndex("slug", topicSlug);
+
+  useUpdateRouterIfSlugChanges(topic);
+
   return <UIHolder>{topic && <TopicWithMessages topic={topic} />}</UIHolder>;
 });
 
