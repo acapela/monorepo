7f08f801c207e3fb924e9f0d717be11fb4f88e29
Christoph Witzko <github@christophwitzko.com>
2021-10-13 16:00:37 +0200

    feat: add unicode/emoji slug support

--- shared/slugify.ts
+++ shared/slugify.ts
@@ -1,16 +1,8 @@
-export function slugify(text: string) {
-  const from = "ąàáäâèęéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;";
-  const __to = "aaaaaeeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------";
-  const specialCharsRegexp = new RegExp(from.split("").join("|"), "g");
+import _slugify from "@sindresorhus/slugify";
+import emojis from "emoji.json";
 
-  return text
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(specialCharsRegexp, (char) => __to.charAt(from.indexOf(char))) // Replace special chars
-    .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w-]+/g, "") // Remove all non-word chars
-    .replace(/--+/g, "-") // Replace multiple - with single -
-    .replace(/^-+/, "") // Trim - from start of text
-    .replace(/-+$/, ""); // Trim - from end of text
+const customReplacements = emojis.map(({ char, name }) => [char, name] as [string, string]);
+
+export function slugify(text: string) {
+  return _slugify(text, { customReplacements });
 }
