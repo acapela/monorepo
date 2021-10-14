aef70e9a74bd1d4acaa2d3c90c2b6759366171ed
Christoph Witzko <github@christophwitzko.com>
2021-10-14 11:11:25 +0200

    test(shared): add slugify tests

---
+++ shared/slugify.test.ts
@@ -0,0 +1,19 @@
+import { slugify } from "~shared/slugify";
+
+describe("slugify", () => {
+  const testCases = [
+    ["hello world", "hello-world"],
+    ["Hello World", "hello-world"],
+    ["hello     world", "hello-world"],
+    ["hello 🌍", "hello"],
+    ["🌍", "globe-showing-europe-africa"],
+    ["⎃", "U9091"],
+    ["⎋", "U9099"],
+  ];
+  for (const tc of testCases) {
+    it(`slugify title ${tc[0]}`, async () => {
+      const slug = await slugify(tc[0]);
+      expect(slug).toEqual(tc[1]);
+    });
+  }
+});
--- shared/slugify.ts
+++ shared/slugify.ts
@@ -3,7 +3,9 @@ import _slugify from "@sindresorhus/slugify";
 async function slugifyWithEmojis(text: string): Promise<string> {
   const emojis = await import("emoji.json");
   const customReplacements = emojis.default.map(({ char, name }) => [char, name] as [string, string]);
-  return _slugify(text, { customReplacements });
+  const slug = _slugify(text, { customReplacements });
+  if (slug.length != 0) return slug;
+  return `U${text.charCodeAt(0)}`;
 }
 
 export async function slugify(text: string): Promise<string> {
