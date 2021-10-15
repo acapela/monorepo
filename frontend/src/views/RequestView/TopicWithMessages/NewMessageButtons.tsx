d172037c284f5463938c1cf6b425f53d1721bd53
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-15 10:34:33 +0200

    wip: prepare action buttons

--- frontend/src/views/RequestView/TopicWithMessages/NewMessageButtons.tsx
+++ frontend/src/views/RequestView/TopicWithMessages/NewMessageButtons.tsx
@@ -1,22 +1,65 @@
 import { observer } from "mobx-react";
 import styled from "styled-components";
 
-import { Button } from "~frontend/../../ui/buttons/Button";
-import { theme } from "~frontend/../../ui/theme";
 import { TopicEntity } from "~frontend/clientdb/topic";
+import { Button } from "~ui/buttons/Button";
+import { theme } from "~ui/theme";
 
 interface Props {
   topic: TopicEntity;
+  onSendRequest?: () => void;
+  onCompleteRequest?: () => void;
+  onCloseRequest?: () => void;
 }
 
-export const NewMessageButtons = observer(({ topic }: Props) => {
+type AllowedActionName = "send" | "complete" | "close-request";
+
+type AllowedActionsInfo = {
+  actions: AllowedActionName[];
+  primaryAction: AllowedActionName;
+};
+
+function getAllowedActions(topic: TopicEntity): AllowedActionsInfo {
+  return {
+    actions: ["send", "complete"],
+    primaryAction: "complete",
+  };
+}
+
+export const NewMessageButtons = observer(({ topic, onSendRequest, onCompleteRequest, onCloseRequest }: Props) => {
+  const { actions, primaryAction } = getAllowedActions(topic);
   return (
     <UIHolder>
-      <Button shortcut={["Mod", "Enter"]}>Send</Button>
-      <Button shortcut={["Mod", "C"]}>Complete</Button>
-      <Button shortcut={["Mod", "X"]} kind="primary">
-        Close request
-      </Button>
+      {actions.includes("send") && (
+        <Button
+          shortcut={["Mod", "Enter"]}
+          kind={primaryAction === "send" ? "primary" : "secondary"}
+          tooltip="Send without completing pending tasks"
+          onClick={onSendRequest}
+        >
+          Send
+        </Button>
+      )}
+      {actions.includes("complete") && (
+        <Button
+          shortcut={["Mod", "C"]}
+          kind={primaryAction === "complete" ? "primary" : "secondary"}
+          tooltip="Send and complete pending tasks"
+          onClick={onCompleteRequest}
+        >
+          Complete
+        </Button>
+      )}
+      {actions.includes("close-request") && (
+        <Button
+          shortcut={["Mod", "X"]}
+          kind={primaryAction === "close-request" ? "primary" : "secondary"}
+          tooltip="Close entire request"
+          onClick={onCloseRequest}
+        >
+          Close request
+        </Button>
+      )}
     </UIHolder>
   );
 });
