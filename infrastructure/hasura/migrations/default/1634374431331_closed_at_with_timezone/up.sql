33c1a2686547805155c9235e5841dc3c8bd3413a
Omar Duarte <omarduarte@gmail.com>
2021-10-16 15:16:43 +0300

    fix: drop unread messages view permanently

--- infrastructure/hasura/migrations/default/1634374431331_closed_at_with_timezone/up.sql
+++ infrastructure/hasura/migrations/default/1634374431331_closed_at_with_timezone/up.sql
@@ -1,22 +1,8 @@
 -- Cannot alter type of column used by a view or rule
--- We begin dropping the view, alter column, then restoring view
+-- The unread messages view is not needed any more, it's removed before alter column
 
 BEGIN;  
   DROP VIEW "public"."unread_messages";
 
   ALTER TABLE "public"."topic" ALTER COLUMN "closed_at" TYPE timestamp with time zone;
-
-  CREATE VIEW "public"."unread_messages" AS
-    SELECT tm.user_id,
-           topic.team_id,
-           topic.id AS topic_id,
-           count(message.id) AS unread_messages
-    FROM (((team_member tm
-        LEFT JOIN topic ON ((topic.team_id = tm.team_id)))
-        LEFT JOIN last_seen_message ON (((last_seen_message.user_id = tm.user_id) AND (last_seen_message.topic_id = topic.id))))
-             LEFT JOIN message ON ((message.topic_id = topic.id)))
-    WHERE ((message.user_id <> tm.user_id) AND ((message.created_at > last_seen_message.seen_at) OR (last_seen_message.seen_at IS NULL))) AND topic.closed_at IS NULL
-    GROUP BY tm.user_id, topic.team_id, topic.id
-    ORDER BY tm.user_id, topic.team_id, topic.id;
-
 COMMIT;
