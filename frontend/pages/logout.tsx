42e62b2d6eb2f9c39ead64eec11a0e9ccc503ec9
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-19 00:55:08 +0200

    feat: use new action ui for auth and invitation views

--- frontend/pages/logout.tsx
+++ frontend/pages/logout.tsx
@@ -1,7 +1,9 @@
 import { signOut } from "next-auth/react";
 import { useEffect } from "react";
+import styled from "styled-components";
 
 import { trackEvent } from "~frontend/analytics/tracking";
+import { theme } from "~ui/theme";
 
 export default function LogoutPage() {
   useEffect(() => {
@@ -9,5 +11,15 @@ export default function LogoutPage() {
     signOut({ callbackUrl: "/" });
   }, []);
 
-  return <div>Logging out...</div>;
+  return <UIHolder>Logging out...</UIHolder>;
 }
+
+const UIHolder = styled.div`
+  display: flex;
+  height: 100vh;
+  ${theme.gradients.actionPageBg.asBg};
+  align-items: center;
+  justify-content: center;
+  ${theme.font.bold};
+  color: #fff;
+`;
