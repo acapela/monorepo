815d1644fcca95fd7538a3df4ea7c8f47309ae37
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-19 09:49:53 +0200

    fix: dont require team members to be able to create new request

--- frontend/src/views/NewRequestView/index.tsx
+++ frontend/src/views/NewRequestView/index.tsx
@@ -2,25 +2,19 @@ import { observer } from "mobx-react";
 import React from "react";
 import styled from "styled-components";
 
-import { useDb } from "~frontend/clientdb";
 import { DropFileContext } from "~richEditor/DropFileContext";
 import { ClientSideOnly } from "~ui/ClientSideOnly";
 
 import { NewRequest } from "./NewRequest";
 
 export const NewRequestView = observer(function NewRequestView() {
-  const db = useDb();
-  const hasTeamMembers = db.teamMember.all.length > 1;
-
   return (
     <>
-      {hasTeamMembers && (
-        <UIDropFileHolder>
-          <ClientSideOnly>
-            <NewRequest />
-          </ClientSideOnly>
-        </UIDropFileHolder>
-      )}
+      <UIDropFileHolder>
+        <ClientSideOnly>
+          <NewRequest />
+        </ClientSideOnly>
+      </UIDropFileHolder>
     </>
   );
 });
