ee171e68d1027e47544341823222140dffd185eb
Christoph Witzko <github@christophwitzko.com>
2021-10-20 14:50:19 +0200

    feat: autolink mentions from slack to acapela

--- backend/src/slack/createTopicForSlackUsers.ts
+++ backend/src/slack/createTopicForSlackUsers.ts
@@ -1,10 +1,11 @@
 import { sendInviteNotification } from "~backend/src/inviteUser";
 import { slackClient } from "~backend/src/slack/app";
+import { parseAndTransformToTipTapJSON } from "~backend/src/slack/slackMarkdown/parser";
 import { findUserBySlackId } from "~backend/src/slack/utils";
 import { Account, User, db } from "~db";
 import { convertMessageContentToPlainText } from "~richEditor/content/plainText";
-import { RichEditorNode } from "~richEditor/content/types";
 import { slugify } from "~shared/slugify";
+import { DEFAULT_TOPIC_TITLE_TRUNCATE_LENGTH, truncateTextWithEllipsis } from "~shared/text/ellipsis";
 import { MentionType } from "~shared/types/mention";
 
 async function createAndInviteMissingUsers(
@@ -48,7 +49,7 @@ async function createAndInviteMissingUsers(
   return usersWithSlackIds.map(({ slackUserId, user }) => ({ slackUserId, userId: user.id }));
 }
 
-type UserWithRequest = { userId: string; requestType: MentionType };
+type UserWithRequest = { userId: string; requestType: MentionType; slackUserId: string };
 
 type SlackUserIdWithRequestType = { slackUserId: string; requestType: MentionType };
 
@@ -70,11 +71,18 @@ export async function findOrCreateUsers({
       user: await findUserBySlackId(slackToken, slackUserId, teamId),
     }))
   );
-
   const userIds = usersForSlackIds
     .filter((item) => item.user)
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    .map((item) => ({ userId: item.user!.id, requestType: item.requestType } as const));
+
+    .map(
+      (item) =>
+        ({
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          userId: item.user!.id,
+          requestType: item.requestType,
+          slackUserId: item.slackUserId,
+        } as const)
+    );
 
   const missingUsersSlackIds = usersForSlackIds.filter((item) => !item.user).map((item) => item.slackUserId);
 
@@ -89,6 +97,7 @@ export async function findOrCreateUsers({
         userId: row.userId,
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         requestType: slackUserIdsWithRequestType.find((item) => item.slackUserId == row.slackUserId)!.requestType,
+        slackUserId: row.slackUserId,
       } as const)
   );
   return [...userIds, ...newUserIds];
@@ -98,15 +107,17 @@ export async function createTopicForSlackUsers({
   token,
   teamId,
   ownerId,
+  slackTeamId,
   topicName,
-  topicMessage,
+  rawTopicMessage,
   slackUserIdsWithRequestType,
 }: {
   token: string;
   teamId: string;
   ownerId: string;
-  topicName: string;
-  topicMessage: RichEditorNode;
+  slackTeamId: string;
+  topicName?: string;
+  rawTopicMessage: string;
   slackUserIdsWithRequestType: SlackUserIdWithRequestType[];
 }) {
   const usersWithRequestType = await findOrCreateUsers({
@@ -116,11 +127,31 @@ export async function createTopicForSlackUsers({
     slackUserIdsWithRequestType,
   });
 
+  const mentionedUsersBySlackId = Object.assign(
+    {},
+    ...usersWithRequestType.map((u) => {
+      return {
+        [u.slackUserId]: {
+          type: u.requestType,
+          userId: u.userId,
+        },
+      };
+    })
+  );
+
+  const topicMessage = parseAndTransformToTipTapJSON(rawTopicMessage, {
+    slackTeamId,
+    mentionedUsersBySlackId,
+  });
+  const topicMessagePlainText = convertMessageContentToPlainText(topicMessage);
+  const finalTopicName =
+    topicName || truncateTextWithEllipsis(topicMessagePlainText, DEFAULT_TOPIC_TITLE_TRUNCATE_LENGTH);
+
   return await db.topic.create({
     data: {
       team_id: teamId,
-      name: topicName,
-      slug: await slugify(topicName),
+      name: finalTopicName,
+      slug: await slugify(finalTopicName),
       index: "a",
       owner_id: ownerId,
       message: {
@@ -128,7 +159,7 @@ export async function createTopicForSlackUsers({
           type: "TEXT",
           user_id: ownerId,
           content: topicMessage,
-          content_text: convertMessageContentToPlainText(topicMessage),
+          content_text: topicMessagePlainText,
           task: {
             createMany: {
               data: usersWithRequestType.map((item) => ({
