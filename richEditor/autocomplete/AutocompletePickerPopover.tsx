a14549e462a296ee78a9564e7f759e49208a5c0a
Omar Duarte <omarduarte@gmail.com>
2021-10-21 17:57:30 +0300

    feat: animate new request page

--- richEditor/autocomplete/AutocompletePickerPopover.tsx
+++ richEditor/autocomplete/AutocompletePickerPopover.tsx
@@ -1,8 +1,10 @@
 import { NodeViewWrapper } from "@tiptap/react";
 import { SuggestionProps } from "@tiptap/suggestion";
-import { ComponentType, useState } from "react";
+import { ComponentType, useRef, useState } from "react";
 
+import { useBoolean } from "~shared/hooks/useBoolean";
 import { useComparingEffect } from "~shared/hooks/useComparingEffect";
+import { useResizeCallback } from "~shared/hooks/useResizeCallback";
 import { useValueRef } from "~shared/hooks/useValueRef";
 import { PopPresenceAnimator } from "~ui/animations";
 import { useShortcut } from "~ui/keyboard/useShortcut";
@@ -17,10 +19,28 @@ interface Props<D> {
 
 export function AutocompletePickerPopoverBase<D>({ baseProps, PickerComponent }: Props<D>) {
   const anchorRef = useValueRef(baseProps.decorationNode as HTMLElement);
-  const [isHidden, setIsHidden] = useState(false);
+  const editorRef = useRef<HTMLElement>(baseProps.editor.view.dom as HTMLElement);
+
+  const [isOpen, { set: open, unset: hide }] = useBoolean(true);
+  const [popoverDelayedOpenTimerRef, setPopoverDelayedOpenTimerRef] = useState<null | number>(null);
+
+  // If the editor resizes at any point, close all popovers until resize is done
+  // This prevents the popover to be left floating on the wrong place
+  useResizeCallback(editorRef, () => {
+    // Cancels any open timers. Happens if resize is still not done
+    if (popoverDelayedOpenTimerRef) {
+      clearTimeout(popoverDelayedOpenTimerRef);
+    }
+
+    hide();
+
+    // Wait 100ms until we can check if the resize is done
+    const timeoutCancel = window.setTimeout(() => open(), 100);
+    setPopoverDelayedOpenTimerRef(timeoutCancel);
+  });
 
   useComparingEffect((queryNow, queryBefore) => {
-    if (!isHidden) {
+    if (isOpen) {
       return;
     }
 
@@ -30,16 +50,16 @@ export function AutocompletePickerPopoverBase<D>({ baseProps, PickerComponent }:
     }
 
     // We started typing something new, re-show the popover.
-    setIsHidden(false);
+    open();
   }, baseProps.query);
 
   useShortcut("Escape", () => {
-    setIsHidden(true);
+    hide();
   });
 
   return (
     <NodeViewWrapper className="picker">
-      {!isHidden && (
+      {isOpen && (
         <Popover anchorRef={anchorRef} placement="top-start">
           <PopPresenceAnimator>
             <PickerComponent
