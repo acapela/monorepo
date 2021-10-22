e0cf8cec75e1075a70d5d445671df3a3f893aa0f
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-22 09:54:07 +0200

    feat: updated at index in db for all missing tables

---
+++ infrastructure/hasura/migrations/default/1634889129443_indexes_for_updated_at_columns/down.sql
@@ -0,0 +1,20 @@
+
+DROP INDEX IF EXISTS "user_updated_at_key";
+
+DROP INDEX IF EXISTS "transcription_updated_at_key";
+
+DROP INDEX IF EXISTS "team_slack_installation_updated_at_key";
+
+DROP INDEX IF EXISTS "team_member_slack_installation_updated_at_key";
+
+DROP INDEX IF EXISTS "team_member_updated_at_key";
+
+DROP INDEX IF EXISTS "team_updated_at_key";
+
+DROP INDEX IF EXISTS "message_reaction_updated_at_key";
+
+DROP INDEX IF EXISTS "last_seen_message_updated_at_key";
+
+DROP INDEX IF EXISTS "attachment_updated_at_key";
+
+DROP INDEX IF EXISTS "account_updated_at_key";
