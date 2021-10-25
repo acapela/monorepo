69d88b0f4c204e1c70b5fc4c144349b233b51c92
Omar Duarte <omarduarte@gmail.com>
2021-10-25 16:05:16 +0300

    feat: sending slack notification also accepts blocks

--- backend/src/notifications/sendNotification.ts
+++ backend/src/notifications/sendNotification.ts
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/node";
+import { Block, KnownBlock } from "@slack/bolt";
 
 import { slackClient } from "~backend/src/slack/app";
 import { fetchTeamBotToken, findSlackUserId } from "~backend/src/slack/utils";
@@ -6,12 +7,18 @@ import { User, db } from "~db";
 import { assertDefined } from "~shared/assert";
 import { DEFAULT_NOTIFICATION_EMAIL, sendEmail } from "~shared/email";
 
-async function trySendSlackNotification(teamId: string, user: User, text: string) {
+async function trySendSlackNotification(teamId: string, user: User, payload: string | (KnownBlock | Block)[]) {
   const [token, slackUserId] = await Promise.all([fetchTeamBotToken(teamId), findSlackUserId(teamId, user)]);
   if (!token || !slackUserId) {
     return;
   }
-  await slackClient.chat.postMessage({ token, channel: slackUserId, text });
+
+  const textOrBlocks = typeof payload === "string" ? { text: payload } : { blocks: payload };
+  await slackClient.chat.postMessage({
+    token,
+    channel: slackUserId,
+    ...textOrBlocks,
+  });
 }
 
 /*
@@ -27,7 +34,7 @@ export async function sendNotificationPerPreference(
       subject: string;
       html: string;
     };
-    slack: string;
+    slack: string | (KnownBlock | Block)[];
   }
 ): Promise<unknown> {
   const teamMember = assertDefined(
