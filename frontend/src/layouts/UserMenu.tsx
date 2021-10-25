e9beeee43a88d9211b4e5834aaea3d987316d505
Adam Pietrasiak <adam@pietrasiak.com>
2021-10-25 16:31:25 +0200

    feat: no 3-dots next to user avatar for menu

--- frontend/src/layouts/UserMenu.tsx
+++ frontend/src/layouts/UserMenu.tsx
@@ -6,16 +6,15 @@ import styled from "styled-components";
 import { useAssertCurrentUser } from "~frontend/authentication/useCurrentUser";
 import { UserAvatar } from "~frontend/ui/users/UserAvatar";
 import { routes } from "~shared/routes";
-import { IconButton } from "~ui/buttons/IconButton";
-import { IconExternalLink, IconGear, IconMoreHoriz, IconSignOut, IconUsers } from "~ui/icons";
+import { IconExternalLink, IconGear, IconSignOut, IconUsers } from "~ui/icons";
 import { PopoverMenuTrigger } from "~ui/popovers/PopoverMenuTrigger";
+import { theme } from "~ui/theme";
 
 export const UserMenu = observer(function UserMenu() {
   const user = useAssertCurrentUser();
 
   return (
     <UIHolder>
-      <UserAvatar user={user} size={30} disableNameTooltip />
       <PopoverMenuTrigger
         options={[
           {
@@ -43,14 +42,44 @@ export const UserMenu = observer(function UserMenu() {
           },
         ]}
       >
-        <IconButton icon={<IconMoreHoriz />} />
+        <UserAvatar user={user} size={30} disableNameTooltip />
       </PopoverMenuTrigger>
+      <UIHoverIndicator />
     </UIHolder>
   );
 });
 
+const HOVER_OFFSET_PX = 2;
+
+const UIHoverIndicator = styled.div`
+  position: absolute;
+  top: ${-HOVER_OFFSET_PX}px;
+  left: ${-HOVER_OFFSET_PX}px;
+  right: ${-HOVER_OFFSET_PX}px;
+  bottom: ${-HOVER_OFFSET_PX}px;
+  ${theme.colors.layout.backgroundAccent.active.asBg};
+  ${theme.radius.circle};
+  z-index: 1;
+`;
+
 const UIHolder = styled.div<{}>`
-  display: flex;
-  align-items: center;
-  gap: 8px;
+  position: relative;
+  ${UserAvatar} {
+    position: relative;
+    cursor: pointer;
+    z-index: 2;
+  }
+
+  ${UIHoverIndicator} {
+    opacity: 0;
+    transform: scale(1.2);
+    transition: 0.2s all;
+  }
+
+  &:hover {
+    ${UIHoverIndicator} {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
 `;
