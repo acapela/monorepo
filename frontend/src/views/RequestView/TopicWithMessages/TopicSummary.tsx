65b8089e20b7174c0d90f2a8122d5c8bbfbf71cf
Omar Duarte <omarduarte@gmail.com>
2021-10-26 15:30:20 +0300

    feat: add different closed and archived notices

--- frontend/src/views/RequestView/TopicWithMessages/TopicSummary.tsx
+++ frontend/src/views/RequestView/TopicWithMessages/TopicSummary.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { TopicEntity } from "~frontend/clientdb/topic";
 import { UserAvatar } from "~frontend/ui/users/UserAvatar";
 import { styledObserver } from "~shared/component";
+import { TextButton } from "~ui/buttons/TextButton";
+import { IconAcapelaWave } from "~ui/icons";
 import { CircleLabel } from "~ui/icons/CircleLabel";
 import { theme } from "~ui/theme";
 import { TimeLabelWithDateTooltip } from "~ui/time/DateLabel";
@@ -13,8 +15,14 @@ type Props = {
   className?: string;
 };
 
+const TextAction = (props: Omit<React.ComponentProps<typeof TextButton>, "kind" | "inline">) => (
+  <TextButton {...props} kind="primary" inline />
+);
+
 // Temporary, to be deleted
-export const TopicSummaryMessage = styledObserver<Props>(({ topic, className }) => {
+// Parts of this will be copied over to make the event system work
+export const TopicSummaryMessage = styledObserver<Props>((props) => {
+  const { topic, className } = props;
   const { closedByUser, closed_at } = topic;
 
   if (!topic.isClosed) {
@@ -24,24 +32,74 @@ export const TopicSummaryMessage = styledObserver<Props>(({ topic, className }) 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const closedAtDate = new Date(closed_at!);
 
-  if (!closedByUser) {
-    return (
-      <UIHolder className={className}>
-        <UIHead>
-          <UICheckCircle label={<>✔</>} />
-          <UIHead>Hurray! The request has been completed by everyone and has been Closed</UIHead>
-          <UISideTimeLabel date={closedAtDate} />
-        </UIHead>
-      </UIHolder>
-    );
+  return (
+    <>
+      {closedByUser && (
+        <UIHolder className={className}>
+          <UIHead>
+            <UserAvatar user={closedByUser} size={20} />
+            <div>Closed the request</div>
+            <UISideTimeLabel date={closedAtDate} />
+          </UIHead>
+        </UIHolder>
+      )}
+      {!closedByUser && (
+        <UIHolder className={className}>
+          <UIHead>
+            <UICheckCircle label={<IconAcapelaWave />} />
+            <div>
+              Hurray! <UIBold>{topic.name}</UIBold> has been completed by everyone and has been <UIBold>Closed</UIBold>.
+            </div>
+            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
+            <UISideTimeLabel date={new Date(topic.closed_at!)} />
+          </UIHead>
+        </UIHolder>
+      )}
+
+      {!topic.isArchived && <TopicArchiveNotice {...props} />}
+
+      {topic.isArchived && (
+        <UIHolder className={className}>
+          <UIHead>
+            <UICheckCircle label={<IconAcapelaWave />} />
+            <div>
+              <UIBold>{topic.name}</UIBold> is archived.
+            </div>
+            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
+            <UISideTimeLabel date={new Date(topic.archived_at!)} />
+          </UIHead>
+        </UIHolder>
+      )}
+    </>
+  );
+})``;
+
+const TopicArchiveNotice = styledObserver<Props>(({ topic, className }: Props) => {
+  function handleReopen() {
+    topic.update({ closed_at: null, closed_by_user_id: null });
+  }
+
+  function handleArchive() {
+    topic.update({ archived_at: new Date().toISOString() });
   }
 
   return (
     <UIHolder className={className}>
       <UIHead>
-        <UserAvatar user={closedByUser} size={20} />
-        <div>Closed the request</div>
-        <UISideTimeLabel date={closedAtDate} />
+        <UICheckCircle label={<IconAcapelaWave />} />
+
+        <div>
+          <UIBold>{topic.name}</UIBold> will be{" "}
+          <UIArchiveTooltip data-tooltip="Archived requests are available through the search bar">
+            archived
+          </UIArchiveTooltip>{" "}
+          automatically in the next weekday. <br /> You could also{" "}
+          <TextAction onClick={handleReopen}>Reopen</TextAction> or{" "}
+          <TextAction onClick={handleArchive}>Archive now</TextAction>.
+        </div>
+
+        {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
+        <UISideTimeLabel date={new Date(topic.closed_at!)} />
       </UIHead>
     </UIHolder>
   );
@@ -56,7 +114,7 @@ const UIHead = styled.div<{}>`
   flex-direction: row;
   align-items: center;
   ${theme.spacing.horizontalActions.asGap}
-  ${theme.font.semibold}
+  ${theme.font.medium}
 `;
 
 const UISideTimeLabel = styled(TimeLabelWithDateTooltip)<{}>`
@@ -66,4 +124,13 @@ const UISideTimeLabel = styled(TimeLabelWithDateTooltip)<{}>`
 const UICheckCircle = styled(CircleLabel)<{}>`
   height: 20px;
   width: 20px;
+`;
+
+const UIArchiveTooltip = styled.span<{}>`
+  text-decoration: underline;
+  cursor: default;
+`;
+
+const UIBold = styled.span<{}>`
+  ${theme.font.black}
 `;
