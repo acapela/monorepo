0f91b3a7a7db95823459e4035d51a8cb5b78085e
Omar Duarte <omarduarte@gmail.com>
2021-10-26 13:44:10 +0300

    feat: handle topic closed automatically in UI

--- frontend/src/views/RequestView/TopicWithMessages/TopicSummary.tsx
+++ frontend/src/views/RequestView/TopicWithMessages/TopicSummary.tsx
@@ -2,44 +2,68 @@ import React from "react";
 import styled from "styled-components";
 
 import { TopicEntity } from "~frontend/clientdb/topic";
-import { MessageLikeContent } from "~frontend/message/feed/MessageLikeContent";
+import { UserAvatar } from "~frontend/ui/users/UserAvatar";
 import { styledObserver } from "~shared/component";
-import { niceFormatDate } from "~shared/dates/format";
+import { CircleLabel } from "~ui/icons/CircleLabel";
 import { theme } from "~ui/theme";
+import { TimeLabelWithDateTooltip } from "~ui/time/DateLabel";
 
 type Props = {
   topic: TopicEntity;
   className?: string;
 };
 
+// Temporary, to be deleted
 export const TopicSummaryMessage = styledObserver<Props>(({ topic, className }) => {
   const { closedByUser, closed_at } = topic;
 
-  if (!closedByUser || !closed_at) {
+  if (!topic.isClosed) {
     return null;
   }
 
-  const closedAtDate = new Date(closed_at);
-  return (
-    <MessageLikeContent user={closedByUser} date={closedAtDate} className={className}>
-      <UIHolder>
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const closedAtDate = new Date(closed_at!);
+
+  if (!closedByUser) {
+    return (
+      <UIHolder className={className}>
         <UIHead>
-          Request was closed by {closedByUser.name} on {niceFormatDate(closedAtDate)} 🎉
+          <UICheckCircle label={<>✔</>} />
+          <UIHead>Hurray! The request has been completed by everyone and has been Closed</UIHead>
+          <UISideTimeLabel date={closedAtDate} />
         </UIHead>
       </UIHolder>
-    </MessageLikeContent>
+    );
+  }
+
+  return (
+    <UIHolder className={className}>
+      <UIHead>
+        <UserAvatar user={closedByUser} size={20} />
+        <div>Closed the request</div>
+        <UISideTimeLabel date={closedAtDate} />
+      </UIHead>
+    </UIHolder>
   );
 })``;
 
 const UIHolder = styled.div<{}>`
-  padding: 10px;
-
-  /* TODO PR */
-  background: #f0faf5;
-  border: 1px solid #d7f4e4;
-  ${theme.radius.panel};
+  padding: 20px 0;
 `;
 
 const UIHead = styled.div<{}>`
-  font-weight: bold;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  ${theme.spacing.horizontalActions.asGap}
+  ${theme.font.semibold}
+`;
+
+const UISideTimeLabel = styled(TimeLabelWithDateTooltip)<{}>`
+  ${theme.typo.content.secondary};
+`;
+
+const UICheckCircle = styled(CircleLabel)<{}>`
+  height: 20px;
+  width: 20px;
 `;
