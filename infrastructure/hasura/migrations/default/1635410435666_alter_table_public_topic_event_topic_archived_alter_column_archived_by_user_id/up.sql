fe8b428a399d0827b6c624ff6f116824eb5c897c
Omar Duarte <omarduarte@gmail.com>
2021-10-28 12:06:27 +0300

    fix: user nullable for other topic event tables

---
+++ infrastructure/hasura/migrations/default/1635410435666_alter_table_public_topic_event_topic_archived_alter_column_archived_by_user_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."topic_event_topic_archived" alter column "archived_by_user_id" drop not null;
---
+++ infrastructure/hasura/migrations/default/1635411776687_alter_table_public_topic_event_topic_unarchived_alter_column_unarchived_by_user_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."topic_event_topic_unarchived" alter column "unarchived_by_user_id" drop not null;
