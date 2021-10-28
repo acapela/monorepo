f1b8b3b79db5e7822c4bef70bd4376dda891b4e7
Omar Duarte <omarduarte@gmail.com>
2021-10-28 11:13:27 +0300

    chore: refactor topic updates notifications

--- backend/src/topics/events.ts
+++ backend/src/topics/events.ts
@@ -7,34 +7,47 @@ import { createClosureNotificationMessage } from "../notifications/bodyBuilders/
 import { sendNotificationPerPreference } from "../notifications/sendNotification";
 
 export async function handleTopicUpdates(event: HasuraEvent<Topic>) {
+  const topicId = event.item.id;
+  const topic = await db.topic.findUnique({ where: { id: topicId } });
+  assert(topic, `Topic id (id=${topicId}) provided by event was not found`);
+
   if (event.type === "update") {
-    const ownerId = event.item.owner_id;
-    const userIdThatClosedTopic = event.item.closed_by_user_id;
-
-    const isClosedByOwner = ownerId === userIdThatClosedTopic;
-    const wasJustClosed = event.item.closed_at && !event.itemBefore.closed_at;
-    if (wasJustClosed && !isClosedByOwner) {
-      const topicOwner = await db.user.findFirst({ where: { id: ownerId } });
-      const topicCloser = userIdThatClosedTopic
-        ? await db.user.findFirst({ where: { id: userIdThatClosedTopic as string } })
-        : null;
-
-      assert(topicOwner, `[Closing Topic][id=${event.item.id}] Owner ${ownerId} not found.`);
-
-      const topicURL = `${process.env.FRONTEND_URL}${routes.topic({ topicSlug: event.item.slug })}`;
-
-      const topicName = event.item.name;
-
-      sendNotificationPerPreference(
-        topicOwner,
-        event.item.team_id,
-        createClosureNotificationMessage({
-          closedBy: topicCloser?.name,
-          topicId: event.item.id,
-          topicName,
-          topicURL,
-        })
-      );
-    }
+    notifyTopicUpdates(event, topic);
   }
+}
+
+function notifyTopicUpdates(event: HasuraEvent<Topic>, topic: Topic) {
+  const ownerId = topic.owner_id;
+  const userIdThatClosedTopic = topic.closed_by_user_id;
+
+  const isClosedByOwner = ownerId === userIdThatClosedTopic;
+  const wasJustClosed = topic.closed_at && !event?.itemBefore?.closed_at;
+
+  if (wasJustClosed && !isClosedByOwner) {
+    notifyOwnerOfTopicClosure(ownerId, userIdThatClosedTopic as string, topic);
+  }
+}
+
+async function notifyOwnerOfTopicClosure(ownerId: string, userIdThatClosedTopic: string, topic: Topic) {
+  const topicOwner = await db.user.findFirst({ where: { id: ownerId } });
+  const topicCloser = userIdThatClosedTopic
+    ? await db.user.findFirst({ where: { id: userIdThatClosedTopic as string } })
+    : null;
+
+  assert(topicOwner, `[Closing Topic][id=${topic.id}] Owner ${ownerId} not found.`);
+
+  const topicURL = `${process.env.FRONTEND_URL}${routes.topic({ topicSlug: topic.slug })}`;
+
+  const topicName = topic.name;
+
+  sendNotificationPerPreference(
+    topicOwner,
+    topic.team_id,
+    createClosureNotificationMessage({
+      closedBy: topicCloser?.name,
+      topicId: topic.id,
+      topicName,
+      topicURL,
+    })
+  );
 }
