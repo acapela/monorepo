d0a5b6ef60c0aea2f2e9140bcdf33381ad5f4af8
Omar Duarte <omarduarte@gmail.com>
2021-10-28 14:06:25 +0300

    infra: add relationships and permissions + type field

---
+++ infrastructure/hasura/migrations/default/1635417091415_set_fk_public_topic_event_topic_closed_topic_event_id/up.sql
@@ -0,0 +1,5 @@
+alter table "public"."topic_event_topic_closed" drop constraint "topic_event_topic_closed_topic_event_id_fkey",
+  add constraint "topic_event_topic_closed_topic_event_id_fkey"
+  foreign key ("topic_event_id")
+  references "public"."topic_event"
+  ("id") on update cascade on delete cascade;
---
+++ infrastructure/hasura/migrations/default/1635417279807_alter_table_public_topic_event_add_column_type/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."topic_event" add column "type" text
+ not null;
