8674e450d907f611dc8fe0d580cc6c94e24b2089
Christoph Witzko <github@christophwitzko.com>
2021-10-29 10:49:28 +0200

    ci: add SENTRY_AUTH_TOKEN to docker build args

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -12,9 +12,9 @@ IMAGE_NAME="eu.gcr.io/meetnomoreapp/monorepo"
 IMAGE_NAME_VERSION="$IMAGE_NAME:$VERSION"
 
 echo "building image..."
-docker build --build-arg BUILD_ID=$GITHUB_SHA --build-arg BUILD_DATE="$(date '+%Y-%m-%d')" --build-arg SENTRY_RELEASE=$VERSION -t $IMAGE_NAME_VERSION .
-docker tag $IMAGE_NAME_VERSION $IMAGE_NAME
+docker build --build-arg SENTRY_AUTH_TOKEN="$SENTRY_AUTH_TOKEN" --build-arg BUILD_ID="$GITHUB_SHA" --build-arg BUILD_DATE="$(date '+%Y-%m-%d')" --build-arg SENTRY_RELEASE=$VERSION -t $IMAGE_NAME_VERSION .
+docker tag "$IMAGE_NAME_VERSION" "$IMAGE_NAME"
 
 echo "pushing to gcr..."
-docker push $IMAGE_NAME_VERSION
-docker push $IMAGE_NAME
+docker push "$IMAGE_NAME_VERSION"
+docker push "$IMAGE_NAME"
