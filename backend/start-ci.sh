8fcf8e06b39591bd4aed0bcc314d3d028d440cad
Christoph Witzko <github@christophwitzko.com>
2021-11-03 14:30:56 +0100

    ci: upload e2e test logs

--- backend/start-ci.sh
+++ backend/start-ci.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nohup yarn ts-node --transpile-only ./src/index.ts & echo $! > backend.pid
+nohup yarn ts-node --transpile-only ./src/index.ts > backend.log 2>&1 & echo $! > backend.pid
