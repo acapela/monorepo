cdf8e8dd9020db73916815fcd49df5861dc429b3
Christoph Witzko <github@christophwitzko.com>
2021-11-03 14:56:33 +0100

    ci: more logging

--- frontend/start-ci.sh
+++ frontend/start-ci.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "starting frontend..."
 nohup yarn node ./next-server.js > frontend.log 2>&1 & echo $! > frontend.pid
+
+echo "frontend should now be running in the background."
+cat frontend.log
