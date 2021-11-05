e0fe45dc88bd069557e597c14d7ab96d7295032a
Christoph Witzko <github@christophwitzko.com>
2021-11-05 20:24:54 +0100

    feat remove frontend tracking lib

--- frontend/pages/logout.tsx
+++ frontend/pages/logout.tsx
@@ -1,12 +1,10 @@
 import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 
-import { trackEvent } from "~frontend/analytics/tracking";
 import { FocusedActionLayout } from "~frontend/layouts/FocusedActionLayout/FocusedActionLayout";
 
 export default function LogoutPage() {
   useEffect(() => {
-    trackEvent("Signed Out");
     signOut({ callbackUrl: "/" });
   }, []);
 
