beb2c1852e6e2d53233d03f240685318724c027e
Omar Duarte <omarduarte@gmail.com>
2021-11-09 18:05:52 +0200

    feat: clientdb/messageTaskDueDate entity

---
+++ frontend/src/clientdb/messageTaskDueDate.tsx
@@ -0,0 +1,37 @@
+import gql from "graphql-tag";
+
+import { EntityByDefinition, defineEntity } from "~clientdb";
+import { MessageTaskDueDateFragment } from "~gql";
+
+import { getFragmentKeys } from "./utils/analyzeFragment";
+import { getGenericDefaultData } from "./utils/getGenericDefaultData";
+import { createHasuraSyncSetupFromFragment } from "./utils/sync";
+
+const messageTaskDueDateFragment = gql`
+  fragment MessageTaskDueDate on message_task_due_date {
+    id
+    message_id
+    due_date
+    created_at
+    updated_at
+  }
+`;
+
+export const messageTaskDueDateEntity = defineEntity<MessageTaskDueDateFragment>({
+  name: "message_task_due_date",
+  updatedAtField: "updated_at",
+  keyField: "id",
+  keys: getFragmentKeys<MessageTaskDueDateFragment>(messageTaskDueDateFragment),
+  defaultSort: (event) => new Date(event.created_at).getTime(),
+  getDefaultValues: () => ({
+    __typename: "message_task_due_date",
+    ...getGenericDefaultData(),
+  }),
+  sync: createHasuraSyncSetupFromFragment<MessageTaskDueDateFragment>(messageTaskDueDateFragment, {
+    insertColumns: ["id", "message_id", "due_date", "created_at", "updated_at"],
+    updateColumns: ["due_date"],
+    teamScopeCondition: (teamId) => ({ message: { topic: { team_id: { _eq: teamId } } } }),
+  }),
+});
+
+export type MessageTaskDueDateEntity = EntityByDefinition<typeof messageTaskDueDateEntity>;
