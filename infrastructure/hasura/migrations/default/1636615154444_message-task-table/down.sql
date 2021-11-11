c832c01777bef8e4f15993f050879468743414f6
Omar Duarte <omarduarte@gmail.com>
2021-11-11 09:48:24 +0200

    chore: update down migration

--- infrastructure/hasura/migrations/default/1636615154444_message-task-table/down.sql
+++ infrastructure/hasura/migrations/default/1636615154444_message-task-table/down.sql
@@ -2,15 +2,6 @@
 alter table "public"."task" alter column "due_at" drop not null;
 alter table "public"."task" add column "due_at" timestamptz;
 
-alter table "public"."task" drop constraint "task_message_task_due_date_id_fkey",
-  add constraint "task_message_task_due_date_id_fkey"
-  foreign key ("message_task_due_date_id")
-  references "public"."message_task_due_date"
-  ("id") on update set null on delete set null;
-
-
 alter table "public"."task" drop constraint "task_id_key";
-
-alter table "public"."task" drop constraint "task_message_task_due_date_id_fkey";
 
 DROP TABLE "public"."message_task_due_date";
