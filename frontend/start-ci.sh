d789fdc0a0279ea490ce5892e2b52c0a6809ab06
Christoph Witzko <github@christophwitzko.com>
2021-11-12 15:37:30 +0100

    feat: add caddy as resverse proxy

--- frontend/start-ci.sh
+++ frontend/start-ci.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export NODE_ENV=production
 echo "starting frontend..."
-nohup yarn node ./next-server.js > frontend.log 2>&1 & echo $! > frontend.pid
+nohup yarn next:start > frontend.log 2>&1 & echo $! > frontend.pid
 
 sleep 1
 echo "frontend should now be running in the background."
