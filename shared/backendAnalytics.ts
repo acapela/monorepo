bdea8210e15ecb17eb44612881fd50db7987f377
Heiki Riesenkampf <me@heikir.com>
2021-11-15 12:00:02 +0100

    fix: simplify tracking call type

--- shared/backendAnalytics.ts
+++ shared/backendAnalytics.ts
@@ -10,7 +10,7 @@ function getAnalyticsProfileFromDbUser(user: User): AnalyticsUserProfile {
     email: user.email,
     name: user.name,
     createdAt: user.created_at,
-    avatar: user.avatar_url ?? undefined,
+    avatar: user.avatar_url,
   };
 }
 
