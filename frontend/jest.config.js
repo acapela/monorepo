da9f5b1e4fecf2ad9eec45673a2b0229974630ab
Christoph Witzko <github@christophwitzko.com>
2021-11-17 12:28:45 +0100

    test: fix frontend tests and use next/jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,15 +1,12 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = require("next/jest")()({
   collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
   setupFilesAfterEnv: ["<rootDir>/src/testSupport/setupTests.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/../node_modules/babel-jest",
     "^.+\\.css$": "<rootDir>/src/testSupport/cssTransform.ts",
   },
-  transformIgnorePatterns: ["/node_modules/(?!~).+\\.js$", "^.+\\.module\\.(css|sass|scss)$"],
   moduleNameMapper: {
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
     "~frontend/(.*)": "<rootDir>/src/$1",
   },
   testEnvironment: "jsdom",
-};
+})();
