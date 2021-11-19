a167f6966211818cfb83a2d6374ac28a5cc6245e
Omar Duarte <omarduarte@gmail.com>
2021-11-19 15:18:22 +0200

    chore(Sidebar): move archived groups builder

--- frontend/src/layouts/SidebarLayout/RequestFeed/RequestFeedGroups/RequestFeedGroups.tsx
+++ frontend/src/layouts/SidebarLayout/RequestFeed/RequestFeedGroups/RequestFeedGroups.tsx
@@ -1,22 +1,9 @@
-import {
-  endOfDay,
-  endOfMonth,
-  format,
-  isAfter,
-  startOfDay,
-  startOfMonth,
-  startOfWeek,
-  subBusinessDays,
-  subWeeks,
-} from "date-fns";
-import { subMonths } from "date-fns/esm";
 import { AnimatePresence, motion } from "framer-motion";
 import { observer } from "mobx-react";
-import React, { useMemo, useRef } from "react";
+import React, { useRef } from "react";
 import styled, { css } from "styled-components";
 
 import { TopicEntity } from "~frontend/clientdb/topic";
-import { groupByFilter } from "~shared/groupByFilter";
 import { useBoolean } from "~shared/hooks/useBoolean";
 import { useBoundingBox } from "~shared/hooks/useBoundingBox";
 import { IconChevronDown } from "~ui/icons";
@@ -24,14 +11,12 @@ import { theme } from "~ui/theme";
 
 import { RequestsGroup, RequestsGroupProps } from "../RequestsGroup";
 import { prepareTopicsGroups } from "./prepareTopicsGroups";
+import { useArchivedGroups } from "./useArchivedGroups";
 
 interface Props {
   topics: TopicEntity[];
   showArchived?: boolean;
 }
-
-// TODO; optimize enough to show more topics
-const SHOW_RECENT_ONLY = true;
 
 const ANIMATION_DURATION = 0.55;
 
@@ -45,66 +30,7 @@ export const RequestFeedGroups = observer(({ topics, showArchived = false }: Pro
   const [isShowingArchivedTimeline, { toggle: toggleShowArchived }] = useBoolean(false);
 
   // MBA m1 2021 -> 26 ms to process
-  const archivedGroups: RequestsGroupProps[] = useMemo(() => {
-    const groups: RequestsGroupProps[] = [];
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const toArchivedDate = (topic: TopicEntity) => new Date(topic.archived_at!);
-    const happenedWithin = (compareDate: Date) => (topic: TopicEntity) => isAfter(toArchivedDate(topic), compareDate);
-
-    const now = new Date();
-    const sameTimeYesterday = subBusinessDays(now, 1);
-    const sameTimeLastWeek = subWeeks(now, 1);
-    const sameTimeLastMonth = subMonths(now, 1);
-
-    const [today, notToday] = groupByFilter(archived, happenedWithin(startOfDay(now)));
-    if (today.length > 0) {
-      groups.push({ groupName: "Today", topics: today });
-    }
-
-    const [yesterday, notYesterday] = groupByFilter(notToday, happenedWithin(startOfDay(sameTimeYesterday)));
-    if (yesterday.length > 0) {
-      groups.push({ groupName: "Yesterday", topics: yesterday });
-    }
-
-    const [thisWeek, notThisWeek] = groupByFilter(notYesterday, happenedWithin(startOfWeek(now)));
-    if (thisWeek.length > 0) {
-      groups.push({ groupName: "Rest of this week", topics: thisWeek });
-    }
-
-    const [lastWeek, notLastWeek] = groupByFilter(notThisWeek, happenedWithin(startOfWeek(sameTimeLastWeek)));
-    if (lastWeek.length > 0) {
-      groups.push({ groupName: "Last week", topics: lastWeek });
-    }
-
-    if (SHOW_RECENT_ONLY) {
-      return groups;
-    }
-
-    const [restOfMonth, allRemaining] = groupByFilter(notLastWeek, happenedWithin(startOfMonth(sameTimeLastMonth)));
-    if (restOfMonth.length > 0) {
-      groups.push({ groupName: "Rest of month", topics: lastWeek });
-    }
-
-    let remainingDays = allRemaining;
-
-    while (remainingDays.length > 0) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const beginningOfMonth = new Date(remainingDays[0].archived_at!);
-
-      const previousMonth = endOfMonth(endOfDay(subMonths(beginningOfMonth, 1)));
-
-      const [month, remaining] = groupByFilter(remainingDays, happenedWithin(previousMonth));
-      if (month.length > 0) {
-        const groupName = format(beginningOfMonth, "MMMM uuuu");
-        groups.push({ groupName, topics: month });
-      }
-
-      remainingDays = remaining;
-    }
-
-    return groups;
-  }, [archived]);
+  const archivedGroups: RequestsGroupProps[] = useArchivedGroups(archived);
 
   return (
     <UIHolder ref={ref} data-test-id="sidebar-all-request-groups">
---
+++ frontend/src/layouts/SidebarLayout/RequestFeed/RequestFeedGroups/useArchivedGroups.ts
@@ -0,0 +1,86 @@
+import {
+  endOfDay,
+  endOfMonth,
+  format,
+  isAfter,
+  startOfDay,
+  startOfMonth,
+  startOfWeek,
+  subBusinessDays,
+  subMonths,
+  subWeeks,
+} from "date-fns";
+import { useMemo } from "react";
+
+import { TopicEntity } from "~frontend/clientdb/topic";
+import { groupByFilter } from "~shared/groupByFilter";
+
+import { RequestsGroupProps } from "../RequestsGroup";
+
+// TODO; optimize enough to show more topics
+const SHOW_RECENT_ONLY = true;
+
+export const useArchivedGroups = (archived: TopicEntity[]) => {
+  const archivedGroups = useMemo(() => {
+    const groups: RequestsGroupProps[] = [];
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const toArchivedDate = (topic: TopicEntity) => new Date(topic.archived_at!);
+    const happenedWithin = (compareDate: Date) => (topic: TopicEntity) => isAfter(toArchivedDate(topic), compareDate);
+
+    const now = new Date();
+    const sameTimeYesterday = subBusinessDays(now, 1);
+    const sameTimeLastWeek = subWeeks(now, 1);
+    const sameTimeLastMonth = subMonths(now, 1);
+
+    const [today, notToday] = groupByFilter(archived, happenedWithin(startOfDay(now)));
+    if (today.length > 0) {
+      groups.push({ groupName: "Today", topics: today });
+    }
+
+    const [yesterday, notYesterday] = groupByFilter(notToday, happenedWithin(startOfDay(sameTimeYesterday)));
+    if (yesterday.length > 0) {
+      groups.push({ groupName: "Yesterday", topics: yesterday });
+    }
+
+    const [thisWeek, notThisWeek] = groupByFilter(notYesterday, happenedWithin(startOfWeek(now)));
+    if (thisWeek.length > 0) {
+      groups.push({ groupName: "Rest of this week", topics: thisWeek });
+    }
+
+    const [lastWeek, notLastWeek] = groupByFilter(notThisWeek, happenedWithin(startOfWeek(sameTimeLastWeek)));
+    if (lastWeek.length > 0) {
+      groups.push({ groupName: "Last week", topics: lastWeek });
+    }
+
+    if (SHOW_RECENT_ONLY) {
+      return groups;
+    }
+
+    const [restOfMonth, allRemaining] = groupByFilter(notLastWeek, happenedWithin(startOfMonth(sameTimeLastMonth)));
+    if (restOfMonth.length > 0) {
+      groups.push({ groupName: "Rest of month", topics: lastWeek });
+    }
+
+    let remainingDays = allRemaining;
+
+    while (remainingDays.length > 0) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const beginningOfMonth = new Date(remainingDays[0].archived_at!);
+
+      const previousMonth = endOfMonth(endOfDay(subMonths(beginningOfMonth, 1)));
+
+      const [month, remaining] = groupByFilter(remainingDays, happenedWithin(previousMonth));
+      if (month.length > 0) {
+        const groupName = format(beginningOfMonth, "MMMM uuuu");
+        groups.push({ groupName, topics: month });
+      }
+
+      remainingDays = remaining;
+    }
+
+    return groups;
+  }, [archived]);
+
+  return archivedGroups;
+};
