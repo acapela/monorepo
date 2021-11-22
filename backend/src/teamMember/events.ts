e1d2e05df1431775b6068c40e37f8b77aa1e2707
Heiki Riesenkampf <me@heikir.com>
2021-11-22 17:20:55 +0100

    fix(tracking): remove unnecessary email params, track invite deletions, use correct userId when tracking removing users

--- backend/src/teamMember/events.ts
+++ backend/src/teamMember/events.ts
@@ -3,11 +3,23 @@ import { trackBackendUserEvent } from "~shared/backendAnalytics";
 
 import { HasuraEvent } from "../hasura";
 
-export async function handleTeamMemberDeleted({ item: teamMember }: HasuraEvent<TeamMember>) {
+export async function handleTeamMemberDeleted({ userId, item: teamMember }: HasuraEvent<TeamMember>) {
   // remove user's current_team_id
-  trackBackendUserEvent(teamMember.user_id, "Account Removed User", {
-    teamId: teamMember.team_id,
-    userId: teamMember.user_id,
-  });
-  await db.user.update({ where: { id: teamMember.user_id }, data: { current_team_id: null } });
+  const deletedUser = await db.user.update({ where: { id: teamMember.user_id }, data: { current_team_id: null } });
+
+  if (userId) {
+    if (teamMember.has_joined) {
+      // person accepted the invite
+      trackBackendUserEvent(userId, "Account Removed User", {
+        teamId: teamMember.team_id,
+        userId: teamMember.user_id,
+      });
+    } else {
+      // the invite had not been accepted yet
+      trackBackendUserEvent(userId, "Deleted Invite", {
+        teamId: teamMember.team_id,
+        email: deletedUser.email,
+      });
+    }
+  }
 }
