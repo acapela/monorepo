a00a4e8eb61bf3464459d9cdf4e69ce7eda8f72c
Gregor Weber <gregorwbr@gmail.com>
2021-11-24 13:32:17 +0100

    fix: disable source map uploading

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ const withTranspileModules = require("next-transpile-modules");
 const dotenv = require("dotenv");
 const fs = require("fs");
 const path = require("path");
-const SentryCliPlugin = require("@sentry/webpack-plugin");
+// const SentryCliPlugin = require("@sentry/webpack-plugin");
 
 /**
  * Let's tell next.js to compile TypeScript files from other packages of monorepo.
@@ -67,7 +67,7 @@ const envVariables = (nextConfig = {}) => {
   }
 
   return Object.assign({}, nextConfig, {
-    productionBrowserSourceMaps: true,
+    // productionBrowserSourceMaps: true,
 
     webpack: (config, options) => {
       if (typeof nextConfig.webpack === "function") {
@@ -77,16 +77,16 @@ const envVariables = (nextConfig = {}) => {
       const isServer = options.isServer;
       const allEnvVariablesMap = { ...tryGetDotEnvVars(options.config.envFilePath), ...process.env };
 
-      if (process.env.SENTRY_RELEASE && process.env.SENTRY_AUTH_TOKEN) {
-        config.plugins.push(
-          new SentryCliPlugin({
-            include: ".next",
-            org: "acapela",
-            project: "acapela",
-            release: process.env.SENTRY_RELEASE,
-          })
-        );
-      }
+      // if (process.env.SENTRY_RELEASE && process.env.SENTRY_AUTH_TOKEN) {
+      //   config.plugins.push(
+      //     new SentryCliPlugin({
+      //       include: ".next",
+      //       org: "acapela",
+      //       project: "acapela",
+      //       release: process.env.SENTRY_RELEASE,
+      //     })
+      //   );
+      // }
 
       // Prepare list of var names.
       // Note: On frontend, only vars prefixed with NEXT_PUBLIC_ will be available. (this follows official docs)
