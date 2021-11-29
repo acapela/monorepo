ebfbcf5642deffab4b00da0a69c003d2e8350309
Gregor Weber <gregorwbr@gmail.com>
2021-11-29 14:22:26 +0100

    fix: feedbackd

--- backend/src/slack/install.ts
+++ backend/src/slack/install.ts
@@ -8,7 +8,7 @@ import { InstallMetadata } from "./installMetadata";
 
 export const getSlackInstallURL = async (mode: "full" | "user-only", metadata: InstallMetadata) => {
   const basePath = IS_DEV ? (await getDevPublicTunnelURL(3000)) + "/api/backend" : process.env.BACKEND_API_ENDPOINT;
-  return assertDefined(slackReceiver.installer, "must have installer").generateInstallUrl({
+  return assertDefined(slackReceiver.installer, "no installer configured").generateInstallUrl({
     userScopes,
     scopes: mode == "full" ? botScopes : [],
     redirectUri: basePath + "/slack/oauth_redirect",
