61594768ddb9c6bc2bc32e0f1acc99aad1baf475
Christoph Witzko <github@christophwitzko.com>
2021-11-29 17:08:21 +0100

    fix(slack): add read access checkbox to public channels

--- backend/src/slack/utils.ts
+++ backend/src/slack/utils.ts
@@ -81,6 +81,7 @@ export type CreateRequestOrigin = AnalyticsEventsMap["Created Request"]["origin"
 export type ChannelInfo = {
   members: string[];
   name: string | undefined;
+  isPrivate: boolean;
   conversationType: "direct" | "group" | "channel";
 } | null;
 
