0a5ae0c0d34bce8ce9365c8c5debb0f13347068c
Gregor Weber <gregorwbr@gmail.com>
2021-11-30 15:36:08 +0100

    revert(slack): disable posting through bot
    This came with manifest changes we can't make while the app is in submission

--- shared/slack/index.ts
+++ shared/slack/index.ts
@@ -4,9 +4,9 @@ import manifest from "./manifest.json";
 
 export const { bot: botScopes, user: userScopes } = manifest.oauth_config.scopes;
 
-const compareArrays = (a: unknown[], b: unknown[]) => _.intersection(a, b).length === b.length;
-export const checkHasAllSlackUserScopes = (scopes: string[]) => compareArrays(scopes, userScopes);
-export const checkHasAllSlackBotScopes = (scopes: string[]) => compareArrays(scopes, botScopes);
+const isSubsetOf = (a: unknown[], b: unknown[]) => _.intersection(a, b).length === a.length;
+export const checkHasAllSlackUserScopes = (scopes: string[]) => isSubsetOf(userScopes, scopes);
+export const checkHasAllSlackBotScopes = (scopes: string[]) => isSubsetOf(botScopes, scopes);
 
 export const SLACK_INSTALL_ERROR_KEY = "slack_install_error";
 export const SLACK_WORKSPACE_ALREADY_USED_ERROR = "slack_workspace_already_used";
