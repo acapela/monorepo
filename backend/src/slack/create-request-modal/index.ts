86d3c746af872f92f0afbc3275580832b5f41687
Gregor Weber <gregorwbr@gmail.com>
2021-11-30 18:54:11 +0100

    fix(slack): do not try to join before posting

--- backend/src/slack/create-request-modal/index.ts
+++ backend/src/slack/create-request-modal/index.ts
@@ -4,7 +4,6 @@ import { format } from "date-fns";
 import { difference, find, get } from "lodash";
 import { Bits, Blocks, Elements, Md, Message, Modal } from "slack-block-builder";
 
-import { isWebAPIErrorType } from "~backend/src/slack/errors";
 import { backendGetTopicUrl } from "~backend/src/topics/url";
 import { db } from "~db";
 import { assert, assertDefined } from "~shared/assert";
@@ -211,14 +210,6 @@ export function setupCreateRequestModal(app: App) {
       return;
     }
 
-    try {
-      // try to join the channel in case the bot/user is not in it already
-      await client.conversations.join({ token, channel: channelId });
-    } catch (error) {
-      if (!isWebAPIErrorType(error, "method_not_supported_for_channel_type")) {
-        throw error;
-      }
-    }
     const response = await client.chat.postMessage({
       ...(await LiveTopicMessage(topic, { isMessageContentExcluded: hasRequestOriginatedFromMessageAction })),
       token,
