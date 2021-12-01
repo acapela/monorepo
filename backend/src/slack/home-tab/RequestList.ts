6dd62c0c045ec177295b90023cdeb79bbd3cfa5c
Christoph Witzko <github@christophwitzko.com>
2021-12-01 11:10:41 +0100

    feat(slack): show unread messages count next to topic in slack home view

--- backend/src/slack/home-tab/RequestList.ts
+++ backend/src/slack/home-tab/RequestList.ts
@@ -2,20 +2,33 @@ import { Blocks, Md } from "slack-block-builder";
 
 import { GenerateContext } from "../md/generator";
 import { RequestItem } from "./RequestItem";
-import { TopicWithOpenTask } from "./types";
+import { TopicWithOpenTask, UnreadMessages } from "./types";
 
 const Padding = [Blocks.Section({ text: " " }), Blocks.Section({ text: " " })];
 
-export async function RequestsList(title: string, topics: TopicWithOpenTask[], context: GenerateContext) {
+export async function RequestsList(
+  title: string,
+  topics: TopicWithOpenTask[],
+  context: GenerateContext,
+  unreadMessages: UnreadMessages
+) {
   const header = [...Padding, Blocks.Header({ text: title })];
 
   if (topics.length === 0) {
     return [...header, Blocks.Section({ text: Md.italic("No requests here") })];
   }
 
+  const unreadMessagesByTopicId = Object.assign(
+    {},
+    ...unreadMessages.map((um) => ({ [um.topic_id]: um.unread_messages }))
+  );
+
   const nestedTopicsBlocks = await Promise.all(
     topics.map(async (topic, i) => {
-      return [...(await RequestItem(topic, context)), i < topics.length - 1 ? Blocks.Divider() : undefined];
+      return [
+        ...(await RequestItem(topic, context, unreadMessagesByTopicId[topic.id] || 0)),
+        i < topics.length - 1 ? Blocks.Divider() : undefined,
+      ];
     })
   );
 
