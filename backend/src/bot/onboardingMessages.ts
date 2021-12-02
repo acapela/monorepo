c37ecfd3b2aceffb524edd6aed6ae53a2b313e23
Adam Pietrasiak <adam@pietrasiak.com>
2021-12-02 16:38:48 +0100

    copy: tweak onboarding messages typo and links

--- backend/src/bot/onboardingMessages.ts
+++ backend/src/bot/onboardingMessages.ts
@@ -1,6 +1,6 @@
 import { JSONContent } from "@tiptap/core";
 
-// Edit here - http://localhost:3000/dev/content?content=eyJ0eXBlIjoiZG9jIiwiY29udGVudCI6W3sidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwidGV4dCI6IldoYXQgYmV0dGVyIHdheSB0byBvbmJvYXJkIHlvdSAifSx7InR5cGUiOiJtZW50aW9uIiwiYXR0cnMiOnsiZGF0YSI6eyJ1c2VySWQiOiIyMDJmMjIzMy04ZTBmLTQ4N2MtODI1OS1hNzI5ZTA1MWZjZTIiLCJ0eXBlIjoicmVxdWVzdC1yZWFkIn19fSx7InR5cGUiOiJ0ZXh0IiwidGV4dCI6IiAgdGhhbiBzZW5kaW5nIHlvdXIgZmlyc3QgcmVxdWVzdD8ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6ImJ1bGxldExpc3QiLCJjb250ZW50IjpbeyJ0eXBlIjoibGlzdEl0ZW0iLCJjb250ZW50IjpbeyJ0eXBlIjoicGFyYWdyYXBoIiwiY29udGVudCI6W3sidHlwZSI6InRleHQiLCJtYXJrcyI6W3sidHlwZSI6ImJvbGQifV0sInRleHQiOiJNYXJrIHRoaXMgUmVxdWVzdCBhcyBkb25lIn0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgdG8gc2VlIGl0IHJlc29sdmVkLiJ9XX1dfSx7InR5cGUiOiJsaXN0SXRlbSIsImNvbnRlbnQiOlt7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsIm1hcmtzIjpbeyJ0eXBlIjoiYm9sZCJ9XSwidGV4dCI6IkNsb3NlIn0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgaXQgdG8gZnJlZSB1cCB5b3VyIFJlY2VpdmVkIHNlY3Rpb24gYW5kIHdhdGNoIGl0IGRpc2FwcGVhciBhZnRlciAyNCBob3Vycy4ifV19XX0seyJ0eXBlIjoibGlzdEl0ZW0iLCJjb250ZW50IjpbeyJ0eXBlIjoicGFyYWdyYXBoIiwiY29udGVudCI6W3sidHlwZSI6InRleHQiLCJ0ZXh0IjoiWW91IGNhbiAifSx7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0IjoiQ2xvc2UgJiBBcmNoaXZlIn0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgaXQgZnJvbSB0aGUgdG9wIHJpZ2h0IG1lbnUgdG8gbWFrZSBpdCBkaXNhcHBlciBxdWlja2VyLiJ9XX1dfV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwidGV4dCI6Ik5vdCBzdXJlIHdoYXQgdG8gZG8gbmV4dD8gVHJ5IENyZWF0aW5nIGEgbmV3IHJlcXVlc3QgZm9yIHlvdXJzZWxmIG9yIHRlYW1tYXRlISJ9XX1dfQ%3D%3D
+// Edit here - http://localhost:3000/dev/content?content=eyJ0eXBlIjoiZG9jIiwiY29udGVudCI6W3sidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwidGV4dCI6IldoYXQgYmV0dGVyIHdheSB0byBvbmJvYXJkIHlvdSAifSx7InR5cGUiOiJtZW50aW9uIiwiYXR0cnMiOnsiZGF0YSI6eyJ1c2VySWQiOiIyMDJmMjIzMy04ZTBmLTQ4N2MtODI1OS1hNzI5ZTA1MWZjZTIiLCJ0eXBlIjoicmVxdWVzdC1yZXNwb25zZSJ9fX0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgdGhhbiBzZW5kaW5nIHlvdXIgZmlyc3QgcmVxdWVzdD%2FigKgifV19LHsidHlwZSI6ImJ1bGxldExpc3QiLCJjb250ZW50IjpbeyJ0eXBlIjoibGlzdEl0ZW0iLCJjb250ZW50IjpbeyJ0eXBlIjoicGFyYWdyYXBoIiwiY29udGVudCI6W3sidHlwZSI6InRleHQiLCJtYXJrcyI6W3sidHlwZSI6ImJvbGQifV0sInRleHQiOiJNYXJrIHRoaXMgUmVxdWVzdCBhcyBkb25lIn0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgdG8gc2VlIGl0IHJlc29sdmVkLiJ9XX1dfSx7InR5cGUiOiJsaXN0SXRlbSIsImNvbnRlbnQiOlt7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJBZnRlciBtYXJraW5nIGl0IGFzIGRvbmUsIHlvdSBjYW4gIn0seyJ0eXBlIjoidGV4dCIsIm1hcmtzIjpbeyJ0eXBlIjoiYm9sZCJ9XSwidGV4dCI6ImNsb3NlIHRoaXMgcmVxdWVzdCJ9LHsidHlwZSI6InRleHQiLCJ0ZXh0IjoiIHVzaW5nIGJ1dHRvbiBpbiB0b3AgcmlnaHQgY29ybmVyLiJ9XX1dfSx7InR5cGUiOiJsaXN0SXRlbSIsImNvbnRlbnQiOlt7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJBZnRlciBjbG9zaW5nIHRoaXMgcmVxdWVzdCB5b3UgY2FuICJ9LHsidHlwZSI6InRleHQiLCJtYXJrcyI6W3sidHlwZSI6ImJvbGQifV0sInRleHQiOiJhcmNoaXZlIn0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgaXQgeW91cnNlbGYgb3Igd2FpdCBmb3IgaXQgdG8gZ2V0IGFyY2hpdmVkIGF1dG9tYXRpY2FsbHkgYWZ0ZXIgMjQgaG91cnMuIn1dfV19XX0seyJ0eXBlIjoicGFyYWdyYXBoIn0seyJ0eXBlIjoicGFyYWdyYXBoIiwiY29udGVudCI6W3sidHlwZSI6InRleHQiLCJ0ZXh0IjoiTm90IHN1cmUgd2hhdCB0byBkbyBuZXh0PyBUcnkgIn0seyJ0eXBlIjoidGV4dCIsIm1hcmtzIjpbeyJ0eXBlIjoiYm9sZCJ9XSwidGV4dCI6IkNyZWF0aW5nIGEgbmV3IHJlcXVlc3QifSx7InR5cGUiOiJ0ZXh0IiwidGV4dCI6IiBmcm9tIHRoZSBzaWRlYmFyIGZvciB5b3Vyc2VsZiBvciB0ZWFtbWF0ZSEifV19XX0%3D
 export function getWelcomeToAcapelaMessage(userId: string): JSONContent {
   return {
     type: "doc",
@@ -16,19 +16,16 @@ export function getWelcomeToAcapelaMessage(userId: string): JSONContent {
             type: "mention",
             attrs: {
               data: {
-                userId: userId,
+                userId,
                 type: "request-read",
               },
             },
           },
           {
             type: "text",
-            text: "  than sending your first request?",
+            text: " than sending your first request?",
           },
         ],
-      },
-      {
-        type: "paragraph",
       },
       {
         type: "bulletList",
@@ -64,16 +61,20 @@ export function getWelcomeToAcapelaMessage(userId: string): JSONContent {
                 content: [
                   {
                     type: "text",
+                    text: "After marking it as done, you can ",
+                  },
+                  {
+                    type: "text",
                     marks: [
                       {
                         type: "bold",
                       },
                     ],
-                    text: "Close",
+                    text: "close this request",
                   },
                   {
                     type: "text",
-                    text: " it to free up your Received section and watch it disappear after 24 hours.",
+                    text: " using button in top right corner.",
                   },
                 ],
               },
@@ -87,7 +88,7 @@ export function getWelcomeToAcapelaMessage(userId: string): JSONContent {
                 content: [
                   {
                     type: "text",
-                    text: "You can ",
+                    text: "After closing this request you can ",
                   },
                   {
                     type: "text",
@@ -96,11 +97,11 @@ export function getWelcomeToAcapelaMessage(userId: string): JSONContent {
                         type: "bold",
                       },
                     ],
-                    text: "Close & Archive",
+                    text: "archive",
                   },
                   {
                     type: "text",
-                    text: " it from the top right menu to make it disapper quicker.",
+                    text: " it yourself or wait for it to get archived automatically after 24 hours.",
                   },
                 ],
               },
@@ -116,7 +117,20 @@ export function getWelcomeToAcapelaMessage(userId: string): JSONContent {
         content: [
           {
             type: "text",
-            text: "Not sure what to do next? Try Creating a new request for yourself or teammate!",
+            text: "Not sure what to do next? Try ",
+          },
+          {
+            type: "text",
+            marks: [
+              {
+                type: "bold",
+              },
+            ],
+            text: "Creating a new request",
+          },
+          {
+            type: "text",
+            text: " from the sidebar for yourself or teammate!",
           },
         ],
       },
@@ -124,7 +138,7 @@ export function getWelcomeToAcapelaMessage(userId: string): JSONContent {
   };
 }
 
-// Edit here - http://localhost:3000/dev/content?content=eyJ0eXBlIjoiZG9jIiwiY29udGVudCI6W3sidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwidGV4dCI6IkhleSAifSx7InR5cGUiOiJtZW50aW9uIiwiYXR0cnMiOnsiZGF0YSI6eyJ1c2VySWQiOiIyMDJmMjIzMy04ZTBmLTQ4N2MtODI1OS1hNzI5ZTA1MWZjZTIiLCJ0eXBlIjoicmVxdWVzdC1yZXNwb25zZSJ9fX0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgLCBnZXQgYSBoZWFkc3RhcnQgYnkgY2hlY2tpbmcgb3V0IHRoZXNlIHRpcHM6In1dfSx7InR5cGUiOiJwYXJhZ3JhcGgifSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsIm1hcmtzIjpbeyJ0eXBlIjoiYm9sZCJ9XSwidGV4dCI6IvCflZMgU2V0IGR1ZSBkYXRlcyBvbiByZXF1ZXN0cyJ9XX0seyJ0eXBlIjoicGFyYWdyYXBoIiwiY29udGVudCI6W3sidHlwZSI6InRleHQiLCJ0ZXh0IjoiTWFrZSBzdXJlIHlvdXIgcmVxdWVzdCBpcyB0b3Agb2YgbWluZCBmb3IgeW91IHRlYW0ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0Ijoi8J%2BTjCBZb3UgY2FuIG1lbnRpb24geW91cnNlbGYhIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJUaGlzIHdheSwgQWNhcGVsYSBjYW4gaGVscCB5b3UgZ2V0IHRocm91Z2ggeW91ciBkYWlseSB0YXNrcy4ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0Ijoi8J%2BZgiBBZGQgcmVhY3Rpb25zIHRvIG1lc3NhZ2VzICh3ZWIgYXBwIG9ubHkpIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJTaG93IHlvdXIgYXBwcmVjaWF0aW9uIG9yIHF1aWNrIGZlZWRiYWNrIHdpdGggZW1vamkgcmVhY3Rpb25zLiJ9XX0seyJ0eXBlIjoicGFyYWdyYXBoIn0seyJ0eXBlIjoicGFyYWdyYXBoIiwiY29udGVudCI6W3sidHlwZSI6InRleHQiLCJtYXJrcyI6W3sidHlwZSI6ImJvbGQifV0sInRleHQiOiLwn6SdIE1lbnRpb24gZ3JvdXBzICh3ZWIgYXBwIG9ubHkpIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJZb3UgY2FuIG5vdyBtZW50aW9uIGdyb3VwcyBvZiBwZW9wbGUgYW5kIGNyZWF0ZSBjdXN0b20gYWxpYXNlcy4ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0Ijoi8J%2BTjiBBZGQgbGlua3MgYW5kIGF0dGFjaG1lbnRzIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJObyBtb3JlIGRvY3VtZW50IGh1bnRpbmcgYW5kIGNoYXNpbmcgZm9yIHRlYW0gcHJvamVjdHMuIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgifSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJIYXZlIG1vcmUgaWRlYXMgb3IgZmVhdHVyZSByZXF1ZXN0cz8gTGVhdmUgdXMgZmVlZGJhY2suIFRoYW5rIHlvdSBhIGJ1bmNoLiDwn5mPIn1dfV19
+// Edit here - http://localhost:3000/dev/content?content=eyJ0eXBlIjoiZG9jIiwiY29udGVudCI6W3sidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwidGV4dCI6IkhleSAifSx7InR5cGUiOiJtZW50aW9uIiwiYXR0cnMiOnsiZGF0YSI6eyJ1c2VySWQiOiIyMDJmMjIzMy04ZTBmLTQ4N2MtODI1OS1hNzI5ZTA1MWZjZTIiLCJ0eXBlIjoicmVxdWVzdC1yZXNwb25zZSJ9fX0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIsIGdldCBhIGhlYWRzdGFydCBieSBjaGVja2luZyBvdXQgdGhlc2UgdGlwczoifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0Ijoi8J%2BVkyBTZXQgZHVlIGRhdGVzIG9uIHJlcXVlc3RzIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJNYWtlIHN1cmUgeW91ciByZXF1ZXN0IGlzIHRvcCBvZiBtaW5kIGZvciB5b3UgdGVhbS4ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0Ijoi8J%2BTjCBZb3UgY2FuIG1lbnRpb24geW91cnNlbGYhIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJUaGlzIHdheSwgQWNhcGVsYSBjYW4gaGVscCB5b3UgZ2V0IHRocm91Z2ggeW91ciBkYWlseSB0YXNrcy4ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0Ijoi8J%2BZgiBBZGQgcmVhY3Rpb25zIHRvIG1lc3NhZ2VzICh3ZWIgYXBwIG9ubHkpIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJTaG93IHlvdXIgYXBwcmVjaWF0aW9uIG9yIHF1aWNrIGZlZWRiYWNrIHdpdGggZW1vamkgcmVhY3Rpb25zLiJ9XX0seyJ0eXBlIjoicGFyYWdyYXBoIn0seyJ0eXBlIjoicGFyYWdyYXBoIiwiY29udGVudCI6W3sidHlwZSI6InRleHQiLCJtYXJrcyI6W3sidHlwZSI6ImJvbGQifV0sInRleHQiOiLwn6SdIE1lbnRpb24gZ3JvdXBzICh3ZWIgYXBwIG9ubHkpIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJZb3UgY2FuIG5vdyBtZW50aW9uIGdyb3VwcyBvZiBwZW9wbGUgYW5kIGNyZWF0ZSBjdXN0b20gYWxpYXNlcy4ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwibWFya3MiOlt7InR5cGUiOiJib2xkIn1dLCJ0ZXh0Ijoi8J%2BTjiBBZGQgbGlua3MgYW5kIGF0dGFjaG1lbnRzIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJObyBtb3JlIGRvY3VtZW50IGh1bnRpbmcgYW5kIGNoYXNpbmcgZm9yIHRlYW0gcHJvamVjdHMuIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgifSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJIYXZlIG1vcmUgaWRlYXMgb3IgZmVhdHVyZSByZXF1ZXN0cz8gR2l2ZSB1cyBmZWVkYmFjayB1c2luZyB0aGUgYnV0dG9uIG9uIHRoZSByaWdodC1oYW5kIHNpZGUuIFRoYW5rIHlvdSBhIGJ1bmNoISDwn5mPIn1dfV19
 export function getInitialTipsMessage(userId: string): JSONContent {
   return {
     type: "doc",
@@ -141,13 +155,13 @@ export function getInitialTipsMessage(userId: string): JSONContent {
             attrs: {
               data: {
                 userId,
-                type: "request-response",
+                type: "request-read",
               },
             },
           },
           {
             type: "text",
-            text: " , get a headstart by checking out these tips:",
+            text: ", get a headstart by checking out these tips:",
           },
         ],
       },
@@ -173,7 +187,7 @@ export function getInitialTipsMessage(userId: string): JSONContent {
         content: [
           {
             type: "text",
-            text: "Make sure your request is top of mind for you team",
+            text: "Make sure your request is top of mind for you team.",
           },
         ],
       },
@@ -289,7 +303,7 @@ export function getInitialTipsMessage(userId: string): JSONContent {
         content: [
           {
             type: "text",
-            text: "Have more ideas or feature requests? Leave us feedback. Thank you a bunch. 🙏",
+            text: "Have more ideas or feature requests? Give us feedback using the button on the right-hand side. Thank you a bunch! 🙏",
           },
         ],
       },
@@ -297,7 +311,7 @@ export function getInitialTipsMessage(userId: string): JSONContent {
   };
 }
 
-// Edit here - http://localhost:3000/dev/content?content=eyJ0eXBlIjoiZG9jIiwiY29udGVudCI6W3sidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJtZW50aW9uIiwiYXR0cnMiOnsiZGF0YSI6eyJ1c2VySWQiOiIyMDJmMjIzMy04ZTBmLTQ4N2MtODI1OS1hNzI5ZTA1MWZjZTIiLCJ0eXBlIjoicmVxdWVzdC1yZXNwb25zZSJ9fX0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgSW4gY2FzZSB5b3UgaGF2ZW7igJl0IHRyaWVkIGl0IHlldCwgd2UgaGF2ZSBhIHByZXR0eSBwb3dlcmZ1bCBTbGFjayBhZGQtb24geW91IHNob3VsZCB0cnkhIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgifSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiLwn5GJIEFkZCB0byB5b3VyIFNsYWNrIG5vdy4ifV19LHsidHlwZSI6InBhcmFncmFwaCJ9LHsidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJ0ZXh0IiwidGV4dCI6IkhhdmUgbW9yZSBpZGVhcyBvciBmZWF0dXJlIHJlcXVlc3RzPyBMZWF2ZSB1cyBmZWVkYmFjay4gVGhhbmsgeW91IGEgYnVuY2guIPCfmY8ifV19XX0%3D
+// Edit here - http://localhost:3000/dev/content?content=eyJ0eXBlIjoiZG9jIiwiY29udGVudCI6W3sidHlwZSI6InBhcmFncmFwaCIsImNvbnRlbnQiOlt7InR5cGUiOiJtZW50aW9uIiwiYXR0cnMiOnsiZGF0YSI6eyJ1c2VySWQiOiIyMDJmMjIzMy04ZTBmLTQ4N2MtODI1OS1hNzI5ZTA1MWZjZTIiLCJ0eXBlIjoicmVxdWVzdC1yZXNwb25zZSJ9fX0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgaW4gY2FzZSB5b3UgaGF2ZW7igJl0IHRyaWVkIGl0IHlldCwgd2UgaGF2ZSBhIHByZXR0eSBwb3dlcmZ1bCBTbGFjayBhZGQtb24geW91IHNob3VsZCB0cnkhIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgifSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsIm1hcmtzIjpbeyJ0eXBlIjoibGluayIsImF0dHJzIjp7ImhyZWYiOiIvc2V0dGluZ3MiLCJ0YXJnZXQiOiJfYmxhbmsifX1dLCJ0ZXh0Ijoi8J%2BRiSBBZGQgdG8geW91ciBTbGFjayBub3cuIn0seyJ0eXBlIjoidGV4dCIsInRleHQiOiIgIn1dfSx7InR5cGUiOiJwYXJhZ3JhcGgifSx7InR5cGUiOiJwYXJhZ3JhcGgiLCJjb250ZW50IjpbeyJ0eXBlIjoidGV4dCIsInRleHQiOiJIYXZlIG1vcmUgaWRlYXMgb3IgZmVhdHVyZSByZXF1ZXN0cz8gR2l2ZSB1cyBmZWVkYmFjayB1c2luZyB0aGUgYnV0dG9uIG9uIHRoZSByaWdodC1oYW5kIHNpZGUuIFRoYW5rIHlvdSBhIGJ1bmNoISDwn5mPIn1dfV19
 export function getSlackIntegrationTipsMessage(userId: string): JSONContent {
   return {
     type: "doc",
@@ -309,14 +323,14 @@ export function getSlackIntegrationTipsMessage(userId: string): JSONContent {
             type: "mention",
             attrs: {
               data: {
-                userId: userId,
-                type: "request-response",
+                userId,
+                type: "request-action",
               },
             },
           },
           {
             type: "text",
-            text: " In case you haven’t tried it yet, we have a pretty powerful Slack add-on you should try!",
+            text: " in case you haven’t tried it yet, we have a pretty powerful Slack add-on you should try!",
           },
         ],
       },
@@ -328,8 +342,20 @@ export function getSlackIntegrationTipsMessage(userId: string): JSONContent {
         content: [
           {
             type: "text",
+            marks: [
+              {
+                type: "link",
+                attrs: {
+                  href: "/settings",
+                },
+              },
+            ],
             text: "👉 Add to your Slack now.",
           },
+          {
+            type: "text",
+            text: " ",
+          },
         ],
       },
       {
@@ -340,7 +366,7 @@ export function getSlackIntegrationTipsMessage(userId: string): JSONContent {
         content: [
           {
             type: "text",
-            text: "Have more ideas or feature requests? Leave us feedback. Thank you a bunch. 🙏",
+            text: "Have more ideas or feature requests? Give us feedback using the button on the right-hand side. Thank you a bunch! 🙏",
           },
         ],
       },
