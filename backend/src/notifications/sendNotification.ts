57075fd26872ffe7f89a5b4fe89425ea7dd949aa
Omar Duarte <omarduarte@gmail.com>
2021-12-06 09:30:09 +0200

    feat(notifications): enqueue slack message if out of work hours

--- backend/src/notifications/sendNotification.ts
+++ backend/src/notifications/sendNotification.ts
@@ -1,16 +1,19 @@
-import { Block, KnownBlock } from "@slack/bolt";
+import { KnownBlock } from "@slack/bolt";
 import { ChatPostMessageResponse } from "@slack/web-api";
 import { pick } from "lodash";
+import { SlackBlockDto } from "slack-block-builder";
 
 import { slackClient } from "~backend/src/slack/app";
 import { fetchTeamBotToken, findSlackUserId } from "~backend/src/slack/utils";
 import { User, db } from "~db";
-import { assertDefined } from "~shared/assert";
+import { assert, assertDefined } from "~shared/assert";
 import { EmailData, sendEmail } from "~shared/email";
 import { logger } from "~shared/logger";
 
-type SlackMessage = string | (KnownBlock | Block)[];
-type SlackPayload = SlackMessage | (() => Promise<SlackMessage>);
+import { enqueueSlackNotification } from "./enqueueNotification";
+
+export type SlackMessage = string | KnownBlock[] | SlackBlockDto[];
+export type SlackPayload = SlackMessage | (() => Promise<SlackMessage>);
 
 async function trySendSlackNotification(teamId: string, user: User, payload: SlackPayload) {
   const [token, slackUserId] = await Promise.all([fetchTeamBotToken(teamId), findSlackUserId(teamId, user)]);
@@ -19,12 +22,49 @@ async function trySendSlackNotification(teamId: string, user: User, payload: Sla
   }
 
   payload = typeof payload == "function" ? await payload() : payload;
+
   const textOrBlocks = typeof payload === "string" ? { text: payload } : { blocks: payload };
+
   return slackClient.chat.postMessage({
     token,
     channel: slackUserId,
     ...textOrBlocks,
   });
+}
+
+async function shouldSendNotificationDirectly(teamId: string, user: User): Promise<boolean> {
+  const teamMember = await db.team_member.findFirst({
+    where: {
+      team_id: teamId,
+      user_id: user.id,
+    },
+  });
+
+  assert(teamMember, `team member for user(${user.id}) and team(${teamId}) must exist before sending notifications`);
+
+  const { timezone, work_start_hour_in_utc, work_end_hour_in_utc } = teamMember;
+
+  const hasSetupWorkHours = timezone && work_start_hour_in_utc && work_end_hour_in_utc;
+  if (!hasSetupWorkHours) {
+    return true;
+  }
+
+  const currentUtcHour = new Date().getUTCHours();
+
+  // e.g Start 9am - end 6pm, Start 1pm - end 10pm, etc
+  if (work_start_hour_in_utc < work_end_hour_in_utc) {
+    return work_start_hour_in_utc <= currentUtcHour && currentUtcHour < work_end_hour_in_utc;
+  }
+
+  // e.g Start 10pm - End 3AM, Start 5pm - end 12am, etc
+  return work_start_hour_in_utc <= currentUtcHour || currentUtcHour < work_end_hour_in_utc;
+}
+
+async function sendOrEnqueueSlackNotification(teamId: string, user: User, payload: SlackPayload) {
+  if (await shouldSendNotificationDirectly(teamId, user)) {
+    return trySendSlackNotification(teamId, user, payload);
+  }
+  return await enqueueSlackNotification(teamId, user, payload);
 }
 
 export type NotificationMessage = { slack: SlackPayload; email: EmailData };
@@ -36,7 +76,7 @@ async function sendNotification(
 ): Promise<{ slackMessage?: ChatPostMessageResponse }> {
   try {
     const [slackMessage] = await Promise.all([
-      message.slack ? trySendSlackNotification(teamId, user, message.slack) : undefined,
+      message.slack ? sendOrEnqueueSlackNotification(teamId, user, message.slack) : undefined,
       message.email ? sendEmail(message.email, user.email) : undefined,
     ]);
     return slackMessage?.ok ? { slackMessage } : {};
