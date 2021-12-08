d606840af162189a90ed9d265a566421ed5d0530
Christoph Witzko <github@christophwitzko.com>
2021-12-08 17:05:43 +0100

    feat(notifications): notify owner once a request is overdue and there are still outstanding tasks left

--- backend/src/cron/proactiveNotifications.ts
+++ backend/src/cron/proactiveNotifications.ts
@@ -1,14 +1,12 @@
 import { addMinutes } from "date-fns";
+import { groupBy } from "lodash";
 
-import { createAllRequestsDoneNotificationMessage } from "~backend/src/notifications/bodyBuilders/allRequestsDone";
-import { createRequestIsDue } from "~backend/src/notifications/bodyBuilders/requestIsDue";
+import { createRequestIsDue, createRequestIsDueIn3Hours } from "~backend/src/notifications/bodyBuilders/requestIsDue";
 import { sendNotificationPerPreference } from "~backend/src/notifications/sendNotification";
 import { backendGetTopicUrl } from "~backend/src/topics/url";
 import { db } from "~db";
 
-export async function proactiveNotifications() {
-  const now = new Date();
-
+async function requestIsDueIn3Hours(now: Date) {
   const inThreeHours = addMinutes(now, 3 * 60);
   // NOTE! This is highly tightly coupled with the cron job timer. */15 (every 15 minutes)
   const inTwoHoursAnd45Minutes = addMinutes(inThreeHours, -15);
@@ -27,6 +25,9 @@ export async function proactiveNotifications() {
           },
         },
       },
+      done_at: {
+        equals: null,
+      },
     },
     include: {
       message: {
@@ -40,13 +41,65 @@ export async function proactiveNotifications() {
     },
   });
   for (const task of tasks) {
-    const message = createRequestIsDue({
+    const message = createRequestIsDueIn3Hours({
       topicId: task.message.topic_id,
       topicName: task.message.topic.name,
       taskCreatorName: task.message.user.name,
       topicURL: await backendGetTopicUrl(task.message.topic),
     });
-    console.log(message);
     await sendNotificationPerPreference(task.user, task.message.topic.team_id, message);
   }
+}
+
+async function requestIsDue(now: Date) {
+  // NOTE! This is highly tightly coupled with the cron job timer. */15 (every 15 minutes)
+  const fifteenMinutesAgo = addMinutes(now, -15);
+  const tasks = await db.task.findMany({
+    where: {
+      message: {
+        message_task_due_date: {
+          due_at: {
+            gt: fifteenMinutesAgo,
+            lte: now,
+          },
+        },
+        topic: {
+          closed_at: {
+            equals: null,
+          },
+        },
+      },
+      done_at: {
+        equals: null,
+      },
+    },
+    include: {
+      message: {
+        include: {
+          message_task_due_date: true,
+          topic: true,
+          user: true,
+        },
+      },
+      user: true,
+    },
+  });
+
+  const tasksByTopicId = groupBy(tasks, "message.topic_id");
+  for (const topicId in tasksByTopicId) {
+    const topicTasks = tasksByTopicId[topicId];
+    const message = createRequestIsDue({
+      openTasks: topicTasks.length,
+      topicName: topicTasks[0].message.topic.name,
+      topicId,
+      topicURL: await backendGetTopicUrl(topicTasks[0].message.topic),
+    });
+    await sendNotificationPerPreference(topicTasks[0].message.user, topicTasks[0].message.topic.team_id, message);
+  }
+}
+
+export async function proactiveNotifications() {
+  const now = new Date();
+  await requestIsDueIn3Hours(now);
+  await requestIsDue(now);
 }
--- backend/src/notifications/bodyBuilders/requestIsDue.ts
+++ backend/src/notifications/bodyBuilders/requestIsDue.ts
@@ -1,23 +1,21 @@
-import { BlockCollection, Blocks, Elements, Md } from "slack-block-builder";
+import { BlockCollection, Blocks, Elements } from "slack-block-builder";
 
 import { createSlackLink } from "~backend/src/slack/md/utils";
-import { SlackActionIds } from "~backend/src/slack/utils";
+import { pluralize } from "~shared/text/pluralize";
 
 import { NotificationMessage } from "../sendNotification";
 
-interface Props {
-  topicId: string;
-  topicName: string;
-  topicURL: string;
-  taskCreatorName: string;
-}
-
-export function createRequestIsDue({
+export function createRequestIsDueIn3Hours({
   taskCreatorName,
   topicId,
   topicName,
   topicURL,
-}: Props): Partial<NotificationMessage> {
+}: {
+  topicId: string;
+  topicName: string;
+  topicURL: string;
+  taskCreatorName: string;
+}): Partial<NotificationMessage> {
   const messageSlack = `We noticed your task in ${createSlackLink(
     topicURL,
     topicName
@@ -26,6 +24,46 @@ export function createRequestIsDue({
   return {
     email: {
       subject: `You have uncompleted tasks in ${topicName}.`,
+      html: messageHtml,
+    },
+    slack: BlockCollection(
+      Blocks.Section({ text: messageSlack }),
+      Blocks.Actions().elements(
+        Elements.Button({
+          actionId: "open_view_request_modal",
+          value: topicId,
+          text: "View Request",
+        }).primary(true)
+      )
+    ),
+  };
+}
+
+export function createRequestIsDue({
+  topicId,
+  topicName,
+  topicURL,
+  openTasks,
+}: {
+  topicId: string;
+  topicName: string;
+  topicURL: string;
+  openTasks: number;
+}): Partial<NotificationMessage> {
+  const message = `We noticed the request you created in ${topicName} is now overdue and there ${pluralize(
+    openTasks,
+    "is",
+    "are"
+  )} still ${openTasks} ${pluralize(openTasks, "task", "tasks")} that ${pluralize(
+    openTasks,
+    "has",
+    "have"
+  )} not been completed. You can extend the deadline or follow up`;
+  const messageSlack = `${message} ${createSlackLink(topicURL, "here")}.`;
+  const messageHtml = `${message} <a href="${topicURL}">${topicName}</a>.`;
+  return {
+    email: {
+      subject: `Your request has overdue tasks in ${topicName}.`,
       html: messageHtml,
     },
     slack: BlockCollection(
