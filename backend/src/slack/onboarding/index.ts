f060584ffc570fde5dd12bcf7a66bf49702068c0
Heiki Riesenkampf <me@heikir.com>
2021-12-14 11:26:58 +0100

    chore(slack): change image url

--- backend/src/slack/onboarding/index.ts
+++ backend/src/slack/onboarding/index.ts
@@ -17,7 +17,7 @@ export function NewUserOnboardingMessage(slackTeamId: string) {
         Elements.Img({
           altText: "Acapela thumbnail",
           imageUrl:
-            "https://cdn.substack.com/image/fetch/w_1456,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F056b1317-d2b4-4322-b951-f63f7f092fbf_3000x2400.jpeg",
+            "https://bucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com/public/images/056b1317-d2b4-4322-b951-f63f7f092fbf_3000x2400.jpeg",
         })
       ),
       Blocks.Context({}).elements([
