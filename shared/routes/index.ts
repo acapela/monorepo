a6bf622350c770180532349aac9ed136ce0b6d90
Christoph Witzko <github@christophwitzko.com>
2021-12-15 09:25:16 +0100

    feat(frontend): remove static topic part from topic urls

--- shared/routes/index.ts
+++ shared/routes/index.ts
@@ -5,8 +5,8 @@ export const routes = parameterizeRoutes({
   newRequest: "/new",
   // It is left here to support old links for a while. We should remove it ~end of 2021
   _deprecated_topic: ["/topic/[topicSlug]", ["topicSlug"]],
-  topicByHandle: ["/[teamSlug]/topic/[topicSlug]/[topicId]", ["teamSlug", "topicSlug", "topicId"]],
-  topicDuplicate: ["/[teamSlug]/topic/[topicSlug]/[topicId]/duplicate", ["teamSlug", "topicSlug", "topicId"]],
+  topicByHandle: ["/[teamSlug]/[topicSlug]/[topicId]", ["teamSlug", "topicSlug", "topicId"]],
+  topicDuplicate: ["/[teamSlug]/[topicSlug]/[topicId]/duplicate", ["teamSlug", "topicSlug", "topicId"]],
   settings: "/settings",
   invite: "/invite",
   logout: "/logout",
