45643970862d8053ce0cabf3beccbe3eb6d6ab2e
Christoph Witzko <github@christophwitzko.com>
2021-12-15 10:22:20 +0100

    feat(frontend): redirect legacy topic urls in frontend

--- frontend/pages/topic/[topicSlug]/index.tsx
+++ frontend/pages/topic/[topicSlug]/index.tsx
@@ -1,3 +1,23 @@
-import { TopicOrNewRequestPage } from "~frontend/views/TopicOrNewRequestPage";
+import { observer } from "mobx-react";
+import router from "next/router";
 
-export default TopicOrNewRequestPage;
+import { useDb } from "~frontend/clientdb";
+import { useRouteParamsIfRouteActive } from "~frontend/hooks/useRouteParams";
+import { routes } from "~shared/routes";
+
+function Page(): JSX.Element {
+  const topicRouteParams = useRouteParamsIfRouteActive(routes._deprecated_topic);
+  const topicSlug = topicRouteParams?.topicSlug;
+  const db = useDb();
+  const topic = db.topic.findByUniqueIndex("slug", topicSlug);
+  const topicId = topic?.id;
+  const teamName = topic?.team?.name;
+  if (!topicSlug || !teamName || !topicId) {
+    router.push("/404");
+  } else {
+    router.push(`/${teamName}/${topicSlug}/${topicId}`);
+  }
+  return <div></div>;
+}
+
+export default observer(Page);
