0954d8d5217e81d21eb65a656b8cbd9ceb4b8325
Gregor Weber <gregorwbr@gmail.com>
2021-12-20 10:06:02 +0100

    fix: enable request decision without feature flag

--- frontend/src/message/extensions/mentions/MentionTypePicker.tsx
+++ frontend/src/message/extensions/mentions/MentionTypePicker.tsx
@@ -1,9 +1,8 @@
-import { omit, toPairs } from "lodash";
+import { toPairs } from "lodash";
 import { observer } from "mobx-react";
 import React from "react";
 
-import { usePersistedState } from "~frontend/hooks/usePersistedState";
-import { MENTION_TYPE_PICKER_LABELS, MentionType, REQUEST_DECISION } from "~shared/requests";
+import { MENTION_TYPE_PICKER_LABELS, MentionType } from "~shared/requests";
 import { ItemsDropdown } from "~ui/forms/OptionsDropdown/ItemsDropdown";
 
 interface Props {
@@ -11,20 +10,8 @@ interface Props {
   onSelect: (mention: MentionType) => void;
 }
 
-const ENABLE_REQUEST_DECISION = "ENABLE_REQUEST_DECISION";
-
 export const MentionTypePicker = observer(function MentionTypePicker({ selected, onSelect }: Props) {
-  const [isDecisionFeatureFlagEnabled] = usePersistedState<boolean>({
-    key: ENABLE_REQUEST_DECISION,
-    initialValue: false,
-    isDetachedFromTeam: true,
-  });
-
-  const availableMentionTypePickerLabels = isDecisionFeatureFlagEnabled
-    ? MENTION_TYPE_PICKER_LABELS
-    : omit(MENTION_TYPE_PICKER_LABELS, [REQUEST_DECISION]);
-
-  const mentionLabelPairs = toPairs(availableMentionTypePickerLabels) as [MentionType, string][];
+  const mentionLabelPairs = toPairs(MENTION_TYPE_PICKER_LABELS) as [MentionType, string][];
   const selectedPair = selected ? [selected, MENTION_TYPE_PICKER_LABELS[selected]] : [];
 
   return (
