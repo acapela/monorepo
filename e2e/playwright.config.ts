d81858fac3064782b64d3e4bcd2a774b059eabe3
Gregor Weber <gregorwbr@gmail.com>
2021-12-28 11:14:32 +0100

    test: increase retries

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const isCI = Boolean(process.env.CI) && process.env.CI !== "false";
 const config: PlaywrightTestConfig = {
-  retries: isCI ? 1 : 0, // We still have some flakiness, but hopefully this number will go to 0 at some point.
+  retries: isCI ? 2 : 0, // We still have some flakiness, but hopefully this number will go to 0 at some point.
   use: {
     video: "retain-on-failure",
     trace: "retain-on-failure",
