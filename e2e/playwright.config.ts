62a60b5f6a4f214877ad98ee06a34d9a7c31f289
Gregor Weber <gregorwbr@gmail.com>
2021-12-28 11:46:05 +0100

    test: decrease video resolution

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,7 +4,8 @@ const isCI = Boolean(process.env.CI) && process.env.CI !== "false";
 const config: PlaywrightTestConfig = {
   retries: isCI ? 2 : 0, // We still have some flakiness, but hopefully this number will go to 0 at some point.
   use: {
-    video: "retain-on-failure",
+    // To reduce artifact size in CI we set the size to 720p which should be enough
+    video: isCI ? { mode: "retain-on-failure", size: { width: 1280, height: 720 } } : "retain-on-failure",
     trace: "retain-on-failure",
   },
   forbidOnly: isCI,
