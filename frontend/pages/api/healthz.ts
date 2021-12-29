9b1bfadd4f992a4d372b2b56490e177783bbd743
Christoph Witzko <github@christophwitzko.com>
2021-12-29 10:22:34 +0100

    fix(frontend): handle healthcheck errors

--- frontend/pages/api/healthz.ts
+++ frontend/pages/api/healthz.ts
@@ -1,12 +1,18 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const backendRes = await axios.get(`${process.env.BACKEND_HOST}/healthz`);
+  let backendRes = { status: "error" };
+  try {
+    backendRes = (await axios.get(`${process.env.BACKEND_HOST}/healthz`)).data;
+  } catch (e) {
+    console.error((e as AxiosError).toString());
+  }
+
   res.send({
     status: "ok",
     stage: process.env.STAGE,
     version: process.env.SENTRY_RELEASE || "dev",
-    backend: backendRes.data,
+    backend: backendRes,
   });
 }
