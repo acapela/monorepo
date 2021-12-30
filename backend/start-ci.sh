36dc558ccc6e1885ee95debb924feb6a45332280
Christoph Witzko <github@christophwitzko.com>
2021-12-30 13:44:45 +0100

    feat(backend): set APP env

--- backend/start-ci.sh
+++ backend/start-ci.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 echo "building backend..."
 yarn build
 
+export APP=backend
 echo "starting backend..."
 nohup node -r ts-node/register/transpile-only dist/index.js > backend.log 2>&1 & echo $! > backend.pid
 
