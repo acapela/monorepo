1802315cd4fb62c3f7b047779c107c66c4de2d24
Heiki Riesenkampf <me@heikir.com>
2022-01-06 11:01:59 +0100

    fix(slack): add and remove necessary secrets

---
+++ backend/src/waitlist/customerio.ts
@@ -0,0 +1,20 @@
+import { RegionEU, TrackClient } from "customerio-node";
+
+import { assertDefined } from "~shared/assert";
+
+const customerioClientApiKey = assertDefined(
+  process.env.CUSTOMERIO_CLIENT_API_KEY,
+  "CUSTOMERIO_CLIENT_API_KEY is required"
+);
+const customerioClientSiteId = assertDefined(
+  process.env.CUSTOMERIO_CLIENT_SITE_ID,
+  "CUSTOMERIO_CLIENT_SITE_ID is required"
+);
+
+export async function addUserToCustomerio(email: string): Promise<void> {
+  const cio = new TrackClient(customerioClientSiteId, customerioClientApiKey, { region: RegionEU });
+  cio.identify(email, {
+    created_at: Date.now(),
+    origin: "landing-page",
+  });
+}
