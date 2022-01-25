438717a2adca135f71bde42691e4ad1d6bf72e34
Christoph Witzko <github@christophwitzko.com>
2022-01-25 17:06:05 +0100

    fix: show correct app name

--- desktop/scripts/publish.ts
+++ desktop/scripts/publish.ts
@@ -6,6 +6,8 @@ let releaseVersion = process.env.RELEASE_VERSION || "";
 if (releaseVersion.startsWith("refs/tags/")) releaseVersion = releaseVersion.replace(/^refs\/tags\//, "");
 if (releaseVersion.startsWith("v")) releaseVersion = releaseVersion.slice(1);
 pkgJson.version = releaseVersion;
+// this is required to show the correct app name (instead of @aca/desktop)
+pkgJson.name = "Acapela";
 
 console.info(`updating version to ${pkgJson.version}`);
 writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2) + "\n");
