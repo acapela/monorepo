bcdaa1ce2970857ba66c6266d64d06d78137163d
Adam Pietrasiak <adam@pietrasiak.com>
2022-01-26 14:35:04 +0100

    feat: double click topbar to maximize (#924)
    * feat: double click to maximize
    
    * chore: cleanup

--- desktop/bridge/system.ts
+++ desktop/bridge/system.ts
@@ -2,3 +2,4 @@ import { createInvokeBridge } from "./base/channels";
 
 export const restartApp = createInvokeBridge("restart-app");
 export const clearAllData = createInvokeBridge("clear-all-data");
+export const toggleMaximize = createInvokeBridge("toggle-maximize");
