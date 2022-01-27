3bfc457f12e25723a262c24e74b263b9b35d8ae0
Gregor <gregorwbr@gmail.com>
2022-01-27 09:56:37 +0100

    feat(desktop): declarative browser-view (#925)

--- desktop/bridge/preview.ts
+++ desktop/bridge/preview.ts
@@ -1,8 +1,9 @@
 import { createInvokeBridge } from "./base/channels";
 
-interface RequestPreviewData {
-  url: string;
-}
+type Data = { url: string; id: string };
 
-export const requestPreloadInMainWindow = createInvokeBridge<Boolean, RequestPreviewData>("request-preload-in-main");
-export const requestPreviewInMainWindow = createInvokeBridge<Boolean, RequestPreviewData>("request-preview-in-main");
+export const registerBrowserViewPreload = createInvokeBridge<Boolean, Data>("register-browser-view-preload");
+export const unregisterBrowserViewPreload = createInvokeBridge<Boolean, Data>("unregister-browser-view-preload");
+
+export const showBrowserView = createInvokeBridge<Boolean, Data & { bounds: Electron.Rectangle }>("show-browser-view");
+export const hideBrowserView = createInvokeBridge<Boolean, Data>("hide-browser-view");
--- desktop/electron/bridgeHandlers/preview.ts
+++ desktop/electron/bridgeHandlers/preview.ts
@@ -1,56 +1,108 @@
-import { BrowserView, BrowserWindow, Rectangle } from "electron";
+import { BrowserView } from "electron";
+import { mapValues } from "lodash";
 
-import { requestPreloadInMainWindow, requestPreviewInMainWindow } from "@aca/desktop/bridge/preview";
+import {
+  hideBrowserView,
+  registerBrowserViewPreload,
+  showBrowserView,
+  unregisterBrowserViewPreload,
+} from "@aca/desktop/bridge/preview";
 import { appState } from "@aca/desktop/electron/appState";
+import { assert, assertDefined } from "@aca/shared/assert";
 
-const preloads = new Map<string, BrowserView>();
+const DESTROY_BROWSER_VIEW_TIMEOUT_MS = 5000;
 
-const PRVIEW_SIDEBAR_OFFSET = 300;
+type StringURL = string;
+const browserViewRefs: Record<
+  StringURL,
+  {
+    view: BrowserView;
+    subscribers: Set<string>;
+    destroyTimeout: null | ReturnType<typeof setTimeout>;
+  }
+> = {};
 
-function calculatePreviewBounds(targetWindow: BrowserWindow): Rectangle {
-  const { width, height } = targetWindow.getBounds();
+async function registerBrowserViewSubscriber(url: string, id: string) {
+  const { mainWindow } = appState;
+  assert(mainWindow, "mainWindow is not defined");
 
-  return {
-    height,
-    width: width - PRVIEW_SIDEBAR_OFFSET,
-    y: 0,
-    x: PRVIEW_SIDEBAR_OFFSET,
-  };
+  let ref = browserViewRefs[url];
+  if (ref) {
+    if (ref.destroyTimeout) {
+      console.info("cancelling browser view destroy timeout for", url);
+      clearTimeout(ref.destroyTimeout);
+      ref.destroyTimeout = null;
+    }
+    ref.subscribers.add(id);
+  } else {
+    const browserView = new BrowserView();
+    browserView.setBounds(mainWindow.getBounds());
+    ref = browserViewRefs[url] = {
+      view: browserView,
+      subscribers: new Set([id]),
+      destroyTimeout: null,
+    };
+    await browserView.webContents.loadURL(url);
+  }
+
+  return ref.view;
 }
 
-const replaceArchiveWithMessages = (url: string) => url.replace("/archives/", "/messages/");
+function unregisterBrowserViewSubscriber(url: string, id: string) {
+  const ref = assertDefined(browserViewRefs[url], `browserViewRef is missing for: ${url}`);
+  const wasPresent = ref.subscribers.delete(id);
+  if (!wasPresent) {
+    // eslint-disable-next-line no-console
+    console.trace("no subscribers found for url", url, "with id", id);
+  } else if (ref.subscribers.size == 0 && !ref.destroyTimeout) {
+    console.info("starting browser view destroy timeout for", url);
+    ref.destroyTimeout = setTimeout(() => {
+      console.info("destroying browser view for", url);
+      // As of this writing destroy() is an undocumented method for getting rid of a BrowserView.
+      // https://github.com/electron/electron/issues/10096#issuecomment-882837830
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (browserViewRefs[url].view.webContents as any).destroy();
+      delete browserViewRefs[url];
+    }, DESTROY_BROWSER_VIEW_TIMEOUT_MS);
+  }
+}
 
 export function initPreviewHandler() {
-  requestPreloadInMainWindow.handle(async ({ url }) => {
-    url = replaceArchiveWithMessages(url);
-    console.info("preloading", url);
-    if (!preloads.has(url)) {
-      const browserView = new BrowserView();
-      preloads.set(url, browserView);
-      await browserView.webContents.loadURL(url);
-    }
+  registerBrowserViewPreload.handle(async ({ url, id }) => {
+    await registerBrowserViewSubscriber(url, id);
     return true;
   });
 
-  requestPreviewInMainWindow.handle(async ({ url }) => {
-    url = replaceArchiveWithMessages(url);
-    const mainWindow = appState.mainWindow;
+  unregisterBrowserViewPreload.handle(async ({ url, id }) => {
+    unregisterBrowserViewSubscriber(url, id);
+    return true;
+  });
 
-    if (!mainWindow) return false;
+  showBrowserView.handle(async ({ url, id, bounds }) => {
+    const { mainWindow } = appState;
+    assert(mainWindow, "mainWindow is not defined");
 
-    const currentViews = mainWindow.getBrowserViews();
-
-    currentViews.forEach((view) => {
-      mainWindow.removeBrowserView(view);
-    });
-
-    let browserView = preloads.get(url);
-    if (!browserView) {
-      browserView = new BrowserView();
-      await browserView.webContents.loadURL(url);
-    }
+    const browserView = await registerBrowserViewSubscriber(url, id);
     mainWindow.setBrowserView(browserView);
-    browserView.setBounds(calculatePreviewBounds(mainWindow));
+
+    const roundedBounds = mapValues(bounds, (value) => Math.round(value));
+    for (const { view } of Object.values(browserViewRefs)) {
+      // We'll update bounds for all BrowserViews here to make them the correct size for immediate display
+      view.setBounds(roundedBounds);
+    }
+
+    return true;
+  });
+
+  hideBrowserView.handle(async ({ url, id }) => {
+    const browserView = browserViewRefs[url]?.view;
+    if (browserView) {
+      const { mainWindow } = appState;
+      assert(mainWindow, "mainWindow is not defined");
+      mainWindow.removeBrowserView(browserView);
+    }
+
+    unregisterBrowserViewSubscriber(url, id);
 
     return true;
   });
