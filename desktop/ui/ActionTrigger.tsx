a2b5e574da300dce5512d3f282c6328144fa6091
Adam Pietrasiak <adam@pietrasiak.com>
2022-01-27 15:27:37 +0100

    chore: comments

--- desktop/ui/ActionTrigger.tsx
+++ desktop/ui/ActionTrigger.tsx
@@ -11,6 +11,11 @@ interface Props {
   children: ReactNode;
 }
 
+/**
+ * Element that will run action if clicked, but can have any custom children.
+ *
+ * Useful for rendering action buttons that have custom UI (nor Button or IconButton, etc.)
+ */
 export const ActionTrigger = styledObserver(function ActionTrigger({ action, target, children }: Props) {
   const context = createActionContext(target);
 
