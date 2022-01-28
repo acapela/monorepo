1649bc63c598479c18a4205972b5b6d3bf6e5bbf
Christoph Witzko <github@christophwitzko.com>
2022-01-28 14:14:08 +0100

    fix(desktop): preload loading error

--- desktop/electron/preload.ts
+++ desktop/electron/preload.ts
@@ -1,4 +1,4 @@
-import * as Sentry from "@sentry/electron";
+import * as Sentry from "@sentry/electron/dist/renderer";
 import { IpcRendererEvent, contextBridge, ipcRenderer } from "electron";
 
 import { ElectronChannelSubscriber } from "@aca/desktop/bridge/base/channels";
