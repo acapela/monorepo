605ee0690083ee2ffa3d5305685e3bc99997f24d
Adam Pietrasiak <adam@pietrasiak.com>
2022-02-01 00:37:14 +0100

    chore: cleanup

--- frontend/pages/app/download.tsx
+++ frontend/pages/app/download.tsx
@@ -1,15 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-import { useCurrentUserTokenData } from "@aca/frontend/authentication/useCurrentUser";
 import { FocusedActionLayout } from "@aca/frontend/layouts/FocusedActionLayout/FocusedActionLayout";
 import { openInNewTab } from "@aca/frontend/utils/openInNewTab";
 import { PageMeta } from "@aca/frontend/utils/PageMeta";
-import { LoginOptionsView } from "@aca/frontend/views/LoginOptionsView";
 import { Button } from "@aca/ui/buttons/Button";
 
 export default function LoginPage(): JSX.Element {
-  const user = useCurrentUserTokenData();
   const [downloadURL, setDownloadURL] = useState<string | null>(null);
 
   useEffect(() => {
@@ -30,12 +27,9 @@ export default function LoginPage(): JSX.Element {
     <>
       <PageMeta title="Download Acapela" />
       <FocusedActionLayout title={`Download Acapela`} description={`Acapela helps you manage all your notifications`}>
-        {!user && <LoginOptionsView />}
-        {!!user && (
-          <Button kind="primary" onClick={handleDownload} isDisabled={downloadURL === null}>
-            Download (MacOS)
-          </Button>
-        )}
+        <Button kind="primary" onClick={handleDownload} isDisabled={downloadURL === null}>
+          Download (MacOS)
+        </Button>
       </FocusedActionLayout>
     </>
   );
