448409417c43ed70621e2a26401fe654f8219fad
Christoph Witzko <github@christophwitzko.com>
2022-02-03 19:38:24 +0100

    feat: add release script

--- desktop/scripts/update-package.ts
+++ desktop/scripts/update-package.ts
@@ -2,10 +2,16 @@ import { readFileSync, writeFileSync } from "fs";
 
 const pkgJson = JSON.parse(readFileSync("./package.json", "utf-8"));
 
-pkgJson.version = require("./release-version");
-// this is required to show the correct app name (instead of @aca/desktop)
-pkgJson.name = "Acapela";
+if (process.argv.pop() === "reset") {
+  console.info("resetting package.json");
+  pkgJson.name = "@aca/desktop";
+  pkgJson.version = "999.0.0-do-not-autouptdate";
+} else {
+  // this is required to show the correct app name (instead of @aca/desktop)
+  pkgJson.name = "Acapela";
+  pkgJson.version = require("./release-version");
+  console.info(`updating version to ${pkgJson.version}`);
+}
 
-console.info(`updating version to ${pkgJson.version}`);
 writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2) + "\n");
 console.info("done.");
