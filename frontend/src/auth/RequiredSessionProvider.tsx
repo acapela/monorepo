ef3a1fcade3f78a529b6a1df02bcf53cbfed0d6c
Omar Duarte <omarduarte@gmail.com>
2022-02-03 21:07:12 +0200

    fix: frontend themes

--- frontend/src/auth/RequiredSessionProvider.tsx
+++ frontend/src/auth/RequiredSessionProvider.tsx
@@ -5,6 +5,9 @@ import { ReactNode } from "react";
 import { FocusedActionLayout } from "@aca/frontend/layouts/FocusedActionLayout/FocusedActionLayout";
 import { LoginOptionsView } from "@aca/frontend/views/LoginOptionsView";
 import { useConst } from "@aca/shared/hooks/useConst";
+import { AppThemeProvider } from "@aca/ui/theme";
+
+import { BuiltInStyles } from "../../pages/_app";
 
 interface Props {
   session: Session | null;
@@ -21,10 +24,17 @@ export function RequiredSessionProvider({ session, children, dontRequireSession 
 
   return (
     <SessionProvider session={sessionFromServer}>
-      {!sessionFromServer && !dontRequireSession && (
-        <FocusedActionLayout title="Log in to start using Acapela">{<LoginOptionsView />}</FocusedActionLayout>
-      )}
-      {(sessionFromServer || dontRequireSession) && children}
+      {/* SessionProvider seems to be eating up the Styled Components context. */}
+      {/* AppThemeProvider should be set nested inside SessionProvider to avoid this. */}
+      <AppThemeProvider>
+        <BuiltInStyles />
+        {!sessionFromServer && !dontRequireSession && (
+          <FocusedActionLayout title="Log in to start using Acapela">
+            <LoginOptionsView />
+          </FocusedActionLayout>
+        )}
+        {(sessionFromServer || dontRequireSession) && children}
+      </AppThemeProvider>
     </SessionProvider>
   );
 }
