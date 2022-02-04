ee9420b9ad8f273687c3b00671461a5e8ca7bbc8
Christoph Witzko <github@christophwitzko.com>
2022-02-04 12:27:30 +0100

    ci: upload app to staging repo

--- desktop/scripts/update-package.ts
+++ desktop/scripts/update-package.ts
@@ -1,16 +1,21 @@
 import { readFileSync, writeFileSync } from "fs";
 
 const pkgJson = JSON.parse(readFileSync("./package.json", "utf-8"));
-
-if (process.argv.pop() === "reset") {
+const arg = process.argv.pop();
+if (arg === "reset") {
   console.info("resetting package.json");
   pkgJson.name = "@aca/desktop";
   pkgJson.version = "999.0.0-do-not-autouptdate";
+  pkgJson.repository = "github:weareacapela/releases";
 } else {
   // this is required to show the correct app name (instead of @aca/desktop)
   pkgJson.name = "Acapela";
   pkgJson.version = require("./release-version");
   console.info(`updating version to ${pkgJson.version}`);
+  if (arg === "staging") {
+    pkgJson.repository = "github:weareacapela/releases-staging";
+    console.info(`updating repository to ${pkgJson.repository}`);
+  }
 }
 
 writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2) + "\n");
--- desktop/scripts/upload-release.sh
+++ desktop/scripts/upload-release.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-rm -rf dist-final
-mkdir dist-final
+rm -rf dist-final && mkdir dist-final
 
 echo "installing ghr..."
 wget https://github.com/tcnksm/ghr/releases/download/v0.14.0/ghr_v0.14.0_darwin_amd64.zip
@@ -11,12 +10,15 @@ unzip ghr_*
 ghr_bin=$(realpath ghr_*/ghr)
 $ghr_bin --version
 
-echo "packing production release..."
-tar -cvf ./dist-final/production.tar ./dist-electron-production/Acapela-* ./dist-electron-production/latest-mac.yml
-
 echo "adding staging release..."
 cp ./dist-electron-staging/Acapela-* ./dist-final
 cp ./dist-electron-staging/latest-mac.yml ./dist-final
+echo "uploading staging release..."
+$ghr_bin -u weareacapela -r releases-staging "$RELEASE_VERSION" ./dist-final
 
-echo "uploading..."
-$ghr_bin -u weareacapela -r monorepo "$RELEASE_VERSION"
+rm -rf dist-final && mkdir dist-final
+
+echo "packing production release..."
+tar -cvf ./dist-final/production.tar ./dist-electron-production/Acapela-* ./dist-electron-production/latest-mac.yml
+echo "uploading production release..."
+$ghr_bin -u weareacapela -r monorepo "$RELEASE_VERSION" ./dist-final
