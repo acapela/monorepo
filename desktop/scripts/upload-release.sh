8f0d1ab09df153f31d8c30b32360af41995b413b
Christoph Witzko <github@christophwitzko.com>
2022-02-04 12:57:02 +0100

    fix: install ghr correctly

--- desktop/scripts/upload-release.sh
+++ desktop/scripts/upload-release.sh
@@ -7,18 +7,18 @@ rm -rf dist-final && mkdir dist-final
 echo "installing ghr..."
 wget https://github.com/tcnksm/ghr/releases/download/v0.14.0/ghr_v0.14.0_darwin_amd64.zip
 unzip ghr_*
-ghr_bin=$(realpath ghr_*/ghr)
-$ghr_bin --version
+mv ghr_*/ghr ./ghr
+./ghr --version
 
 echo "adding staging release..."
 cp ./dist-electron-staging/Acapela-* ./dist-final
 cp ./dist-electron-staging/latest-mac.yml ./dist-final
 echo "uploading staging release..."
-$ghr_bin -u weareacapela -r releases-staging "$RELEASE_VERSION" ./dist-final
+./ghr -u weareacapela -r releases-staging "$RELEASE_VERSION" ./dist-final
 
 rm -rf dist-final && mkdir dist-final
 
 echo "packing production release..."
 tar -cvf ./dist-final/production.tar ./dist-electron-production/Acapela-* ./dist-electron-production/latest-mac.yml
 echo "uploading production release..."
-$ghr_bin -u weareacapela -r monorepo "$RELEASE_VERSION" ./dist-final
+./ghr -u weareacapela -r monorepo "$RELEASE_VERSION" ./dist-final
