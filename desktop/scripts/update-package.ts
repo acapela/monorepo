136b03d26c4a4b6ae2beab232c67889274f01661
Christoph Witzko <github@christophwitzko.com>
2022-02-08 16:00:21 +0100

    feat: change appId, name and icon

--- desktop/scripts/update-package.ts
+++ desktop/scripts/update-package.ts
@@ -7,11 +7,20 @@ if (arg === "reset") {
   pkgJson.name = "@aca/desktop";
   pkgJson.version = "999.0.0-do-not-autouptdate";
   pkgJson.repository = "github:weareacapela/releases";
+  pkgJson.build.appId = "com.desktop.acapela";
 } else {
   // this is required to show the correct app name (instead of @aca/desktop)
-  pkgJson.name = "Acapela";
+  if (arg === "staging") pkgJson.name = "Acapela Staging";
+  else pkgJson.name = "Acapela";
+  console.info(`updating name to ${pkgJson.name}`);
+
+  if (arg === "staging") pkgJson.build.appId = "com.desktop.acapela.staging";
+  else pkgJson.build.appId = "com.desktop.acapela";
+  console.info(`updating appId to ${pkgJson.build.appId}`);
+
   pkgJson.version = require("./release-version");
   console.info(`updating version to ${pkgJson.version}`);
+
   if (arg === "staging") {
     pkgJson.repository = "github:weareacapela/releases-staging";
     console.info(`updating repository to ${pkgJson.repository}`);
