bd45b6e73811edee01f233646c2dd52f4e295abf
Christoph Witzko <github@christophwitzko.com>
2022-02-08 18:29:02 +0100

    feat: use different icon for staging and production

--- desktop/scripts/update-package.ts
+++ desktop/scripts/update-package.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from "fs";
+import { copyFileSync, readFileSync, writeFileSync } from "fs";
 
 const pkgJson = JSON.parse(readFileSync("./package.json", "utf-8"));
 const arg = process.argv.pop();
@@ -9,22 +9,26 @@ if (arg === "reset") {
   pkgJson.repository = "github:weareacapela/releases";
   pkgJson.build.appId = "com.desktop.acapela";
 } else {
+  const isStaging = arg === "staging";
   // this is required to show the correct app name (instead of @aca/desktop)
-  if (arg === "staging") pkgJson.name = "Acapela Staging";
+  if (isStaging) pkgJson.name = "Acapela Staging";
   else pkgJson.name = "Acapela";
   console.info(`updating name to ${pkgJson.name}`);
 
-  if (arg === "staging") pkgJson.build.appId = "com.desktop.acapela.staging";
+  if (isStaging) pkgJson.build.appId = "com.desktop.acapela.staging";
   else pkgJson.build.appId = "com.desktop.acapela";
   console.info(`updating appId to ${pkgJson.build.appId}`);
 
   pkgJson.version = require("./release-version");
   console.info(`updating version to ${pkgJson.version}`);
 
-  if (arg === "staging") {
+  if (isStaging) {
     pkgJson.repository = "github:weareacapela/releases-staging";
     console.info(`updating repository to ${pkgJson.repository}`);
   }
+
+  if (isStaging) copyFileSync("./build/icon-staging.png", "./build/icon.png");
+  else copyFileSync("./build/icon-production.png", "./build/icon.png");
 }
 
 writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2) + "\n");
