f2c3e654ba71081e73567b86e588cebd54e5976e
Christoph Witzko <github@christophwitzko.com>
2022-02-09 13:15:40 +0100

    feat: upload source maps in linux job

--- desktop/scripts/upload-sourcemaps.js
+++ desktop/scripts/upload-sourcemaps.js
@@ -4,33 +4,33 @@
 const fs = require("fs");
 const SentryCli = require("@sentry/cli");
 const glob = require("glob-promise");
+const version = require("./release-version");
 
 const cli = new SentryCli("./sentry.properties");
 
 async function main() {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const version = require("./release-version");
   if (!version) {
-    console.error("version is missing in package.json");
+    console.error("version is missing");
     process.exit(1);
     return;
   }
-  let isUpdate = false;
-  if (process.argv.pop() === "update") {
-    console.info("only update");
-    isUpdate = true;
+
+  console.info(`creating new sentry release: ${version}`);
+  await cli.execute(["releases", "new", version], true);
+
+  for (let env of ["staging", "production"]) {
+    console.info(`uploading ${env} sourcemaps...`);
+    await cli.execute(
+      ["releases", "files", version, "upload-sourcemaps", `./dist-${env}`, "--url-prefix", "app:///dist"],
+      true
+    );
   }
-  if (!isUpdate) {
-    console.info(`creating new sentry release: ${version}`);
-    await cli.execute(["releases", "new", version], true);
-  }
-  await cli.execute(["releases", "files", version, "upload-sourcemaps", "./dist", "--url-prefix", "app:///dist"], true);
-  if (!isUpdate) {
-    console.info("finalizing release...");
-    await cli.execute(["releases", "finalize", version], true);
-  }
+
+  console.info("finalizing release...");
+  await cli.execute(["releases", "finalize", version], true);
+
   console.info("removing source maps...");
-  const sourceMaps = await glob.promise("./dist/**/*.map");
+  const sourceMaps = await glob.promise("./dist-*/**/*.map");
   for (const sourceMap of sourceMaps) {
     console.info(`deleting ${sourceMap}`);
     await fs.promises.unlink(sourceMap);
