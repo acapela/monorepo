27285676bce20b17de2c17c95ca902a92e6fa950
Adam Pietrasiak <adam@pietrasiak.com>
2022-02-09 12:44:01 +0100

    fix: types

--- desktop/assets/integrations/logos/index.ts
+++ desktop/assets/integrations/logos/index.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
 import figma from "./figma.png";
+// @ts-ignore
 import linear from "./linear.png";
+// @ts-ignore
 import notion from "./notion.png";
+// @ts-ignore
 import slack from "./slack.png";
 
 export const integrationLogos = { figma, linear, notion, slack };
