1a09031011414b2ff6e66bc610c7c5abe0c6784c
Omar Duarte <omarduarte@gmail.com>
2022-02-10 14:14:37 +0200

    feat: open linear in-app

--- desktop/domains/integrations/linear.tsx
+++ desktop/domains/integrations/linear.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 
 import { integrationLogos } from "@aca/desktop/assets/integrations/logos";
+import { getIsAppInstalledLocally } from "@aca/desktop/bridge/apps";
 import { linearAuthTokenBridgeValue, loginLinearBridge } from "@aca/desktop/bridge/auth";
 
 import { IntegrationIcon } from "./IntegrationIcon";
 import { IntegrationClient } from "./types";
+
+const LINEAR_URL_SCHEME = "linear:/";
+const linearURL = "https://linear.app";
 
 export const linearIntegrationClient: IntegrationClient = {
   kind: "integration",
   name: "Linear",
   description: "New issues, task assignments and comments.",
   getIsConnected: () => !!linearAuthTokenBridgeValue.get(),
+  convertToLocalAppUrl: async (url: string) => {
+    const isAppInstalledLocally = await getIsAppInstalledLocally(LINEAR_URL_SCHEME);
+    if (isAppInstalledLocally) {
+      return url.replace(linearURL, LINEAR_URL_SCHEME);
+    }
+    return url;
+  },
   async connect() {
     await loginLinearBridge();
   },
