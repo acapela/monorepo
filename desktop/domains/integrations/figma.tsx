c3216ff5fa3bfaa0c780f2b4f547c83ce3d0e539
Omar Duarte <omarduarte@gmail.com>
2022-02-10 13:06:27 +0200

    feat: open figma locally

--- desktop/domains/integrations/figma.tsx
+++ desktop/domains/integrations/figma.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 import { integrationLogos } from "@aca/desktop/assets/integrations/logos";
+import { getIsAppInstalledLocally } from "@aca/desktop/bridge/apps";
 import { clearServiceCookiesBridge, figmaAuthTokenBridgeValue, loginFigmaBridge } from "@aca/desktop/bridge/auth";
 
 import { IntegrationIcon } from "./IntegrationIcon";
 import { IntegrationClient } from "./types";
+
+const FIGMA_URL_SCHEME = "figma:/";
+const figmaURL = "https://www.figma.com";
 
 export const figmaIntegrationClient: IntegrationClient = {
   kind: "integration",
@@ -13,7 +17,14 @@ export const figmaIntegrationClient: IntegrationClient = {
   getIsConnected: () => !!figmaAuthTokenBridgeValue.get(),
   disconnect: async () => {
     figmaAuthTokenBridgeValue.reset();
-    await clearServiceCookiesBridge({ url: "https://www.figma.com" });
+    await clearServiceCookiesBridge({ url: figmaURL });
+  },
+  convertToLocalAppUrl: async (url: string) => {
+    const isAppInstalledLocally = await getIsAppInstalledLocally(FIGMA_URL_SCHEME);
+    if (isAppInstalledLocally) {
+      return url.replace(figmaURL, FIGMA_URL_SCHEME);
+    }
+    return url;
   },
   async connect() {
     await loginFigmaBridge();
