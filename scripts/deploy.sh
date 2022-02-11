dceeab6bcdef9eeb2092cd083bacf6e8b9b78d15
Christoph Witzko <github@christophwitzko.com>
2022-02-11 13:49:08 +0100

    ci: create and pass deploy info

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -71,7 +71,8 @@ if [[ "${APP_NAME}" == "all" ]]; then
   ./scripts/deploy.sh -s "$STAGE" -a api -v "$APP_VERSION" -S
   ./scripts/deploy.sh -s "$STAGE" -a frontend -v "$APP_VERSION" -S
   ./scripts/wait-for-release.sh "$STAGE" "$APP_VERSION"
-  ./scripts/move-app-to-releases.sh "$STAGE" "$APP_VERSION"
+  echo "creating deploy-info.txt"
+  echo "${STAGE}@${APP_VERSION}" > deploy-info.txt
   exit
 fi
 
