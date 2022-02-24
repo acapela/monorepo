8cb03f22229a1743a15ad30b6bef035d43370c4e
Adam Pietrasiak <adam@pietrasiak.com>
2022-02-24 13:33:39 +0100

    fix: cleanup

--- shared/object.ts
+++ shared/object.ts
@@ -107,28 +107,6 @@ type FromEntries<T> = T extends [infer Key, any][]
 
 export type FromEntriesWithReadOnly<T> = FromEntries<DeepWriteable<T>>;
 
-export function isObject(item) {
-  return item && typeof item === "object" && !Array.isArray(item);
-}
-
-export function mergeDeep(target, ...sources) {
-  if (!sources.length) return target;
-  const source = sources.shift();
-
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) Object.assign(target, { [key]: {} });
-        mergeDeep(target[key], source[key]);
-      } else {
-        Object.assign(target, { [key]: source[key] });
-      }
-    }
-  }
-
-  return mergeDeep(target, ...sources);
-}
-
 export function deepMerge<T>(source: T, overwrites: DeepPartial<T>) {
   return mergeWith(overwrites, source, (value: unknown, srcValue: unknown) => {
     if (isPlainObject(srcValue) || isPlainObject(value)) {
