028b290b44862e470566edec29ccc6eddc118bf4
Gregor Weber <gregorwbr@gmail.com>
2022-03-07 11:43:08 +0000

    feat(slack): oauth test route

--- backend/src/slack/setup.ts
+++ backend/src/slack/setup.ts
@@ -1,5 +1,8 @@
 import { Express } from "express";
 
+import { getIndividualSlackInstallURL } from "@aca/backend/src/slack/install";
+import { getPublicBackendURL } from "@aca/backend/src/utils";
+
 import { setupSlackCapture } from "../notificationCapture/slack";
 import { setupSlackActionHandlers } from "./actions";
 import { slackApp, slackReceiver } from "./app";
@@ -10,6 +13,18 @@ import { setupViewRequestModal } from "./view-request-modal";
 
 export function setupSlack(app: Express) {
   app.use("/api", slackReceiver.router);
+
+  app.use("/api/v1/slack/oauth/test", async (req, res) => {
+    res.redirect(
+      await getIndividualSlackInstallURL({
+        userId: "??",
+        redirectURL: (await getPublicBackendURL()) + "/v1/slack/oauth/success",
+      })
+    );
+  });
+  app.use("/api/v1/slack/oauth/success", (req, res) => {
+    res.send("Success! Your workspace can be integrated with Acapela.");
+  });
 
   setupHomeTab(slackApp);
 
