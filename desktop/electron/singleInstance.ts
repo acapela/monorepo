0ae0fd71d35151232992ceb1fa61ec492fde453c
Adam Pietrasiak <adam@pietrasiak.com>
2022-03-07 11:56:11 +0100

    fix: try to disable 2nd instance lock to see if it fixes auto updater

--- desktop/electron/singleInstance.ts
+++ desktop/electron/singleInstance.ts
@@ -1,28 +1,23 @@
-import { app } from "electron";
-
-import { appState } from "./appState";
-import { initializeMainWindow } from "./mainWindow";
-
 export function initializeSingleInstanceLock() {
-  const isFirstInstance = app.requestSingleInstanceLock();
-
-  if (!isFirstInstance) {
-    app.quit();
-    return;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  app.on("second-instance", (event, commandLine, workingDirectory) => {
-    const { mainWindow } = appState;
-    if (!mainWindow) {
-      initializeMainWindow();
-      return;
-    }
-
-    if (mainWindow.isMinimized()) {
-      mainWindow.restore();
-    }
-
-    mainWindow.focus();
-  });
+  /**
+   * It is possible that instance lock is the reason auto-updater fails. Not sure.
+   * Case: updater opens '2nd' instance before closing 1st one, and it is blocked.
+   */
+  // const isFirstInstance = app.requestSingleInstanceLock();
+  // if (!isFirstInstance) {
+  //   app.quit();
+  //   return;
+  // }
+  // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // app.on("second-instance", (event, commandLine, workingDirectory) => {
+  //   const { mainWindow } = appState;
+  //   if (!mainWindow) {
+  //     initializeMainWindow();
+  //     return;
+  //   }
+  //   if (mainWindow.isMinimized()) {
+  //     mainWindow.restore();
+  //   }
+  //   mainWindow.focus();
+  // });
 }
