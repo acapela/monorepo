274875de03f32a6c285388262bcdeb664eb77c82
Omar Duarte <omarduarte@gmail.com>
2022-03-09 15:01:10 +0000

    feat: notion_space_user in clientdb

---
+++ desktop/clientdb/notification/notion/notionSpaceUser.ts
@@ -0,0 +1,50 @@
+import gql from "graphql-tag";
+
+import { EntityByDefinition, defineEntity } from "@aca/clientdb";
+import { createHasuraSyncSetupFromFragment } from "@aca/clientdb/sync";
+import { getFragmentKeys } from "@aca/clientdb/utils/analyzeFragment";
+import { getGenericDefaultData } from "@aca/clientdb/utils/getGenericDefaultData";
+import {
+  NotionSpaceUserFragment,
+  Notion_Space_User_Bool_Exp,
+  Notion_Space_User_Constraint,
+  Notion_Space_User_Insert_Input,
+  Notion_Space_User_Set_Input,
+} from "@aca/gql";
+
+const notionSpaceUser = gql`
+  fragment notionSpaceUser on notion_space_user {
+    id
+    notion_space_id
+    user_id
+    first_synced_at
+    is_sync_enabled
+    created_at
+    updated_at
+  }
+`;
+
+type NotionSpaceUserConstraints = {
+  key: Notion_Space_User_Constraint;
+  update: Notion_Space_User_Set_Input;
+  insert: Notion_Space_User_Insert_Input;
+  where: Notion_Space_User_Bool_Exp;
+};
+
+export const notionSpaceUserEntity = defineEntity<NotionSpaceUserFragment>({
+  name: "notion_space_user",
+  updatedAtField: "updated_at",
+  keyField: "id",
+  keys: getFragmentKeys<NotionSpaceUserFragment>(notionSpaceUser),
+  getDefaultValues: () => ({
+    __typename: "notion_space_user",
+    ...getGenericDefaultData(),
+  }),
+  sync: createHasuraSyncSetupFromFragment<NotionSpaceUserFragment, NotionSpaceUserConstraints>(notionSpaceUser, {
+    insertColumns: ["id", "notion_space_id", "user_id", "is_sync_enabled", "created_at", "updated_at"],
+    updateColumns: ["is_sync_enabled", "updated_at"],
+    upsertConstraint: "notion_space_user_user_id_notion_space_id_key",
+  }),
+});
+
+export type NotionSpaceUserEntity = EntityByDefinition<typeof notionSpaceUserEntity>;
