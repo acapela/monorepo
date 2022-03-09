63a65e4117314444042094c8a69444cc5461387e
Omar Duarte <omarduarte@gmail.com>
2022-03-09 12:39:58 +0000

    feat: notion data structure

---
+++ infrastructure/hasura/migrations/default/1646829331153_notion-spaces/down.sql
@@ -0,0 +1,22 @@
+
+alter table "public"."notification_notion" drop constraint "notification_notion_notion_space_id_fkey";
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."notification_notion" add column "notion_space_id" uuid
+--  null;
+
+DROP TABLE "public"."notion_space_user";
+
+alter table "public"."notion_space" rename to "notification_space";
+
+alter table "public"."notification_space" alter column "created_by" set not null;
+
+alter table "public"."notification_space" drop constraint "notification_space_created_by_fkey";
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."notification_space" add column "created_by" uuid
+--  not null;
+
+DROP TABLE "public"."notification_space";
