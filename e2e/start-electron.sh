746cdcd214ea3d9e05250656ad82accd9c6ccf64
cojack <xcojack@gmail.com>
2022-03-15 11:34:46 +0100

    wip(ci): set ELECTRON_IS_DEV and setup Xvfb screen and resolution

--- e2e/start-electron.sh
+++ e2e/start-electron.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
+export ELECTRON_IS_DEV=0
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 xvfb-run ../desktop/dist-electron/linux-unpacked/@acadesktop
