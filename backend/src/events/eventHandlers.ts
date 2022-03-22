12ba80ce5bc9238ec2d745a4cd5fd0fba78a4c02
Gregor <gregorwbr@gmail.com>
2022-03-22 14:53:08 +0000

    fix(slack): propagate notification delete (#1192)

--- backend/src/events/eventHandlers.ts
+++
@@ -1,9 +0,0 @@
-import { Account, LinearIssue, User } from "@aca/db";
-
-import { createHasuraEventsHandler } from "../hasura";
-
-export const hasuraEvents = createHasuraEventsHandler<{
-  account_updates: Account;
-  user_updates: User;
-  linear_issue_updates: LinearIssue;
-}>();
