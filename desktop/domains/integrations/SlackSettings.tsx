f1582c70050b151078bdc9a9d8e52c33d86fa2ef
Omar Duarte <omarduarte@gmail.com>
2022-03-23 10:01:43 +0000

    feat: remove previous channel selector

--- desktop/domains/integrations/SlackSettings.tsx
+++ desktop/domains/integrations/SlackSettings.tsx
@@ -1,11 +1,9 @@
 import { observer } from "mobx-react";
 import React from "react";
-import styled from "styled-components";
 
 import { toggleSlackAutoResolve } from "@aca/desktop/actions/slack";
 import { accountStore } from "@aca/desktop/store/account";
 import { ActionTrigger } from "@aca/desktop/ui/ActionTrigger";
-import { SlackConversationsDropdown } from "@aca/desktop/ui/Filters/FilterEditorSlack";
 import { SettingRow } from "@aca/desktop/ui/settings/SettingRow";
 import { VStack } from "@aca/ui/Stack";
 import { Toggle } from "@aca/ui/toggle";
@@ -26,23 +24,7 @@ export const SlackSettings = observer(() => {
         </ActionTrigger>
       </SettingRow>
 
-      <VStack gap={8}>
-        <SettingRow title="Include channels" description="Messages in these channels will turn into notifications.">
-          <UISlackConversationsDropdown
-            placeholder="None"
-            checkSelected={(id) => user.slack_included_channels.includes(id)}
-            onChange={(channels) => {
-              user.update({ slack_included_channels: channels.map((c) => c.id) });
-            }}
-          />
-        </SettingRow>
-      </VStack>
-
       <SlackChannelsByTeamFilters />
     </VStack>
   );
 });
-
-const UISlackConversationsDropdown = styled(SlackConversationsDropdown)`
-  max-width: 200px;
-`;
