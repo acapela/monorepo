9ba55d6e8c9c30bde0f1e58169a914c4ebd21961
Adam Pietrasiak <adam@pietrasiak.com>
2022-03-23 15:14:36 +0100

    chore: cleanups

--- desktop/domains/group/utils.ts
+++ desktop/domains/group/utils.ts
@@ -1,3 +1,13 @@
+/**
+ * countRepeats([{name: "Adam"}, {name: "Adam"}, {name: "Omar"}], item => item.name)
+ *
+ * will return map: {
+ *   Adam: 2,
+ *   Omar: 1
+ * }
+ *
+ * Note: if getter returns undefined - it is not counted
+ */
 export function countRepeats<I, R>(input: I[], getter: (item: I) => R | undefined): Map<R, number> {
   const resultsMap = new Map<R, number>();
 
