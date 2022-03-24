1225373f535f37af7cf82ba34740ad2c7fbb4e80
Christoph Witzko <github@christophwitzko.com>
2022-03-24 12:19:39 +0100

    fix: send slack notification after app release

--- desktop/scripts/publish.sh
+++ desktop/scripts/publish.sh
@@ -12,3 +12,4 @@ echo "building and publishing electron app for $stage..."
 ./scripts/update-package.js "$stage"
 electron-builder build --mac default --universal -p always
 ./scripts/update-package.js reset
+../scripts/send-slack-message.sh ":white_check_mark: Acapela app with version <https://github.com/weareacapela/releases/releases/tag/${RELEASE_VERSION}|*${RELEASE_VERSION}*> was successfully released" "$stage" "product"
