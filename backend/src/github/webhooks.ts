d764ccce1547b8108bb022ac30f004eb7c429ee1
Christoph Witzko <github@christophwitzko.com>
2022-03-26 13:29:34 +0100

    feat(github): handle pull request review requests

--- backend/src/github/webhooks.ts
+++ backend/src/github/webhooks.ts
@@ -1,4 +1,5 @@
 import { EmitterWebhookEvent, Webhooks } from "@octokit/webhooks";
+import { User } from "@octokit/webhooks-types";
 
 import { db } from "@aca/db";
 import { logger } from "@aca/shared/logger";
@@ -8,17 +9,10 @@ export function addWebhookHandlers(webhooks: Webhooks) {
   webhooks.on("installation.created", installationCreated);
 
   webhooks.on("issue_comment.created", commentCreated);
+  webhooks.on("issues.assigned", issueOrPrAssigned);
 
-  webhooks.on("issues.assigned", issueAssigned);
-  webhooks.on("pull_request.assigned", issueAssigned);
-
-  webhooks.on("issues.opened", (event) => {
-    console.info("issues.opened", event.payload);
-  });
-
-  webhooks.on("pull_request.opened", (event) => {
-    console.info("pull_request.opened", event.payload);
-  });
+  webhooks.on("pull_request.assigned", issueOrPrAssigned);
+  webhooks.on("pull_request.review_requested", reviewRequested);
 }
 
 async function installationDeleted(event: EmitterWebhookEvent<"installation.deleted">) {
@@ -107,29 +101,31 @@ async function commentCreated(event: EmitterWebhookEvent<"issue_comment.created"
   await Promise.all(notificationPromises);
 }
 
-async function issueAssigned(
+async function issueOrPrAssigned(
   event: EmitterWebhookEvent<"issues.assigned"> | EmitterWebhookEvent<"pull_request.assigned">
 ) {
   const installationId = event.payload.installation?.id;
   if (!installationId) throw new Error("installation id is missing");
   if (!event.payload.assignee) throw new Error("assignee is missing");
 
+  const senderId = event.payload.sender.id;
+  const assigneeId = event.payload.assignee.id;
   const accounts = await db.github_account_to_installation.findMany({
     where: {
       installation_id: installationId,
       github_account: {
         github_user_id: {
-          in: [event.payload.sender.id, event.payload.assignee.id],
+          in: [senderId, assigneeId],
         },
       },
     },
     include: { github_account: { include: { user: true } } },
   });
 
-  const assigneeAccount = accounts.find((a) => a.github_account.github_user_id === event.payload.assignee?.id);
+  const assigneeAccount = accounts.find((a) => a.github_account.github_user_id === assigneeId);
   if (!assigneeAccount) return;
 
-  const senderAccount = accounts.find((a) => a.github_account.github_user_id === event.payload.sender.id);
+  const senderAccount = accounts.find((a) => a.github_account.github_user_id === senderId);
   const senderName = senderAccount?.github_account.user.name || event.payload.sender.login;
 
   const isIssue = event.name === "issues";
@@ -148,6 +144,51 @@ async function issueAssigned(
       issue_id: isIssue ? issueOrPr.id : null,
       pr_id: !isIssue ? issueOrPr.id : null,
       title: issueOrPr.title,
+      repository_id: event.payload.repository.id,
+      repository_full_name: event.payload.repository.full_name,
+    },
+  });
+}
+
+async function reviewRequested(event: EmitterWebhookEvent<"pull_request.review_requested">) {
+  const installationId = event.payload.installation?.id;
+  if (!installationId) throw new Error("installation id is missing");
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const requestedReviewer: User = event.payload.requested_reviewer;
+  if (!requestedReviewer) throw new Error("requested_reviewer is missing");
+
+  const senderId = event.payload.sender.id;
+  const accounts = await db.github_account_to_installation.findMany({
+    where: {
+      installation_id: installationId,
+      github_account: {
+        github_user_id: {
+          in: [senderId, requestedReviewer.id],
+        },
+      },
+    },
+    include: { github_account: { include: { user: true } } },
+  });
+
+  const requestedReviewerAccount = accounts.find((a) => a.github_account.github_user_id === requestedReviewer.id);
+  if (!requestedReviewerAccount) return;
+
+  const senderAccount = accounts.find((a) => a.github_account.github_user_id === senderId);
+  const senderName = senderAccount?.github_account.user.name || event.payload.sender.login;
+
+  await db.notification_github.create({
+    data: {
+      notification: {
+        create: {
+          user_id: requestedReviewerAccount.user_id,
+          url: event.payload.pull_request.html_url,
+          from: senderName,
+        },
+      },
+      type: "review",
+      pr_id: event.payload.pull_request.id,
+      title: event.payload.pull_request.title,
       repository_id: event.payload.repository.id,
       repository_full_name: event.payload.repository.full_name,
     },
