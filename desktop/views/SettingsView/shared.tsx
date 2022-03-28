45e00246e945ff1010ea1417783f62cd22d46c81
Omar Duarte <omarduarte@gmail.com>
2022-03-28 14:01:13 +0100

    refactor: make layout component for settings

---
+++ desktop/views/SettingsView/shared.tsx
@@ -0,0 +1,94 @@
+import { observer } from "mobx-react";
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+import { AppLayout } from "@aca/desktop/layout/AppLayout";
+import { appViewContainerStyles } from "@aca/desktop/layout/Container";
+import { SystemTopBar } from "@aca/desktop/ui/systemTopBar";
+import { theme } from "@aca/ui/theme";
+
+interface Props {
+  topBarNavigationItems?: ReactNode;
+  isUserMenuIncluded?: boolean;
+  topBarTitle?: string;
+  headerTitle?: string;
+  navItems?: ReactNode;
+  body?: ReactNode;
+}
+
+export const SettingsLayout = observer(function SettingsLayout({
+  topBarNavigationItems,
+  topBarTitle,
+  headerTitle,
+  navItems,
+  body,
+  isUserMenuIncluded = true,
+}: Props) {
+  return (
+    <AppLayout>
+      <SystemTopBar
+        isFullWidth
+        isUserMenuIncluded={isUserMenuIncluded}
+        navigationItems={topBarNavigationItems}
+        titleNode={topBarTitle}
+      />
+      <UIHolder>
+        <UIBody>
+          <UINav>
+            <UIHeader key="settings-header">
+              <UIHeaderMain>{headerTitle}</UIHeaderMain>
+            </UIHeader>
+            {navItems}
+          </UINav>
+          <UIActiveSection>{body}</UIActiveSection>
+        </UIBody>
+      </UIHolder>
+    </AppLayout>
+  );
+});
+
+const UIHolder = styled.div<{}>`
+  ${appViewContainerStyles};
+  display: flex;
+  flex-direction: column;
+  ${theme.layout.settingsPageMaxWidth}
+
+  ${theme.spacing.pageSections.asGap}
+  overflow: hidden;
+`;
+
+const UINav = styled.nav`
+  margin-top: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  max-width: 240px;
+  flex-grow: 1;
+  width: 100%;
+`;
+
+const UIBody = styled.div`
+  display: flex;
+  gap: 24px;
+  overflow: auto;
+`;
+
+const UIActiveSection = styled.div`
+  flex-grow: 1;
+  overflow-y: auto;
+
+  /* Visually appears at same heigh of first nav item */
+  padding-top: 80px;
+  padding-bottom: 80px;
+
+  &::-webkit-scrollbar {
+    width: 0 !important;
+  }
+`;
+
+const UIHeader = styled.div<{}>`
+  ${theme.typo.pageTitle.medium};
+  padding-bottom: 24px;
+`;
+
+const UIHeaderMain = styled.div``;
--- desktop/views/onboarding/InitialIntegrations.tsx
+++ desktop/views/onboarding/InitialIntegrations.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import styled from "styled-components";
 
 import { defineAction } from "@aca/desktop/actions/action";
-import { AppLayout } from "@aca/desktop/layout/AppLayout";
-import { appViewContainerStyles } from "@aca/desktop/layout/Container";
 import { onboardingStore } from "@aca/desktop/store/onboarding";
 import { ActionButton } from "@aca/desktop/ui/ActionButton";
 import { IntegrationsManager } from "@aca/desktop/ui/IntegrationsManager";
-import { SystemTopBar } from "@aca/desktop/ui/systemTopBar";
 import { IconArrowRight } from "@aca/ui/icons";
 import { theme } from "@aca/ui/theme";
+
+import { SettingsLayout } from "../SettingsView/shared";
 
 const completeOnboarding = defineAction({
   name: "Continue",
@@ -25,76 +24,25 @@ const completeOnboarding = defineAction({
 
 export const InitialIntegrationsView = observer(function InitialIntegrationsView() {
   return (
-    <AppLayout>
-      <SystemTopBar isFullWidth isUserMenuIncluded={false} />
-      <UIHolder>
-        <UIBody>
-          <UINav>
-            <UIHeader>
-              <UIHeaderMain>Setup Integrations</UIHeaderMain>
-            </UIHeader>
-
-            <UIDescription>
-              To help you stay on top of things, and make the most of the tools you already use.
-            </UIDescription>
-            <UIActionButton action={completeOnboarding} kind="primary" iconAtStart={false} />
-          </UINav>
-          <UIActiveSection>
-            <IntegrationsManager />
-          </UIActiveSection>
-        </UIBody>
-      </UIHolder>
-    </AppLayout>
+    <SettingsLayout
+      isUserMenuIncluded={false}
+      headerTitle="Setup Integrations"
+      navItems={
+        <>
+          <UIDescription>
+            To help you stay on top of things, and make the most of the tools you already use.
+          </UIDescription>
+          <UIActionButton action={completeOnboarding} kind="primary" iconAtStart={false} />
+        </>
+      }
+      body={<IntegrationsManager />}
+    />
   );
 });
 
-const UIHolder = styled.div<{}>`
-  ${appViewContainerStyles};
-  display: flex;
-  flex-direction: column;
-  ${theme.layout.settingsPageMaxWidth}
-
-  ${theme.spacing.pageSections.asGap}
-  overflow: hidden;
-`;
-
-const UINav = styled.nav`
-  margin-top: 24px;
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
-  max-width: 240px;
-  flex-grow: 1;
-  width: 100%;
-`;
-
-const UIBody = styled.div`
-  display: flex;
-  gap: 24px;
-  overflow: auto;
-`;
-
-const UIActiveSection = styled.div`
-  flex-grow: 1;
-  overflow-y: auto;
-
-  /* Visually appears at same heigh of first nav item */
-  padding-top: 80px;
-  padding-bottom: 80px;
-
-  &::-webkit-scrollbar {
-    width: 0 !important;
-  }
-`;
-
-const UIHeader = styled.div<{}>`
-  ${theme.typo.pageTitle.medium};
-`;
-
-const UIHeaderMain = styled.div``;
-
 const UIDescription = styled.div`
   ${theme.typo.content};
+  padding-bottom: 24px;
 `;
 
 const UIActionButton = styled(ActionButton)`
