b12eb897a29647cb2f50f71de21444c4b4f7b777
Gregor Weber <gregorwbr@gmail.com>
2022-03-31 15:14:53 +0100

    how about a new cwd?

--- e2e/electron.test.ts
+++ e2e/electron.test.ts
@@ -1,9 +1,12 @@
+import path from "path";
+
 import { test } from "@playwright/test";
 import { _electron as electron } from "playwright";
 
 const launchElectron = () =>
   electron.launch({
     args: ["../desktop/dist/electron/index.js", "--user-data-dir=/tmp/electron-test-" + new Date().toISOString()],
+    cwd: path.join(__dirname, ".."),
   });
 
 test("Electron app is starting", async () => {
