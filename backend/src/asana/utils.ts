b222bab0bcb33bb4bad7722f02b3c7492f4dac98
Christoph Witzko <github@christophwitzko.com>
2022-04-08 14:36:32 +0200

    feat(asana): handle complete events & add docs

--- backend/src/asana/utils.ts
+++ backend/src/asana/utils.ts
@@ -8,6 +8,11 @@ export function createClient() {
     clientId: process.env.ASANA_CLIENT_ID,
     clientSecret: process.env.ASANA_CLIENT_SECRET,
     redirectUri: `${process.env.FRONTEND_URL}/api/backend/v1/asana/callback`,
+    defaultHeaders: {
+      // this header is required to suppress deprecation warnings
+      // https://forum.asana.com/t/update-on-our-planned-api-changes-to-user-task-lists-a-k-a-my-tasks/103828
+      "Asana-Enable": "new_user_task_lists",
+    },
   });
 }
 
