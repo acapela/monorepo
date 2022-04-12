43581b5ac35bfd8089dffc20edcd32dbff7358ba
Gregor Weber <gregorwbr@gmail.com>
2022-04-12 10:36:23 +0200

    fix(slack): add missing event handler

--- backend/src/events/events.ts
+++ backend/src/events/events.ts
@@ -4,11 +4,13 @@ import { extractAndAssertBearerToken } from "@aca/backend/src/authentication";
 import { AuthenticationError } from "@aca/backend/src/errors/errorTypes";
 import { handleGithubAccountToInstallationChanges } from "@aca/backend/src/github/onboarding";
 import { createHasuraEventsHandler } from "@aca/backend/src/hasura";
+import { handleNotificationChanges } from "@aca/backend/src/notification";
 import {
   Account,
   GithubAccountToInstallation,
   LinearIssue,
   LinearOauthToken,
+  Notification,
   NotificationSlackMessage,
   User,
   UserSlackInstallation,
@@ -30,6 +32,7 @@ const hasuraEvents = createHasuraEventsHandler<{
   github_account_to_installation_updates: GithubAccountToInstallation;
   linear_issue_updates: LinearIssue;
   notification_slack_message_updates: NotificationSlackMessage;
+  notification_updates: Notification;
   linear_oauth_token_updates: LinearOauthToken;
   user_slack_installation_updates: UserSlackInstallation;
   user_updates: User;
@@ -39,6 +42,7 @@ hasuraEvents.addHandler("account_updates", ["INSERT", "UPDATE", "DELETE"], handl
 hasuraEvents.addHandler("github_account_to_installation_updates", ["INSERT"], handleGithubAccountToInstallationChanges);
 hasuraEvents.addHandler("linear_issue_updates", ["INSERT", "UPDATE"], handleLinearIssueChanges);
 hasuraEvents.addHandler("linear_oauth_token_updates", ["INSERT"], handleLinearOauthTokenCreated);
+hasuraEvents.addHandler("notification_updates", ["UPDATE"], handleNotificationChanges);
 hasuraEvents.addHandler("notification_slack_message_updates", ["DELETE"], handleNotificationSlackMessageChanges);
 hasuraEvents.addHandler("user_slack_installation_updates", ["INSERT"], handleUserSlackInstallationChanges);
 hasuraEvents.addAnyEventHandler(handleCreateSyncRequests);
