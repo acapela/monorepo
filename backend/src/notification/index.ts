35b643abdd4161cf029966c9e8b83c3d0df5ede4
Gregor Weber <gregorwbr@gmail.com>
2022-04-13 10:23:31 +0200

    fix(slack): improve mark as read

--- backend/src/notification/index.ts
+++ backend/src/notification/index.ts
@@ -1,36 +1,43 @@
 import { HasuraEvent } from "@aca/backend/src/hasura";
 import { Notification, db } from "@aca/db";
 
-export async function handleNotificationChanges(event: HasuraEvent<Notification>) {
-  if (event.type !== "update") {
+// Add newly resolved Slack message notifications to the user_slack_conversation_read debounce buffer table which
+// is used to mark messages as read in the Slack API.
+async function addRelatedSlackMessageToMarkAsReadQueue(notification_id: string) {
+  const slackMessage = await db.notification_slack_message.findFirst({ where: { notification_id } });
+  const messageTs = slackMessage?.slack_message_ts;
+  if (!messageTs || !slackMessage.user_slack_installation_id || slackMessage.slack_thread_ts) {
+    // threads can not be marked as read yet in the Slack API
     return;
   }
+  const keyFields = {
+    user_slack_installation_id: slackMessage.user_slack_installation_id,
+    slack_conversation_id: slackMessage.slack_conversation_id,
+  };
+  const existing = await db.user_slack_conversation_read.findFirst({ where: keyFields });
+  if (existing) {
+    await db.user_slack_conversation_read.update({
+      where: { id: existing.id },
+      data: {
+        // we want to trigger an update even if this field remains unchanged to bump updated_at
+        slack_last_message_ts:
+          Number(existing.slack_last_message_ts) > Number(messageTs) ? existing.slack_last_message_ts : messageTs,
+      },
+    });
+  } else {
+    await db.user_slack_conversation_read.create({ data: { ...keyFields, slack_last_message_ts: messageTs } });
+  }
+}
 
-  if (event.item.resolved_at && !event.itemBefore.resolved_at) {
-    // Add newly resolved Slack message notifications to the user_slack_conversation_read debounce buffer table which
-    // is used to mark messages as read in the Slack API.
-    const slackMessage = await db.notification_slack_message.findFirst({ where: { notification_id: event.item.id } });
-    const messageTs = slackMessage?.slack_message_ts;
-    if (messageTs && slackMessage.user_slack_installation_id) {
-      slackMessage.user_slack_installation_id;
-      const keyFields = {
-        user_slack_installation_id: slackMessage.user_slack_installation_id,
-        slack_conversation_id: slackMessage.slack_conversation_id,
-        slack_thread_ts: slackMessage.slack_thread_ts,
-      };
-      const existing = await db.user_slack_conversation_read.findFirst({ where: keyFields });
-      if (existing) {
-        await db.user_slack_conversation_read.update({
-          where: { id: existing.id },
-          data: {
-            // we want to trigger an update even if this field remains unchanged to bump updated_at
-            slack_last_message_ts:
-              Number(existing.slack_last_message_ts) > Number(messageTs) ? existing.slack_last_message_ts : messageTs,
-          },
-        });
-      } else {
-        await db.user_slack_conversation_read.create({ data: { ...keyFields, slack_last_message_ts: messageTs } });
-      }
-    }
+export async function handleNotificationChanges(event: HasuraEvent<Notification>) {
+  if (
+    event.type == "update" &&
+    !event.item.was_auto_resolved &&
+    event.item.resolved_at &&
+    !event.itemBefore.resolved_at &&
+    // we only want to test this feature in staging for now
+    process.env.STAGE !== "production"
+  ) {
+    await addRelatedSlackMessageToMarkAsReadQueue(event.item.id);
   }
 }
