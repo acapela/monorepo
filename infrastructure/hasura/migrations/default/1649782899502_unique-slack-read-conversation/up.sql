f430791ac728d5ab8e10dcf11846814e89cf19e0
Gregor Weber <gregorwbr@gmail.com>
2022-04-20 09:35:51 +0200

    fix: clear user_slack_conversation_read in migration

--- infrastructure/hasura/migrations/default/1649782899502_unique-slack-read-conversation/up.sql
+++ infrastructure/hasura/migrations/default/1649782899502_unique-slack-read-conversation/up.sql
@@ -3,4 +3,5 @@ alter table "public"."user_slack_conversation_read" drop column "slack_thread_ts
 
 
 alter table "public"."user_slack_conversation_read" drop constraint if exists "user_slack_conversation_read_user_slack_installation_id_slack_conversation_id_key";
+delete from "public"."user_slack_conversation_read";
 alter table "public"."user_slack_conversation_read" add constraint "user_slack_conversation_read_user_slack_installation_id_slack_conversation_id_key" unique ("user_slack_installation_id", "slack_conversation_id");
