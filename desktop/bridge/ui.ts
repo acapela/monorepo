b39be8dd2097c21d8212ff0f8545e7c5d1d6bc5c
Heiki Riesenkampf <me@heikir.com>
2022-04-22 15:29:38 +0200

    feat(analytics): add tracking to the onboarding flow

--- desktop/bridge/ui.ts
+++ desktop/bridge/ui.ts
@@ -1,7 +1,8 @@
 import { createBridgeValue } from "./base/persistance";
 
+export type AppTheme = "auto" | "light" | "dark";
 interface UISettings {
-  theme: "auto" | "light" | "dark";
+  theme: AppTheme;
 }
 
 export const uiSettingsBridge = createBridgeValue<UISettings>("ui-settings", {
