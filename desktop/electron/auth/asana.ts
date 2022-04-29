76f1837365b85d3655d9cdb3de27c139b90d51fa
Christoph Witzko <github@christophwitzko.com>
2022-04-29 14:03:31 +0200

    feat: move RETRY_TIMES and RETRY_DELAY_MS to utils

--- desktop/electron/auth/asana.ts
+++ desktop/electron/auth/asana.ts
@@ -3,12 +3,10 @@ import { BrowserWindow, session } from "electron";
 import { asanaAuthTokenBridgeValue, loginAsanaBridge, logoutAsanaBridge } from "@aca/desktop/bridge/auth";
 import { FRONTEND_URL } from "@aca/desktop/lib/env";
 
-import { authWindowDefaultOptions } from "./utils";
+import { RETRY_DELAY_MS, RETRY_TIMES, authWindowDefaultOptions } from "./utils";
 
 const userAgent =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.81 Safari/537.36";
-const RETRY_TIMES = 30;
-const RETRY_DELAY_MS = 500;
 
 export async function loginAsana() {
   const window = new BrowserWindow({ ...authWindowDefaultOptions });
--- desktop/electron/auth/utils.ts
+++ desktop/electron/auth/utils.ts
@@ -4,3 +4,6 @@ export const authWindowDefaultOptions: BrowserWindowConstructorOptions = {
   width: 600,
   height: 700,
 };
+
+export const RETRY_TIMES = 30;
+export const RETRY_DELAY_MS = 500;
