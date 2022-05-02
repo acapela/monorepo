59155d71a866093e320e1356b8d2b93c5172934b
Omar Duarte <omarduarte@gmail.com>
2022-05-02 16:16:44 +0200

    fix: notion user-mentioned bug (#1300)
    * feat: initial desktop test setup
    
    * feat: test working
    
    * feat: fixes failing user mentioned tests
    
    * fix: PR feedback

---
+++ desktop/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/(*.)+(spec|test).ts?(x)"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
+};
