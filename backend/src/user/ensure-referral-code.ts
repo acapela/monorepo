6de23eb8f3d979702c0e3ea0738e8b1813d2db72
Christoph Witzko <github@christophwitzko.com>
2022-05-03 09:51:44 +0200

    feat: ensure referral code for all users

---
+++ backend/src/user/ensure-referral-code.ts
@@ -0,0 +1,11 @@
+import { db } from "@aca/db";
+
+import { createNewReferralCode } from "../utils";
+
+// once all users have a referral code, we can delete this cron job
+export async function ensureReferralCode() {
+  const users = await db.user.findMany({ where: { referral_code: null } });
+  for (const user of users) {
+    await db.user.update({ where: { id: user.id }, data: { referral_code: createNewReferralCode() } });
+  }
+}
