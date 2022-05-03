5b79f82fcafb5f117ab9205584817ff3fe490033
Christoph Witzko <github@christophwitzko.com>
2022-05-03 17:25:02 +0200

    fix: check missing referral codes on startup

--- backend/src/user/ensureReferralCode.ts
+++ backend/src/user/ensureReferralCode.ts
@@ -2,7 +2,7 @@ import { db } from "@aca/db";
 
 import { createNewReferralCode } from "../utils";
 
-// once all users have a referral code, we can delete this cron job
+// once all users have a referral code, we can delete this helper
 export async function ensureReferralCode() {
   const users = await db.user.findMany({ where: { referral_code: null } });
   for (const user of users) {
