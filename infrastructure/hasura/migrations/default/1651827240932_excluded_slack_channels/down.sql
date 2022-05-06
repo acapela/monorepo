9616220a7e7a1b0b4c0a1ecd66f112fc7b5ad4b1
Felix Balda <private@felixbalda.com>
2022-05-06 16:27:50 +0200

    fix: dropped column in wrong order

--- infrastructure/hasura/migrations/default/1651827240932_excluded_slack_channels/down.sql
+++ infrastructure/hasura/migrations/default/1651827240932_excluded_slack_channels/down.sql
@@ -1,15 +1,15 @@
--- Remove the 2 added columns when migrating down
-ALTER TABLE
-    "public"."user_slack_channels_by_team" DROP COLUMN "are_all_channels_included";
-
-ALTER TABLE
-    "public"."user_slack_channels_by_team" DROP COLUMN "excluded_channels";
-
 -- Add the all-channels-included placeholder back to included_channels if 
--- are_all_channels_included is true and if it isn't present already
+-- are_all_channels_included is true
 UPDATE
     "public"."user_slack_channels_by_team"
 SET
     "included_channels" = "included_channels" || '["*****"]' :: jsonb
 WHERE
     "are_all_channels_included" = 'true';
+
+-- Remove the 2 added columns when migrating down
+ALTER TABLE
+    "public"."user_slack_channels_by_team" DROP COLUMN "are_all_channels_included";
+
+ALTER TABLE
+    "public"."user_slack_channels_by_team" DROP COLUMN "excluded_channels";
