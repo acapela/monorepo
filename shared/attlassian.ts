12615b60b8b6129d8b9f2c33065054c12495c24f
Omar Duarte <omarduarte@gmail.com>
2022-05-09 15:05:16 +0300

    feat: jira issue updated ui

--- shared/attlassian.ts
+++ shared/attlassian.ts
@@ -8,3 +8,14 @@ export const SUPPORTED_FIELDS = [
   "labels",
   "reporter",
 ];
+
+export const jiraIssueFieldAlias: Record<typeof SUPPORTED_FIELDS[number], string> = {
+  summary: "Title",
+  description: "Description",
+  attachment: "Attachment",
+  duedate: "Due Date",
+  timeoriginalestimate: "Original Estimate",
+  priority: "Priority",
+  labels: "Labels",
+  reporter: "Issue Reporter",
+};
