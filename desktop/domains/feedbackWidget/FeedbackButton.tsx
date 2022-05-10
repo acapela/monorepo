b523925baf1157d0333ab2f5a7e4bd7e3b18ec7c
Adam Pietrasiak <adam@pietrasiak.com>
2022-05-10 22:57:10 +0200

    feat: nudge to invite friends (#1353)

--- desktop/domains/feedbackWidget/FeedbackButton.tsx
+++ desktop/domains/feedbackWidget/FeedbackButton.tsx
@@ -1,14 +1,45 @@
-import React from "react";
+import { AnimatePresence } from "framer-motion";
+import { action } from "mobx";
+import { observer } from "mobx-react-lite";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 import { trackEvent } from "@aca/desktop/analytics";
 import { USERSNAP_GLOBAL_API_KEY } from "@aca/desktop/lib/env";
 import { desktopRouter } from "@aca/desktop/routes";
+import { useIsElementOrChildHovered } from "@aca/shared/hooks/useIsElementOrChildHovered";
+import { createMobxPersistedStore } from "@aca/shared/mobx/persistedStore";
+import { createTimeout } from "@aca/shared/time";
 import { Button } from "@aca/ui/buttons/Button";
+import { TooltipLabel } from "@aca/ui/popovers/TooltipLabel";
 
 import { openFeedbackWidget } from ".";
 
-export function FeedbackButton() {
+const inviteFriendPersistedStore = createMobxPersistedStore("invite-friend", {
+  didInteract: false,
+});
+
+function useLazyFlag(time: number) {
+  const [isOn, setIsOn] = useState(false);
+
+  useEffect(() => {
+    return createTimeout(() => {
+      setIsOn(true);
+    }, time);
+  }, [time]);
+
+  return isOn;
+}
+
+export const FeedbackButton = observer(function FeedbackButton() {
+  const didDelayToShowInitialTooltipPass = useLazyFlag(1500);
+  const inviteButtonRef = useRef<HTMLDivElement>(null);
+
+  const isInviteButtonHovered = useIsElementOrChildHovered(inviteButtonRef);
+
+  const shouldShowTooltip =
+    (didDelayToShowInitialTooltipPass && !inviteFriendPersistedStore.didInteract) || isInviteButtonHovered;
+
   /**
    * This method takes into account other display rules,
    * like filtering by URL path, email, logged in users and so on.
@@ -28,9 +59,20 @@ export function FeedbackButton() {
 
   return (
     <UIHolder>
-      <Button kind="transparent" onClick={handleInviteFriendClick}>
-        Invite a friend
-      </Button>
+      <AnimatePresence>
+        {shouldShowTooltip && <TooltipLabel anchorRef={inviteButtonRef} label="Have a chance to win AirPods Max" />}
+      </AnimatePresence>
+      <UIInviteButtonHolder
+        ref={inviteButtonRef}
+        onMouseEnter={action(() => {
+          inviteFriendPersistedStore.didInteract = true;
+        })}
+      >
+        <Button kind="primarySubtle" onClick={handleInviteFriendClick}>
+          Invite friends
+        </Button>
+      </UIInviteButtonHolder>
+
       {USERSNAP_GLOBAL_API_KEY && (
         <Button kind="transparent" onClick={handleFeedbackButtonClick}>
           Feedback
@@ -38,9 +80,11 @@ export function FeedbackButton() {
       )}
     </UIHolder>
   );
-}
+});
 
 const UIHolder = styled.div`
   display: flex;
   gap: 4px;
 `;
+
+const UIInviteButtonHolder = styled.div``;
