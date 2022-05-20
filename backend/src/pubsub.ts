0aefa359b6d05c645af7ff416f3f31a0e04f43a0
Christoph Witzko <github@christophwitzko.com>
2022-05-20 13:00:06 +0200

    feat: add redis locking mechanism

---
+++ backend/src/pubsub.ts
@@ -0,0 +1,25 @@
+import Client from "ioredis";
+import Redlock from "redlock";
+
+import { db } from "@aca/db";
+
+const redisClient = new Client({ host: "localhost" });
+const redlock = new Redlock([redisClient], {
+  retryCount: 100,
+});
+
+export async function acquireLock(type: string, messageId: string) {
+  return redlock.acquire(`${type}:${messageId}`, 30000);
+}
+
+export async function markAsProcessed(type: string, messageId: string): Promise<boolean> {
+  try {
+    await db.processed_message.create({ data: { id: `${type}:${messageId}` } });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    // was already marked as processed
+    if (e.code === "P2002") return false;
+    throw e;
+  }
+  return true;
+}
