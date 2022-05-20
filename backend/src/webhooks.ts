95eeec5659ecf402b2b2ec7331035a33245eb71c
Christoph Witzko <github@christophwitzko.com>
2022-05-20 13:00:06 +0200

    feat: add redis locking mechanism

--- backend/src/webhooks.ts
+++ backend/src/webhooks.ts
@@ -3,6 +3,8 @@ import * as Sentry from "@sentry/node";
 
 import { logger } from "@aca/shared/logger";
 
+import { acquireLock, markAsProcessed } from "./pubsub";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ProcessFunc = (rawBody: string, params: any, headers: any) => Promise<void>;
 
@@ -11,7 +13,14 @@ export function listenForWebhooks(service: string, processFn: ProcessFunc) {
   const topicName = `${process.env.STAGE}-webhooks-${service}`;
   const subscription = pubsub.topic(topicName).subscription(`${topicName}-sub`);
   subscription.on("message", async (message) => {
+    let lock = null;
     try {
+      lock = await acquireLock(service, message.id);
+      if (!(await markAsProcessed(service, message.id))) {
+        logger.warn(message.id, "was already processed");
+        await lock.unlock();
+        return;
+      }
       const data = JSON.parse(message.data.toString());
       await processFn(data.rawBody, data.params, data.headers);
       message.ack();
@@ -19,6 +28,7 @@ export function listenForWebhooks(service: string, processFn: ProcessFunc) {
       logger.error(err);
       Sentry.captureException(err);
     }
+    if (lock) await lock.unlock();
   });
   subscription.on("error", (err) => {
     logger.error(err);
