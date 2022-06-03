647b19fa4427397ce7ffbb641fc59b4fe733e847
Heiki Riesenkampf <me@heikir.com>
2022-06-03 17:02:11 +0200

    fix(subscription): revert type error

--- shared/google.ts
+++ shared/google.ts
@@ -1,5 +1,3 @@
-import { SubscriptionPlan } from "@aca/gql";
-
 const wrapScope = (scopeName: string) => `https://www.googleapis.com/auth/${scopeName}`;
 
 const BASE_SCOPES = ["userinfo.profile", "userinfo.email"];
@@ -9,4 +7,4 @@ export const GOOGLE_AUTH_SCOPES = BASE_SCOPES.map(wrapScope).join(" ");
 export const GMAIL_SCOPES = ["readonly", "modify"].map((s) => `gmail.${s}`);
 export const GOOGLE_AUTH_WITH_GMAIL_SCOPES = [...BASE_SCOPES, ...GMAIL_SCOPES].map(wrapScope).join(" ");
 
-export const isGmailIncludedInPlan = (value?: Lowercase<SubscriptionPlan>) => value === "ultimate";
+export const isGmailIncludedInPlan = (value?: string) => value === "ultimate";
