933ba745e606d8e5da5349d34f6c239e668591a8
Adam Pietrasiak <adam@pietrasiak.com>
2022-06-07 20:51:39 +0200

    fix: clientdb type fix

--- clientdb/entity/find.ts
+++ clientdb/entity/find.ts
@@ -4,15 +4,15 @@ import { typedKeys } from "@aca/shared/object";
 
 import { EntityDefinition } from "./definition";
 import { Entity } from "./entity";
-import { IndexValueInput, IndexableKey } from "./queryIndex";
+import { IndexValueInput } from "./queryIndex";
 import { EntityStore } from "./store";
 
-type FindObjectPartInput<T, C> = Partial<{
-  [key in keyof T]: key extends IndexableKey<Entity<T, C>> ? IndexValueInput<T[key]> : never;
+type FindObjectPartInput<T> = Partial<{
+  [key in keyof T]: IndexValueInput<T[key]>;
 }>;
 
-export type FindObjectInput<T, C> = FindObjectPartInput<T, C> & {
-  $or?: FindObjectPartInput<T, C>[];
+export type FindObjectInput<T> = FindObjectPartInput<T> & {
+  $or?: FindObjectPartInput<T>[];
 };
 
 type AnyKeyIndexInput<T> = IndexValueInput<T> extends infer U ? U[keyof U] : never;
@@ -22,7 +22,7 @@ type MaybeObservableArray<T> = IObservableArray<T> | T[];
 function getRemainingItemsAfterApplyingQueryFields<Data, Connections>(
   currentItems: Entity<Data, Connections>[],
   store: EntityStore<Data, Connections>,
-  queryObject: FindObjectPartInput<Data, Connections>
+  queryObject: FindObjectPartInput<Data & Connections>
 ) {
   let passingItems = currentItems;
 
@@ -43,7 +43,7 @@ function getRemainingItemsAfterApplyingQueryFields<Data, Connections>(
 export function findInSourceByObjectInput<Data, Connections>(
   source: MaybeObservableArray<Entity<Data, Connections>>,
   store: EntityStore<Data, Connections>,
-  queryObject: FindObjectInput<Data, Connections>
+  queryObject: FindObjectInput<Data & Connections>
 ) {
   if (source.length === 0) return [];
 
@@ -52,7 +52,7 @@ export function findInSourceByObjectInput<Data, Connections>(
   const itemsMatchingRootQuery = getRemainingItemsAfterApplyingQueryFields(
     source,
     store,
-    requiredKeysRaw as FindObjectPartInput<Data, Connections>
+    requiredKeysRaw as FindObjectPartInput<Data & Connections>
   );
 
   if (!itemsMatchingRootQuery.length) {
@@ -82,7 +82,7 @@ export function findInSourceByObjectInput<Data, Connections>(
 
 type FindFunctionalInput<T> = (item: T) => boolean;
 
-export type FindInput<D, C> = FindObjectInput<D, C> | FindFunctionalInput<Entity<D, C>>;
+export type FindInput<D, C> = FindObjectInput<D & C> | FindFunctionalInput<Entity<D, C>>;
 
 export type EntityFindInputByDefinition<Def> = Def extends EntityDefinition<infer D, infer C> ? FindInput<D, C> : never;
 
