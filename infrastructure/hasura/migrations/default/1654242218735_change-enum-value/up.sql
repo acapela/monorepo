5aeaef8d72fa3b7906f37821c98e3d15b9ae170d
Felix Balda <private@felixbalda.com>
2022-06-07 14:45:07 +0200

    fix: subscription plan rename migration

--- infrastructure/hasura/migrations/default/1654242218735_change-enum-value/up.sql
+++ infrastructure/hasura/migrations/default/1654242218735_change-enum-value/up.sql
@@ -1,2 +1,23 @@
-UPDATE public.subscription_plan SET value = 'ultimate' WHERE value = 'business';
-UPDATE public.user SET subscription_plan = 'ultimate' WHERE subscription_plan = 'business';
+ALTER TABLE
+    public.user DROP CONSTRAINT user_subscription_plan_fkey;
+
+UPDATE
+    public.user
+SET
+    subscription_plan = 'ultimate'
+WHERE
+    subscription_plan = 'business';
+
+UPDATE
+    public.subscription_plan
+SET
+    value = 'ultimate'
+WHERE
+    value = 'business';
+
+ALTER TABLE
+    "public"."user"
+ADD
+    CONSTRAINT "user_subscription_plan_fkey" FOREIGN KEY ("subscription_plan") REFERENCES "public"."subscription_plan" ("value") ON UPDATE RESTRICT ON DELETE
+SET
+    NULL;
