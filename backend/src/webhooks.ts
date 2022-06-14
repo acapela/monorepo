712d9313a17cd7e153e9c64dcf7dbae8333d6607
Christoph Witzko <github@christophwitzko.com>
2022-06-14 14:42:52 +0200

    feat: improve pubsub flow controll and close subscriptions on shutdown

--- backend/src/webhooks.ts
+++ backend/src/webhooks.ts
@@ -1,4 +1,4 @@
-import { PubSub } from "@google-cloud/pubsub";
+import { PubSub, Subscription } from "@google-cloud/pubsub";
 import * as Sentry from "@sentry/node";
 
 import { logger } from "@aca/shared/logger";
@@ -8,23 +8,36 @@ import { acquireLock, markAsProcessed, removeMarkAsProcessed } from "./pubsub";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ProcessFunc = (rawBody: string, params: any, headers: any) => Promise<void>;
 
+const allSubscriptions: Subscription[] = [];
+export async function closeSubscriptions() {
+  for (const subscription of allSubscriptions) {
+    logger.info(`closing subscription ${subscription.name}`);
+    await subscription.close();
+  }
+}
+
 export function listenForWebhooks(service: string, processFn: ProcessFunc) {
   const pubsub = new PubSub();
   const topicName = `${process.env.STAGE}-webhooks-${service}`;
-  const subscription = pubsub.topic(topicName).subscription(`${topicName}-sub`);
+  const subscription = pubsub.topic(topicName).subscription(`${topicName}-sub`, {
+    flowControl: { maxMessages: 10, allowExcessMessages: false },
+  });
   subscription.on("message", async (message) => {
     let lock = null;
+    let shouldAck = false;
     try {
       lock = await acquireLock(service, message.id);
       if (!(await markAsProcessed(service, message.id))) {
         logger.info(`message ${message.id} was already processed`);
         lock.unlock().catch((err) => logger.warn("unlock error (processed)", err));
+        // we do not acknowledge the message if it was already processed
+        message.nack();
         return;
       }
       const data = JSON.parse(message.data.toString());
       try {
         await processFn(data.rawBody, data.params, data.headers);
-        message.ack();
+        shouldAck = true;
       } catch (err) {
         logger.error(err);
         Sentry.captureException(err);
@@ -35,10 +48,12 @@ export function listenForWebhooks(service: string, processFn: ProcessFunc) {
       logger.error(err);
       Sentry.captureException(err);
     }
+    shouldAck ? message.ack() : message.nack();
     if (lock) lock.unlock().catch((err) => logger.warn("unlock error", err));
   });
   subscription.on("error", (err) => {
     logger.error(err);
     Sentry.captureException(err);
   });
+  allSubscriptions.push(subscription);
 }
