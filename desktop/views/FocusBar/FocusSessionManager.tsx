1827fea5b5149289bd137459e72112caa76513f5
Adam Pietrasiak <adam@pietrasiak.com>
2022-06-15 10:14:30 +0200

    fix: hide focus bar when disabled

--- desktop/views/FocusBar/FocusSessionManager.tsx
+++ desktop/views/FocusBar/FocusSessionManager.tsx
@@ -1,14 +1,16 @@
 import { observer } from "mobx-react";
 import React from "react";
 
+import { applicationWideSettingsBridge } from "@aca/desktop/bridge/system";
 import { focusSessionStore } from "@aca/desktop/store/focus";
 
 import { FocusBarWindow } from "./FocusBarWindow";
 
 export const FocusSessionManager = observer(() => {
+  const { useFocusBar } = applicationWideSettingsBridge.get();
   const session = focusSessionStore.session;
 
-  if (!session) return null;
+  if (!session || !useFocusBar) return null;
 
   return <FocusBarWindow session={session} />;
 });
