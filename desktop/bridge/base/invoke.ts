6eb46f5318580a50323e38b9322afa6c99aa73a5
Omar Duarte <omarduarte@gmail.com>
2022-06-16 17:01:52 +0300

    fix: logger works again (#1443)

--- desktop/bridge/base/invoke.ts
+++ desktop/bridge/base/invoke.ts
@@ -44,7 +44,7 @@ export function createInvokeBridge<Input = void, Result = void>(key: string) {
 
   async function handleRequest(arg: Input, event?: IpcMainInvokeEvent) {
     if (!invokeHandler) {
-      throw new Error(`No handler for arg ${JSON.stringify(arg)} given event ${JSON.stringify(event)}`);
+      throw new Error(`No handler initialized`);
     }
     const result = await invokeHandler(arg, event);
 
