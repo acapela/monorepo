adcf8a3bded34264e22a0244dc40393c423f0734
Omar Duarte <omarduarte@gmail.com>
2022-06-23 19:07:33 +0300

    refactor: remove unused code

--- backend/src/atlassian/capturing/utils.ts
+++ backend/src/atlassian/capturing/utils.ts
@@ -36,33 +36,6 @@ export async function fetchIssueWatchers(jiraAccount: JiraAccountWithAllDetails,
   return watchers?.watchers?.map((w) => w.accountId) ?? [];
 }
 
-export async function getLeastRecentlyUsedAtlassianAccount(
-  atlassianCloudUrl: string
-): Promise<JiraAccountWithAllDetails> {
-  const jiraAccounts = await db.jira_account.findMany({
-    where: {
-      atlassian_site: {
-        url: atlassianCloudUrl,
-      },
-    },
-    orderBy: {
-      rest_req_last_used_at: "asc",
-    },
-    include: {
-      account: true,
-      atlassian_site: true,
-    },
-  });
-
-  assert(
-    jiraAccounts[0],
-    "jira account not found for atlassianCloudUrl " + atlassianCloudUrl,
-    logger.error.bind(logger)
-  );
-
-  return jiraAccounts[0];
-}
-
 type WatcherId = string;
 export async function getWatchersIfPermitted(
   atlassianCloudUrl: string,
