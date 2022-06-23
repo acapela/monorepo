976453f448a350bfd19616d7f900831b7551f823
Christoph Witzko <github@christophwitzko.com>
2022-06-23 12:04:14 +0200

    fix: use assert

--- backend/src/slack/send.ts
+++ backend/src/slack/send.ts
@@ -3,18 +3,22 @@ import { Team } from "@slack/web-api/dist/response/TeamInfoResponse";
 import { getPermalink, getSlackInstallationData } from "@aca/backend/src/slack/utils";
 import { db } from "@aca/db";
 import { SendMessageInput } from "@aca/gql";
+import { assert } from "@aca/shared/assert";
 
 import { slackClient } from "./app";
 
 export async function sendSlackMessage(userId: string, msg: SendMessageInput) {
   const [team, channel] = msg.to.split("/");
+  assert(team, "team is missing");
+  assert(channel, "channel is missing");
   const slackInstallation = await db.user_slack_installation.findFirst({
     where: { user_id: userId, slack_team_id: team },
     include: {
       slack_team: true,
     },
   });
-  if (!slackInstallation) throw new Error("user has no slack installation");
+
+  assert(slackInstallation, "user has no slack installation");
   const slackInstallationData = getSlackInstallationData(slackInstallation);
 
   const res = await slackClient.chat.postMessage({
