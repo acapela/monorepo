ff8d4ef9a9887c093c029b26b008c11ed822cd50
Christoph Witzko <github@christophwitzko.com>
2022-07-15 12:30:14 +0200

    fix: cleanup outdated linear issues

--- backend/src/userCleanup/index.ts
+++ backend/src/userCleanup/index.ts
@@ -7,6 +7,10 @@ type OutdatedUser = {
   latest_resolved_at: string;
 };
 
+type OutdatedLinearIssue = {
+  organization_id: string;
+};
+
 export async function userCleanup() {
   const outdatedUsers =
     (await db.$queryRaw`SELECT user_id, email, latest_resolved_at FROM (SELECT user_id, MAX(resolved_at) latest_resolved_at FROM public.notification GROUP BY user_id) as nr JOIN public.user ON id = nr.user_id WHERE latest_resolved_at < NOW() - INTERVAL '10 days' OR latest_resolved_at IS NULL;`) as OutdatedUser[];
@@ -17,6 +21,17 @@ export async function userCleanup() {
       await db.deleted_user.create({ data: { email: outdatedUser.email } });
     } catch (e) {
       logger.warn(`Failed to delete user ${outdatedUser.user_id}`, e);
+    }
+  }
+  const outdatedLinearIssues =
+    (await db.$queryRaw`SELECT DISTINCT organization_id FROM public.linear_issue WHERE organization_id NOT IN (SELECT linear_organization_id FROM public.linear_oauth_token);`) as OutdatedLinearIssue[];
+
+  for (const outdatedLinearIssue of outdatedLinearIssues) {
+    logger.info(`Deleting issues for organization ${outdatedLinearIssue.organization_id}`);
+    try {
+      await db.linear_issue.deleteMany({ where: { id: outdatedLinearIssue.organization_id } });
+    } catch (e) {
+      logger.warn(`Failed to delete issues for organization ${outdatedLinearIssue.organization_id}`, e);
     }
   }
 }
